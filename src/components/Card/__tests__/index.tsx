e2e4adc45285cbd14c3be9298a9b4bdb48a5e938
Ethan Neff <ethan.neff@eneff.com>
2018-12-28 11:28:51 -0800

    chore: increase card test coverage

--- src/components/Card/__tests__/index.tsx
+++ src/components/Card/__tests__/index.tsx
@@ -6,3 +6,8 @@ it("renders correctly", () => {
   const dom = create(<Card />).toJSON();
   expect(dom).toMatchSnapshot();
 });
+
+it("renders correctly with onPress", () => {
+  const dom = create(<Card onPress={() => undefined} />).toJSON();
+  expect(dom).toMatchSnapshot();
+});
