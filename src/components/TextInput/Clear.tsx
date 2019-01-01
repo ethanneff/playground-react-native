e07a001fc234d3f98698b1a00c44a51176317ae0
Ethan Neff <ethan.neff@eneff.com>
2019-01-01 15:15:16 -0800

    style: made clear button closer to right edge

--- src/components/TextInput/Clear.tsx
+++ src/components/TextInput/Clear.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import * as React from "react";
 import { StyleSheet } from "react-native";
 import { Button } from "..";
 
 const styles = StyleSheet.create({
   clear: {
     position: "absolute",
-    right: 0,
+    right: -6,
     top: 6
   }
 });
@@ -16,12 +16,12 @@ interface Props {
 }
 
 export class Clear extends React.PureComponent<Props> {
-  public icon = "close";
+  public icon = "close-circle";
   public render() {
     const { hidden, onPress } = this.props;
     return (
       <Button
-        neutral
+        secondary
         hidden={hidden}
         icon={this.icon}
         buttonStyle={styles.clear}
