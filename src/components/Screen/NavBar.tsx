a6f7a0b8524eaa08c8722c0f0021d6772bebcad2
Ethan Neff <ethan.neff@eneff.com>
2019-03-26 22:25:09 -0700

    fix: remove background color from navbar

--- src/components/Screen/NavBar.tsx
+++ src/components/Screen/NavBar.tsx
@@ -7,7 +7,6 @@ import { NavButton } from "./NavButton";
 const styles = StyleSheet.create({
   container: {
     alignItems: "center",
-    backgroundColor: Theme.color.background,
     flexDirection: "row",
     justifyContent: "space-between"
   },
@@ -36,7 +35,9 @@ export class NavBar extends React.PureComponent<Props> {
       rightIcon = "close"
     } = this.props;
     const isEnabled = title.length > 0 || onLeftPress || onRightPress;
-    if (!isEnabled) { return null; }
+    if (!isEnabled) {
+      return null;
+    }
     return (
       <View style={styles.container}>
         <NavButton icon={leftIcon} onPress={onLeftPress} />
