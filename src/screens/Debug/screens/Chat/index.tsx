05e6d4037e11956bd05a72365cefc39ff11ba102
Ethan Neff <ethan.neff@eneff.com>
2019-03-29 16:41:36 -0700

    fix: add more features to chat

--- src/screens/Debug/screens/Chat/index.tsx
+++ src/screens/Debug/screens/Chat/index.tsx
@@ -1,17 +1,24 @@
 import * as React from "react";
-import { FlatList, StyleSheet } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import { RouteComponentProps } from "react-router";
 import uuid from "uuid";
 import { Button, Screen, Text, TextInput } from "../../../../components";
+import RelativeDate from "../../../../components/RelativeDate";
+import { Theme } from "../../../../utils";
 
 interface Message {
   id: string;
   createdBy: string;
+  updatedAt: number;
   createdAt: number;
   message: string;
+  archive: boolean;
 }
 type Props = RouteComponentProps;
-interface State { message: String; messages: Message[] }
+interface State {
+  message: string;
+  messages: Message[];
+}
 
 export class Chat extends React.PureComponent<Props, State> {
   public state = {
@@ -26,14 +33,13 @@ export class Chat extends React.PureComponent<Props, State> {
   public user = "123";
   public render() {
     const { message, messages } = this.state;
-    console.log(messages);
     return (
       <Screen onLeftPress={this.navBack}>
         <FlatList
           inverted
           data={messages}
           keyExtractor={this.keyExtractor}
-          renderItem={({ item }) => <Text title={item.message} key={item.id} />}
+          renderItem={this.renderItem}
         />
         {/* <View style={this.styles.row}> */}
         <Button icon="check" onPress={this.onSubmit} />
@@ -49,16 +55,55 @@ export class Chat extends React.PureComponent<Props, State> {
     );
   }
 
+  private renderItem = ({ item }: { item: Message }) => {
+    return (
+      <View
+        key={item.id}
+        style={{
+          flexDirection: "row",
+          justifyContent: "flex-end",
+          paddingTop: Theme.padding.p02
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: Theme.color.primary,
+            borderRadius: Theme.padding.p03,
+            padding: Theme.padding.p02
+          }}
+        >
+          <Text title={item.message} body2 />
+          <RelativeDate date={item.createdAt} />
+        </View>
+        <Button
+          title="D"
+          onPress={() => {
+            this.setState({
+              messages: this.state.messages.filter(
+                (message: Message) => message.id !== item.id
+              )
+            });
+          }}
+        />
+      </View>
+    );
+  };
   private keyExtractor = (item: Message) => item.id;
   private navBack = () => this.props.history.goBack();
-  private onMessageChange = (message: String) => this.setState({ message });
+  private onMessageChange = (message: string) => this.setState({ message });
   private onSubmit = () => {
     const { message, messages } = this.state;
+    if (message.trim().length === 0) {
+      return;
+    }
+    const date = Date.now();
     const newMessage: Message = {
-      id: uuid.v4(),
+      archive: false,
+      createdAt: date,
       createdBy: this.user,
-      createdAt: Date.now(),
-      message
+      id: uuid.v4(),
+      message,
+      updatedAt: date
     };
     this.setState({ message: "", messages: [newMessage, ...messages] });
   };
