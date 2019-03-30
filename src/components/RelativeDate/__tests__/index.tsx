f4efb76f12d9a04c56f202623239d4d57dcc3c93
Ethan Neff <ethan.neff@eneff.com>
2019-03-29 17:12:56 -0700

    fix: update relative date tests to not update on each run

--- src/components/RelativeDate/__tests__/index.tsx
+++ src/components/RelativeDate/__tests__/index.tsx
@@ -2,19 +2,21 @@ import * as React from "react";
 import { create } from "react-test-renderer";
 import RelativeDate from "..";
 
+const date = 1553904714478;
+
 it("renders correctly", () => {
-  const dom = create(<RelativeDate date={Date.now()} />).toJSON();
+  const dom = create(<RelativeDate date={date} />).toJSON();
   expect(dom).toMatchSnapshot();
 });
 
 it("renders correctly on touch", () => {
-  const dom = create(<RelativeDate date={Date.now()} />);
+  const dom = create(<RelativeDate date={date} />);
   dom.root.instance.toggleRelativeDate();
   expect(dom).toMatchSnapshot();
 });
 
 it("updates state touch", () => {
-  const dom = create(<RelativeDate date={Date.now()} />);
+  const dom = create(<RelativeDate date={date} />);
   expect(dom.root.instance.state.showRelativeDate).toBe(true);
   dom.root.instance.toggleRelativeDate();
   expect(dom.root.instance.state.showRelativeDate).toBe(false);
@@ -23,7 +25,7 @@ it("updates state touch", () => {
 });
 
 it("kills timer on unmount", () => {
-  const dom = create(<RelativeDate date={Date.now()} />);
+  const dom = create(<RelativeDate date={date} />);
   const timer = dom.root.instance.timer;
   expect(timer._onTimeout).not.toBeNull();
   dom.unmount();
