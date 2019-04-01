d18ec0788acacd8c1b2467c47c9821e1dca5b146
Ethan Neff <ethan.neff@eneff.com>
2019-04-01 10:36:17 -0700

    fix: remove date diff with ci

--- src/components/RelativeDate/__tests__/index.tsx
+++ src/components/RelativeDate/__tests__/index.tsx
@@ -3,35 +3,39 @@ import { create, ReactTestRenderer } from "react-test-renderer";
 import RelativeDate from "..";
 
 describe("RelativeDate component", () => {
-  const date = Date.now();
+  const date = 1;
   let dom: ReactTestRenderer;
 
   beforeEach(() => {
     dom = create(<RelativeDate date={date} />);
   });
 
+  afterEach(() => {
+    dom.unmount();
+  });
+
   it("renders correctly", () => {
     expect(dom).toMatchSnapshot();
   });
 
-  // it("renders correctly on touch", () => {
-  //   const staticDom = create(<RelativeDate date={1553904714478} />);
-  //   staticDom.root.instance.toggleRelativeDate();
-  //   expect(staticDom).toMatchSnapshot();
-  // });
+  it("renders correctly on touch", () => {
+    const staticDom = create(<RelativeDate date={date} />);
+    staticDom.root.instance.toggleRelativeDate();
+    expect(staticDom).toMatchSnapshot();
+  });
 
-  // it("updates state touch", () => {
-  //   expect(dom.root.instance.state.showRelativeDate).toBe(true);
-  //   dom.root.instance.toggleRelativeDate();
-  //   expect(dom.root.instance.state.showRelativeDate).toBe(false);
-  //   dom.root.instance.toggleRelativeDate();
-  //   expect(dom.root.instance.state.showRelativeDate).toBe(true);
-  // });
+  it("updates state touch", () => {
+    expect(dom.root.instance.state.showRelativeDate).toBe(true);
+    dom.root.instance.toggleRelativeDate();
+    expect(dom.root.instance.state.showRelativeDate).toBe(false);
+    dom.root.instance.toggleRelativeDate();
+    expect(dom.root.instance.state.showRelativeDate).toBe(true);
+  });
 
-  // it("kills timer on unmount", () => {
-  //   const timer = dom.root.instance.timer;
-  //   expect(timer._onTimeout).not.toBeNull();
-  //   dom.unmount();
-  //   expect(timer._onTimeout).toBeNull();
-  // });
+  it("kills timer on unmount", () => {
+    const timer = dom.root.instance.timer;
+    expect(timer._onTimeout).not.toBeNull();
+    dom.unmount();
+    expect(timer._onTimeout).toBeNull();
+  });
 });
