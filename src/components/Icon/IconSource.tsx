435eca0e11c371915ec5d0113bfd2e84f55a73e5
Ethan Neff <ethan.neff@eneff.com>
2019-04-03 07:41:22 -0700

    fix: make icons work again

--- src/components/Icon/IconSource.tsx
+++ src/components/Icon/IconSource.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { ViewStyle } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
-import { Text } from "..";
 
 interface Props {
   name: string;
@@ -13,7 +12,6 @@ interface Props {
 export class IconSource extends React.PureComponent<Props> {
   public render() {
     const { name, size, color, style } = this.props;
-    return <Text title={name} />;
     return <Icon name={name} size={size} color={color} style={style} />;
   }
 }
