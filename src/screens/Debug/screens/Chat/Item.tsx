a2d428322b3d226a16944f0812937151e0f20a7c
Ethan Neff <ethan.neff@eneff.com>
2019-04-11 07:37:04 -0700

    fix: make it look more like slack

--- src/screens/Debug/screens/Chat/Item.tsx
+++ src/screens/Debug/screens/Chat/Item.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Image } from "react-native";
 import { Message } from ".";
 import { Button, RelativeDate, Text } from "../../../../components";
 import { Theme } from "../../../../utils";
+import moment from "moment";
 
 interface Props {
   item: Message;
@@ -10,28 +11,38 @@ interface Props {
 }
 
 export class Item extends React.PureComponent<Props> {
-  private styles = StyleSheet.create({
-    chat: {
-      backgroundColor: Theme.color.primary,
-      borderRadius: Theme.padding.p03,
-      padding: Theme.padding.p02
-    },
-    container: {
-      flexDirection: "row",
-      justifyContent: "flex-end",
-      paddingTop: Theme.padding.p02
-    }
-  });
+  image = require("../../../../assets/line-chart.png");
+  private styles = StyleSheet.create({});
 
   public render() {
     const { item, onDelete } = this.props;
     return (
-      <View key={item.id} style={this.styles.container}>
-        <View style={this.styles.chat}>
-          <Text title={item.message} body2 />
-          <RelativeDate date={item.createdAt} />
+      <View key={item.id} style={{ flexDirection: "row" }}>
+        <View style={{ width: 40 }}>
+          <Image
+            source={this.image}
+            style={{
+              resizeMode: "contain",
+              paddingTop: 30,
+              width: 20,
+              height: 20,
+              alignSelf: "center"
+            }}
+          />
         </View>
-        <Button title="D" onPress={onDelete(item)} />
+        <View style={{ flexShrink: 1 }}>
+          <View style={{ flexDirection: "row" }}>
+            <Text
+              title={item.createdBy}
+              h6
+              bold
+              style={{ paddingRight: Theme.padding.p02 }}
+            />
+            <Text title={moment(item.createdAt).format("LT")} subtitle1 />
+            {/* <RelativeDate date={item.createdAt} /> */}
+          </View>
+          <Text title={item.message} body1 style={{ paddingTop: 5 }} />
+        </View>
       </View>
     );
   }
