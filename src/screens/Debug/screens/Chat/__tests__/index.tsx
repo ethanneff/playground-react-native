7ecbc457affba748ab7ca8193f19edb95e23a394
Ethan Neff <ethan.neff@eneff.com>
2019-04-14 09:29:01 -0700

    fix: mock moment

--- src/screens/Debug/screens/Chat/__tests__/index.tsx
+++ src/screens/Debug/screens/Chat/__tests__/index.tsx
@@ -7,9 +7,12 @@ import mockDate from "mockdate";
 jest.mock("uuid", () => ({
   v4: jest.fn(() => 1)
 }));
+jest.mock("moment", () => () => ({
+  format: () => "2018–01–30T12:34:56+00:00",
+  subtract: () => jest.fn()
+}));
 it("renders correctly", () => {
   mockDate.set("2000-11-22");
-
   const dom = create(<MockProviderAndRouter Component={Chat} />).toJSON();
   expect(dom).toMatchSnapshot();
 });
