8bba48a5f35b93b713ced4546816b9078cdac272
Ethan Neff <ethan.neff@eneff.com>
2019-04-14 09:34:26 -0700

    fix: update tests to mock date.now

--- src/screens/Debug/screens/Chat/__tests__/index.tsx
+++ src/screens/Debug/screens/Chat/__tests__/index.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import { create } from "react-test-renderer";
 import { Chat } from "..";
 import { MockProviderAndRouter } from "../../../../../utils";
-import mockDate from "mockdate";
 
 jest.mock("uuid", () => ({
   v4: jest.fn(() => 1)
@@ -11,8 +10,9 @@ jest.mock("moment", () => () => ({
   format: () => "2018–01–30T12:34:56+00:00",
   subtract: () => jest.fn()
 }));
+
 it("renders correctly", () => {
-  mockDate.set("2000-11-22");
+  Date.now = jest.fn(() => 1555259619262);
   const dom = create(<MockProviderAndRouter Component={Chat} />).toJSON();
   expect(dom).toMatchSnapshot();
 });
