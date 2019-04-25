6850e5e5865636e08fa2343066d9e037c7cb8b0c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-25 13:42:49 +0000

    Deploy ethanneff/example to github.com/ethanneff/example.git:gh-pages

---
+++ precache-manifest.64fe7239e8b56260037c182047ad1940.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "429cf99ca6c5209ecd70bfc2cb7961b0",
+    "url": "/example/index.html"
+  },
+  {
+    "revision": "be965d02a59e068ba62d",
+    "url": "/example/static/js/2.49758aa1.chunk.js"
+  },
+  {
+    "revision": "7b00370528505c09d2fc",
+    "url": "/example/static/js/main.e16eb6f3.chunk.js"
+  },
+  {
+    "revision": "91c2a3b6d402a5254b11",
+    "url": "/example/static/js/runtime~main.3b33d9c4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/example/precache-manifest.6462325f7cbefbd41a60c73935fdb358.js"
+  "/example/precache-manifest.64fe7239e8b56260037c182047ad1940.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/example/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/example/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
