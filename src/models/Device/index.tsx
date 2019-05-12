e55e9ab3a55e61511451e5bb0e0ec318cc041d55
Ethan Neff <ethan.neff@eneff.com>
2019-05-11 23:37:35 -0700

    style: add spaces between functions

--- src/models/Device/index.tsx
+++ src/models/Device/index.tsx
@@ -62,13 +62,17 @@ export type DeviceActions = ActionType<
 export const onBatteryChange = createStandardAction("DEVICE/UPDATE_BATTERY")<
   number
 >();
+
 export const onFingerprintChange = createStandardAction(
   "DEVICE/UPDATE_FINGERPRINT"
 )<boolean>();
+
 export const onDeviceLoad = createStandardAction("DEVICE/LOAD")<DeviceState>();
+
 export const onNetworkChange = createStandardAction("DEVICE/UPDATE_NETWORK")<
   ConnectionInfo
 >();
+
 export const onDimensionChange = createStandardAction(
   "DEVICE/UPDATE_DIMENSION"
 )<DimensionsProps>();
@@ -76,26 +80,32 @@ export const onDimensionChange = createStandardAction(
 // selectors
 export const getLandscapeOrientation = (state: RootState): boolean =>
   state.device.windowDimensions.height < state.device.windowDimensions.width;
+
 export const getSmallestDimension = (state: RootState): number =>
   state.device.windowDimensions.height > state.device.windowDimensions.width
     ? state.device.windowDimensions.width
     : state.device.windowDimensions.height;
+
 export const getLargestDimension = (state: RootState): number =>
   state.device.windowDimensions.height > state.device.windowDimensions.width
     ? state.device.windowDimensions.height
     : state.device.windowDimensions.width;
+
 export const getWidth = (state: RootState): number =>
   state.device.windowDimensions.width;
+
 export const getHeight = (state: RootState): number =>
   state.device.windowDimensions.height;
 
 // reducers
 const windowDimensions = Dimensions.get("window");
 const screenDimensions = Dimensions.get("screen");
+
 export const deviceInitialState: DeviceState = {
   screenDimensions,
   windowDimensions
 };
+
 export const deviceReducer = (
   state: DeviceState = deviceInitialState,
   action: RootAction
