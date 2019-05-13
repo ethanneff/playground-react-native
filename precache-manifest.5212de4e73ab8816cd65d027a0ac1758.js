11dda8af83befd955fc84699877ac9dafbc42719
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-13 15:23:49 +0000

    Deploy ethanneff/example to github.com/ethanneff/example.git:gh-pages

---
+++ precache-manifest.5212de4e73ab8816cd65d027a0ac1758.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56625dc95303274068d8db0eec9fbc43",
+    "url": "/example/index.html"
+  },
+  {
+    "revision": "a2166b0daf5c0efb1f05",
+    "url": "/example/static/js/2.1fc91bb7.chunk.js"
+  },
+  {
+    "revision": "eb0fa0433c675ea6edfa",
+    "url": "/example/static/js/main.97159874.chunk.js"
+  },
+  {
+    "revision": "91c2a3b6d402a5254b11",
+    "url": "/example/static/js/runtime~main.3b33d9c4.js"
+  }
+]);
