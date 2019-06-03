fbbfeef4787307af3ef5ffeae0714f0013bd1134
Ethan Neff <ethan.neff@eneff.com>
2019-06-03 14:06:24 -0700

    style: relocate styles

--- src/screens/Landing/Walkthrough.tsx
+++ src/screens/Landing/Walkthrough.tsx
@@ -5,15 +5,6 @@ import { Text } from "../../components";
 import { getWidth } from "../../models/Device";
 import { RootState } from "../../containers";
 
-const styles = (width?: number, color?: string) =>
-  StyleSheet.create({
-    item: {
-      backgroundColor: color,
-      justifyContent: "center",
-      width
-    }
-  });
-
 interface StateProps {
   width: number;
 }
@@ -21,10 +12,19 @@ interface StateProps {
 type Props = StateProps;
 
 class Component extends React.PureComponent<Props> {
+  private styles = (width?: number, color?: string) =>
+    StyleSheet.create({
+      item: {
+        backgroundColor: color,
+        justifyContent: "center",
+        width
+      }
+    });
+
   public data = [
-    { key: 1, text: "hello", color: "white" },
-    { key: 2, text: "bob", color: "pink" },
-    { key: 3, text: "steve", color: "green" }
+    { key: 1, text: "hello", color: "lightgrey" },
+    { key: 2, text: "bob", color: "lightblue" },
+    { key: 3, text: "steve", color: "lightgreen" }
   ];
   public render() {
     const { width } = this.props;
@@ -35,7 +35,7 @@ class Component extends React.PureComponent<Props> {
         keyExtractor={item => String(item.key)}
         data={this.data}
         renderItem={({ item }) => (
-          <View style={styles(width, item.color).item}>
+          <View style={this.styles(width, item.color).item}>
             <Text title={item.text} center />
           </View>
         )}
