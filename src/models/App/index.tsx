86f300a3d5f77bc1bc40986a3c6b520fb7d597c2
Ethan Neff <ethan.neff@eneff.com>
2019-06-30 09:46:19 -0700

    fix: no undefineds for app store

--- src/models/App/index.tsx
+++ src/models/App/index.tsx
@@ -1,17 +1,19 @@
-import { AppStateStatus } from "react-native";
+import { AppState as AppActiveState, AppStateStatus } from "react-native";
+import DeviceInfo from "react-native-device-info";
+import VersionNumber from "react-native-version-number";
 import { ActionType, createStandardAction, getType } from "typesafe-actions";
 import { RootAction, RootState } from "../../containers";
 import { logout } from "../Auth";
 
-// interfaces
+/* INTERFACES */
 export interface AppState {
-  appVersion?: string;
-  buildVersion?: string;
-  bundleIdentifier?: string;
-  applicationName?: string;
-  buildNumber?: string;
-  version?: string;
-  readableVersion?: string;
+  appVersion: string;
+  buildVersion: string;
+  bundleIdentifier: string;
+  applicationName: string;
+  buildNumber: string;
+  version: string;
+  readableVersion: string;
   status: AppStateStatus;
   keyboardVisible: boolean;
 }
@@ -34,11 +36,19 @@ export const getAppStatus = (state: RootState): AppStateStatus =>
 export const getKeyboardVisible = (state: RootState): boolean =>
   state.app.keyboardVisible;
 
-// reducers
+/* REDUCERS */
 export const appInitialState: AppState = {
+  appVersion: VersionNumber.appVersion,
+  applicationName: DeviceInfo.getApplicationName(),
+  buildNumber: DeviceInfo.getBuildNumber(),
+  buildVersion: VersionNumber.buildVersion,
+  bundleIdentifier: DeviceInfo.getBundleId() || VersionNumber.bundleIdentifier,
   keyboardVisible: false,
-  status: "inactive"
+  readableVersion: DeviceInfo.getReadableVersion(),
+  status: AppActiveState.currentState,
+  version: DeviceInfo.getVersion()
 };
+
 export function appReducer(
   state: AppState = appInitialState,
   action: RootAction
