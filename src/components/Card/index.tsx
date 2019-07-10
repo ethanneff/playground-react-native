0f0b334c0e3c19e2aaa9187dbd4b1425e6bc1efb
Ethan Neff <ethan.neff@eneff.com>
2019-07-10 11:25:41 -0700

    feat: add longpress and selected to card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -6,30 +6,45 @@ const styles = StyleSheet.create({
   containerStyle: {
     backgroundColor: Theme.color.background,
     elevation: 1,
-    margin: Theme.padding.p02,
+    marginHorizontal: Theme.padding.p04,
+    marginVertical: Theme.padding.p02,
     padding: Theme.padding.p04,
     position: "relative",
     shadowColor: Theme.color.dark,
-    shadowOffset: { height: 4, width: 0 },
+    shadowOffset: {
+      height: 4,
+      width: 0
+    },
     shadowOpacity: 0.6,
     shadowRadius: 6,
     zIndex: 1
+  },
+  selected: {
+    backgroundColor: Theme.color.primary
   }
 });
 
 interface Props {
   style?: ViewStyle;
+  selected?: boolean;
   onPress?(): void;
+  onLongPress?(): void;
 }
 
 export class Card extends React.PureComponent<Props> {
   private touchOpacity = 0.3;
   public render() {
-    const { style, children, onPress } = this.props;
+    const { style, children, onPress, selected, onLongPress } = this.props;
+    const containerStyles = [
+      styles.containerStyle,
+      selected ? styles.selected : undefined,
+      style
+    ];
     return (
       <TouchableOpacity
-        style={[styles.containerStyle, style]}
+        style={containerStyles}
         onPress={onPress && onPress}
+        onLongPress={onLongPress && onLongPress}
         activeOpacity={onPress ? this.touchOpacity : 1}
       >
         {children}
