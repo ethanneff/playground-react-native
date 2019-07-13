80b3bbda2ab558227eed788ae4fd8f268dd138d0
Ethan Neff <ethan.neff@eneff.com>
2019-07-13 09:00:03 -0700

    refactor: make settings use a list

--- src/apps/Portfolio/screens/Settings/index.tsx
+++ src/apps/Portfolio/screens/Settings/index.tsx
@@ -4,27 +4,46 @@ import {
   navigate,
   NavigationScreen,
   changeTheme,
-  ColorTheme
+  ColorTheme,
+  getCurrentColor
 } from "../../../../models";
-import { useDispatch } from "react-redux";
+import { useRootDispatch, useRootSelector } from "../../../../utils";
+import { FlatList, View } from "react-native";
 
 export const Settings = () => {
-  const dispatch = useDispatch();
+  const dispatch = useRootDispatch();
+  const themes = Object.values(ColorTheme);
+  const color = useRootSelector(state => getCurrentColor(state));
+  const currentTheme = useRootSelector(state => state.theme.currentColor);
+  const navBack = () => dispatch(navigate(NavigationScreen.PortfolioLanding));
+  const themePress = (theme: ColorTheme) => () => dispatch(changeTheme(theme));
+
+  console.log("render");
   return (
-    <Screen>
+    <Screen onLeftPress={navBack}>
       <Text h1 center title="Settings" />
-      <Button
-        title="go home"
-        onPress={() => dispatch(navigate(NavigationScreen.PortfolioLanding))}
-      />
-      <Button
-        title="light"
-        onPress={() => dispatch(changeTheme(ColorTheme.Light))}
-      />
-      <Button
-        title="dark"
-        onPress={() => dispatch(changeTheme(ColorTheme.Dark))}
-      />
+      <View
+        style={{
+          backgroundColor: color.surface,
+          flexDirection: "row"
+        }}
+      >
+        <Text title="theme" />
+        <FlatList
+          horizontal
+          keyExtractor={item => item}
+          data={themes}
+          renderItem={({ item }) => (
+            <Button
+              key={item}
+              title={item}
+              onPress={themePress(item)}
+              contained={currentTheme === item}
+              secondary={currentTheme !== item}
+            />
+          )}
+        />
+      </View>
     </Screen>
   );
 };
