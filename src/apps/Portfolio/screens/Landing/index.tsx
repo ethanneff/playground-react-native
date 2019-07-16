f86a61264f6759f54d4e2a23db77332078dc1811
Ethan Neff <ethan.neff@eneff.com>
2019-07-15 20:56:58 -0700

    refactor: landing page to hooks

--- src/apps/Portfolio/screens/Landing/index.tsx
+++ src/apps/Portfolio/screens/Landing/index.tsx
@@ -1,61 +1,41 @@
 import * as React from "react";
 import { StyleSheet, View } from "react-native";
-import { connect } from "react-redux";
 import { Button, Screen, Text } from "../../../../components";
-import { navigate, NavigationScreen, showModal } from "../../../../models";
+import { navigate, NavigationScreen } from "../../../../models";
+import { useRootDispatch } from "../../../../utils";
 import { Walkthrough } from "./Walkthrough";
 
-interface DispatchProps {
-  navigate: typeof navigate;
-  showModal: typeof showModal;
-}
-
-type Props = DispatchProps;
-
-class Container extends React.PureComponent<Props> {
-  private styles = StyleSheet.create({
+export const Landing = () => {
+  const dispatch = useRootDispatch();
+  const styles = StyleSheet.create({
     row: { flexDirection: "row", justifyContent: "space-around" }
   });
-  public render() {
-    return (
-      <Screen>
-        <Text h1 title="Landing" center />
-        <Walkthrough />
-        <View style={this.styles.row}>
-          <Button
-            title="Login"
-            onPress={this.nav(NavigationScreen.PortfolioLogin)}
-          />
-          <Button
-            title="Main"
-            onPress={this.nav(NavigationScreen.PortfolioMain)}
-          />
-          <Button title="Debug" onPress={this.nav(NavigationScreen.Debug)} />
-          <Button
-            title="Settings"
-            onPress={this.nav(NavigationScreen.PortfolioSettings)}
-          />
-        </View>
-        <View style={this.styles.row}>
-          <Button
-            title="Checklist"
-            onPress={this.nav(NavigationScreen.ChecklistsLists)}
-          />
-          <Button
-            title="CantHurtMe"
-            onPress={this.nav(NavigationScreen.CantHurtMeHome)}
-          />
-        </View>
-      </Screen>
-    );
-  }
 
-  private nav = (to: NavigationScreen) => () => this.props.navigate(to);
-}
+  const nav = (to: NavigationScreen) => () => dispatch(navigate(to));
 
-const mapDispatchToProps: DispatchProps = { navigate, showModal };
-
-export const Landing = connect(
-  null,
-  mapDispatchToProps
-)(Container);
+  return (
+    <Screen>
+      <Text h1 title="Landing" center />
+      <Walkthrough />
+      <View style={styles.row}>
+        <Button title="Login" onPress={nav(NavigationScreen.PortfolioLogin)} />
+        <Button title="Main" onPress={nav(NavigationScreen.PortfolioMain)} />
+        <Button title="Debug" onPress={nav(NavigationScreen.Debug)} />
+        <Button
+          title="Settings"
+          onPress={nav(NavigationScreen.PortfolioSettings)}
+        />
+      </View>
+      <View style={styles.row}>
+        <Button
+          title="Checklist"
+          onPress={nav(NavigationScreen.ChecklistsLists)}
+        />
+        <Button
+          title="CantHurtMe"
+          onPress={nav(NavigationScreen.CantHurtMeHome)}
+        />
+      </View>
+    </Screen>
+  );
+};
