0fe5a1494a086f6013572e4bd3b6e03142755a3a
Ethan Neff <ethan.neff@eneff.com>
2019-07-15 20:56:46 -0700

    fix: clean up settings

--- src/apps/Portfolio/screens/Settings/index.tsx
+++ src/apps/Portfolio/screens/Settings/index.tsx
@@ -1,19 +1,17 @@
 import * as React from "react";
+import { FlatList, View } from "react-native";
 import { Button, Screen, Text } from "../../../../components";
 import {
-  navigate,
-  NavigationScreen,
   changeTheme,
   ColorTheme,
-  getCurrentColor
+  navigate,
+  NavigationScreen
 } from "../../../../models";
 import { useRootDispatch, useRootSelector } from "../../../../utils";
-import { FlatList, View } from "react-native";
 
 export const Settings = () => {
   const dispatch = useRootDispatch();
   const themes = Object.values(ColorTheme);
-  const color = useRootSelector(state => getCurrentColor(state));
   const currentTheme = useRootSelector(state => state.theme.currentColor);
   const navBack = () => dispatch(navigate(NavigationScreen.PortfolioLanding));
   const themePress = (theme: ColorTheme) => () => dispatch(changeTheme(theme));
@@ -21,27 +19,23 @@ export const Settings = () => {
   return (
     <Screen onLeftPress={navBack}>
       <Text h1 center title="Settings" />
-      <View
-        style={{
-          backgroundColor: color.surface,
-          flexDirection: "row"
-        }}
-      >
-        <Text title="theme" />
-        <FlatList
-          horizontal
-          keyExtractor={item => item}
-          data={themes}
-          renderItem={({ item }) => (
-            <Button
-              key={item}
-              title={item}
-              onPress={themePress(item)}
-              contained={currentTheme === item}
-              secondary={currentTheme !== item}
-            />
-          )}
-        />
+      <FlatList
+        ListHeaderComponent={() => <Button disable title="Theme" />}
+        horizontal
+        keyExtractor={item => item}
+        data={themes}
+        renderItem={({ item }) => (
+          <Button
+            key={item}
+            title={item}
+            onPress={themePress(item)}
+            contained={currentTheme === item}
+            secondary={currentTheme !== item}
+          />
+        )}
+      />
+      <View style={{ flex: 1 }}>
+        <Text title="hello" centerVertically center />
       </View>
     </Screen>
   );
