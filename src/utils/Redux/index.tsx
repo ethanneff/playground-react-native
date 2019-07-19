3d5f169b563a8f82aa341ff8ddbbb7c7c58bf5d3
Ethan Neff <ethan.neff@eneff.com>
2019-07-19 08:29:19 -0700

    fix: correct typings for useRootDispatch

--- src/utils/Redux/index.tsx
+++ src/utils/Redux/index.tsx
@@ -1,6 +1,6 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { Dispatch } from "redux";
-import { RootState } from "../../containers";
+import { RootAction, RootState } from "../../containers";
 
 export const useRootSelector: TypedUseSelectorHook<RootState> = useSelector;
-export const useRootDispatch: () => Dispatch = useDispatch;
+export const useRootDispatch: () => Dispatch<RootAction> = useDispatch();
