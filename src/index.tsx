5c7a47c8cfc14c49705234e2365b14447fc03fb7
Ethan Neff <ethan.neff@eneff.com>
2019-08-11 20:46:01 -0700

    fix: remove yellowbox

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-import { AppRegistry, Platform, YellowBox } from "react-native";
+import { AppRegistry, Platform } from "react-native";
 import { Provider } from "react-redux";
 import { App, store } from "./containers";
 import { Config } from "./utils";
-
-// TODO: remove
-YellowBox.ignoreWarnings(["NetInfo", "Async Storage", "Battery state"]);
 
 export const Launcher = () => (
   <Provider store={store}>
