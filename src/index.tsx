7593432b8eb723e23b6c92a0f05df5a227d56619
Ethan Neff <ethan.neff@eneff.com>
2019-08-11 20:46:11 -0700

    fix: add memo to launcher

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { memo } from "react";
 import { AppRegistry, Platform } from "react-native";
 import { Provider } from "react-redux";
 import { App, store } from "./containers";
 import { Config } from "./utils";
 
-export const Launcher = () => (
+export const Launcher = memo(() => (
   <Provider store={store}>
     <App />
   </Provider>
-);
+));
 
 AppRegistry.registerComponent(Config.app.name, () => Launcher);
 if (Platform.OS === Config.os.web) {
