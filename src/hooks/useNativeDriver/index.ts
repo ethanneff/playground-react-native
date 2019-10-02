856822f646125bc6b55ee13ee9bf3f064e89f414
Ethan Neff <ethan.neff@eneff.com>
2019-10-01 22:09:53 -0700

    feat: create useNativeDriver hook

---
+++ src/hooks/useNativeDriver/index.ts
@@ -0,0 +1,12 @@
+import { Platform } from "react-native";
+import { Config, useRootSelector } from "../../utils";
+
+export const useNativeDriver = () => {
+  const emulator = useRootSelector(state => state.device.isEmulator);
+
+  return !(
+    process.env.JEST_WORKER_ID || // eslint-disable-line no-undef
+    emulator ||
+    Platform.OS === Config.os.web
+  );
+};
