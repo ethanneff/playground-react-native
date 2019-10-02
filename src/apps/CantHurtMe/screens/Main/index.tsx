8198ca26e9a7422f1fee61299a8084b6dff8ab52
Ethan Neff <ethan.neff@eneff.com>
2019-10-01 22:04:48 -0700

    fix: remove redux modals for dialogs

--- src/apps/CantHurtMe/screens/Main/index.tsx
+++ src/apps/CantHurtMe/screens/Main/index.tsx
@@ -1,66 +1,84 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 import { FlatList, View } from "react-native";
-import { Button, Card, Screen } from "../../../../components";
-import {
-  getLandscapeOrientation,
-  NavigationModal,
-  showModal
-} from "../../../../models";
-import { Theme, useRootDispatch, useRootSelector } from "../../../../utils";
+import { Button, Card, Dialog, Screen } from "../../../../components";
+import { getLandscapeOrientation } from "../../../../models";
+import { Theme, useRootSelector } from "../../../../utils";
 import { DailyProgress, Header, ProfileLevel } from "../../components";
 import { app } from "../../data";
-import { useColor, useNav } from "../../../../hooks";
+import { useNav } from "../../../../hooks";
 import { Item } from "./Item";
 
+const initialState = { settings: false, profile: false };
+
 export default memo(function CantHurtMeMain() {
-  const dispatch = useRootDispatch();
+  const [showModal, setShowModal] = useState(initialState);
   const landscape = useRootSelector(state => getLandscapeOrientation(state));
-  const color = useColor();
-  const nav = useNav();
-  const modal = (to: NavigationModal) => () => dispatch(showModal(to));
-  const keyExtractor = (id: string) => app.goals.byId[id].id;
-  const renderFooter = () => (
-    <View style={{ paddingBottom: Theme.padding.p04 }} />
-  );
   const columns = landscape ? 4 : 2;
+  const nav = useNav();
+  const keyExtractor = (id: string) => app.goals.byId[id].id;
+
+  const handleModalBackgroundPress = () => setShowModal(initialState);
+  const handleProfilePress = () =>
+    setShowModal({ ...initialState, profile: true });
+  const handleSettingsPress = () =>
+    setShowModal({ ...initialState, settings: true });
 
   return (
-    <Screen
-      onLeftPress={nav.to("portfolioLanding")}
-      title="Can't Hurt Me"
-      style={{
-        backgroundColor: color.background,
-        padding: Theme.padding.p02
-      }}
-    >
-      <Header title="Progress" />
-      <Card>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between"
-          }}
-        >
-          <ProfileLevel onPress={modal(NavigationModal.CantHurtMeProfile)} />
-          <Button
-            icon="settings"
-            onPress={modal(NavigationModal.CantHurtMeConfigs)}
-            neutral
-            fab
-            center
-          />
-        </View>
-        <DailyProgress />
-      </Card>
-      <Header title="Challenges" />
-      <FlatList
-        keyExtractor={keyExtractor}
-        key={columns}
-        data={app.goals.orderById}
-        renderItem={Item}
-        numColumns={columns}
-        ListFooterComponent={renderFooter}
-      />
-    </Screen>
+    <>
+      <Screen
+        border
+        gutter
+        onLeftPress={nav.to("portfolioLanding")}
+        title="Can't Hurt Me"
+      >
+        <FlatList
+          keyExtractor={keyExtractor}
+          key={columns}
+          data={app.goals.orderById}
+          renderItem={Item}
+          numColumns={columns}
+          ListHeaderComponent={
+            <>
+              <Header title="Progress" />
+              <Card>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between"
+                  }}
+                >
+                  <ProfileLevel onPress={handleProfilePress} />
+                  <Button
+                    icon="settings"
+                    onPress={handleSettingsPress}
+                    neutral
+                    fab
+                    center
+                  />
+                </View>
+                <DailyProgress />
+              </Card>
+              <Header title="Challenges" />
+            </>
+          }
+          ListFooterComponent={
+            <View style={{ paddingBottom: Theme.padding.p04 }} />
+          }
+        />
+      </Screen>
+      {showModal.profile && 
+        <Dialog
+          duration={2000}
+          title="profile"
+          onBackgroundPress={handleModalBackgroundPress}
+        />
+      }
+      {showModal.settings && 
+        <Dialog
+          title="settings"
+          onBackgroundPress={handleModalBackgroundPress}
+        />
+      }
+    </>
   );
 });
