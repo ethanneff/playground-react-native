9460c3ef66ddf99996c0d4b4da3ed7418886852f
Ethan Neff <ethan.neff@eneff.com>
2019-10-08 20:35:36 -0700

    refactor: convert dialog to memo

--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -26,136 +26,134 @@ interface OwnProps {
 type Props = OwnProps;
 
 const fadeDuration = 200;
-export const Dialog: React.FC<Props> = memo(
-  ({
-    onCancelButtonPress,
-    onConfirmButtonPress,
-    onBackgroundPress,
-    confirmButtonText = "confirm",
-    cancelButtonText = "cancel",
-    message,
-    title,
-    duration,
-    testID
-  }) => {
-    const nativeDriver = useNativeDriver();
-    const color = useColor();
-    const styles = StyleSheet.create({
-      buttonContainer: {
-        flexDirection: "row",
-        justifyContent: "center",
-        paddingTop: Theme.padding.p13
-      },
-      cancel: {
-        flex: 1,
-        marginRight: Theme.padding.p02
-      },
-      confirm: {
-        flex: 1,
-        marginLeft: Theme.padding.p02
-      },
-      container: {
-        bottom: 0,
-        elevation: 100,
-        left: 0,
-        position: "absolute",
-        right: 0,
-        top: 0,
-        zIndex: 100
-      },
-      modal: {
-        maxWidth: 500,
-        width: "100%"
-      },
-      overlay: {
-        alignItems: "center",
-        backgroundColor: colorWithOpacity(color.black),
-        flex: 1,
-        flexDirection: "column",
-        justifyContent: "center",
-        padding: Theme.padding.p08
-      },
-      title: {
-        paddingBottom: Theme.padding.p03
+export const Dialog: React.FC<Props> = memo(function Dialog({
+  onCancelButtonPress,
+  onConfirmButtonPress,
+  onBackgroundPress,
+  confirmButtonText = "confirm",
+  cancelButtonText = "cancel",
+  message,
+  title,
+  duration,
+  testID
+}) {
+  const nativeDriver = useNativeDriver();
+  const color = useColor();
+  const styles = StyleSheet.create({
+    buttonContainer: {
+      flexDirection: "row",
+      justifyContent: "center",
+      paddingTop: Theme.padding.p13
+    },
+    cancel: {
+      flex: 1,
+      marginRight: Theme.padding.p02
+    },
+    confirm: {
+      flex: 1,
+      marginLeft: Theme.padding.p02
+    },
+    container: {
+      bottom: 0,
+      elevation: 100,
+      left: 0,
+      position: "absolute",
+      right: 0,
+      top: 0,
+      zIndex: 100
+    },
+    modal: {
+      maxWidth: 500,
+      width: "100%"
+    },
+    overlay: {
+      alignItems: "center",
+      backgroundColor: colorWithOpacity(color.black),
+      flex: 1,
+      flexDirection: "column",
+      justifyContent: "center",
+      padding: Theme.padding.p08
+    },
+    title: {
+      paddingBottom: Theme.padding.p03
+    }
+  });
+  const fade = new Animated.Value(0);
+  const opacity = fade;
+  const twoButtons =
+    Boolean(onConfirmButtonPress) && Boolean(onCancelButtonPress);
+  const containerStyle = [styles.container, { opacity }];
+  const cancelButtonStyle = [twoButtons ? styles.cancel : undefined];
+  const confirmButtonStyle = [twoButtons ? styles.confirm : undefined];
+
+  // TODO: animation is not clean on devices
+  const animate = useCallback(
+    (toValue: number) => {
+      return new Promise(resolve => {
+        Animated.timing(fade, {
+          duration: fadeDuration,
+          toValue,
+          useNativeDriver: nativeDriver
+        }).start(resolve);
+      });
+    },
+    [fade, nativeDriver]
+  );
+
+  const dismiss = useCallback(
+    (callback?: () => void) => async () => {
+      if (!callback) {
+        return;
       }
-    });
-    const fade = new Animated.Value(0);
-    const opacity = fade;
-    const twoButtons =
-      Boolean(onConfirmButtonPress) && Boolean(onCancelButtonPress);
-    const containerStyle = [styles.container, { opacity }];
-    const cancelButtonStyle = [twoButtons ? styles.cancel : undefined];
-    const confirmButtonStyle = [twoButtons ? styles.confirm : undefined];
+      await animate(0);
+      callback();
+    },
+    [animate]
+  );
 
-    // TODO: animation is not clean on devices
-    const animate = useCallback(
-      (toValue: number) => {
-        return new Promise(resolve => {
-          Animated.timing(fade, {
-            duration: fadeDuration,
-            toValue,
-            useNativeDriver: nativeDriver
-          }).start(resolve);
-        });
-      },
-      [fade, nativeDriver]
-    );
+  useEffect(() => {
+    animate(1);
+    if (!duration) {
+      return () => undefined;
+    }
+    const alertTimeout = setTimeout(dismiss(onBackgroundPress), duration);
+    return () => {
+      clearTimeout(alertTimeout);
+    };
+  }, [animate, duration, dismiss, onBackgroundPress]);
 
-    const dismiss = useCallback(
-      (callback?: () => void) => async () => {
-        if (!callback) {
-          return;
-        }
-        await animate(0);
-        callback();
-      },
-      [animate]
-    );
-
-    useEffect(() => {
-      animate(1);
-      if (!duration) {
-        return () => undefined;
-      }
-      const alertTimeout = setTimeout(dismiss(onBackgroundPress), duration);
-      return () => {
-        clearTimeout(alertTimeout);
-      };
-    }, [animate, duration, dismiss, onBackgroundPress]);
-
-    return (
-      <Animated.View style={containerStyle}>
-        <TouchableWithoutFeedback
-          testID={testID}
-          onPress={dismiss(onBackgroundPress)}
-        >
-          <View style={styles.overlay}>
-            <TouchableWithoutFeedback onPress={undefined}>
-              <Card style={styles.modal}>
-                <Text title={title} h3 style={styles.title} />
-                <Text title={message} />
-                <View style={styles.buttonContainer}>
-                  <Button
-                    hidden={!onCancelButtonPress}
-                    title={cancelButtonText}
-                    onPress={dismiss(onCancelButtonPress)}
-                    buttonStyle={cancelButtonStyle}
-                    half={!twoButtons}
-                  />
-                  <Button
-                    hidden={!onConfirmButtonPress}
-                    title={confirmButtonText}
-                    onPress={dismiss(onConfirmButtonPress)}
-                    buttonStyle={confirmButtonStyle}
-                    secondary
-                    half={!twoButtons}
-                  />
-                </View>
-              </Card>
-            </TouchableWithoutFeedback>
-          </View>
-        </TouchableWithoutFeedback>
-      </Animated.View>
-    );
-  }
-);
+  return (
+    <Animated.View style={containerStyle}>
+      <TouchableWithoutFeedback
+        testID={testID}
+        onPress={dismiss(onBackgroundPress)}
+      >
+        <View style={styles.overlay}>
+          <TouchableWithoutFeedback onPress={undefined}>
+            <Card style={styles.modal}>
+              <Text title={title} h3 style={styles.title} />
+              <Text title={message} />
+              <View style={styles.buttonContainer}>
+                <Button
+                  hidden={!onCancelButtonPress}
+                  title={cancelButtonText}
+                  onPress={dismiss(onCancelButtonPress)}
+                  buttonStyle={cancelButtonStyle}
+                  half={!twoButtons}
+                />
+                <Button
+                  hidden={!onConfirmButtonPress}
+                  title={confirmButtonText}
+                  onPress={dismiss(onConfirmButtonPress)}
+                  buttonStyle={confirmButtonStyle}
+                  secondary
+                  half={!twoButtons}
+                />
+              </View>
+            </Card>
+          </TouchableWithoutFeedback>
+        </View>
+      </TouchableWithoutFeedback>
+    </Animated.View>
+  );
+});
