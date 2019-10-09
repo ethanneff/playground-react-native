e104659a0ad6d37da14d90bcf501e43930602109
Ethan Neff <ethan.neff@eneff.com>
2019-10-08 20:26:19 -0700

    fix: add access to dimension redux on web

--- src/containers/App/hooks.web.ts
+++ src/containers/App/hooks.web.ts
@@ -1,6 +1,26 @@
+import { Dimensions } from "react-native";
+import { DimensionsProps } from "./../../models/Device/index";
+import { useRootDispatch } from "../../utils";
+import { useCallback, useEffect } from "react";
+import { updateDimension } from "../../models";
+
 export const useAppLoad = () => undefined;
 export const useNetInfo = () => undefined;
 export const useDeviceInfo = () => undefined;
-export const useDimensions = () => undefined;
+export const useDimensions = () => {
+  const dispatch = useRootDispatch();
+
+  const handleChange = useCallback(
+    (change: DimensionsProps) => dispatch(updateDimension(change)),
+    [dispatch]
+  );
+
+  useEffect(() => {
+    Dimensions.addEventListener("change", handleChange);
+    return () => {
+      Dimensions.removeEventListener("change", handleChange);
+    };
+  }, [handleChange, dispatch]);
+};
 export const useAppState = () => undefined;
 export const useKeyboard = () => undefined;
