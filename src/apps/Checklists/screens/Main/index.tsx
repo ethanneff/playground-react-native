b883a031eec69fd8bf14b056016dce1ad169dac3
Ethan Neff <ethan.neff@eneff.com>
2019-11-25 05:57:05 +0800

    fix: convert checklists to functional

--- src/apps/Checklists/screens/Main/index.tsx
+++ src/apps/Checklists/screens/Main/index.tsx
@@ -1,63 +1,51 @@
-import React from "react";
+import React, { memo } from "react";
 import { FlatList, View } from "react-native";
-import { connect } from "react-redux";
-import { Button, Screen, Text } from "../../../../components";
-import { RootState } from "../../../../containers";
-import { NavigationScreen, navigate } from "../../../../models";
+import { Button, Screen } from "../../../../components";
+import { useNav } from "../../../../hooks";
 
-interface DispatchProps {
-  navigate: typeof navigate;
-}
-type Props = DispatchProps;
+type Items = {
+  id: string;
+  title: string;
+}[];
 
-class Component extends React.PureComponent<Props> {
-  private items = [
-    {
-      id: "1",
-      title: "bob"
-    },
-    {
-      id: "2",
-      title: "steve"
-    },
-    {
-      id: "3",
-      title: "jill"
-    }
-  ];
-  public render() {
-    return (
-      <Screen onLeftPress={this.nav("portfolioLanding")}>
-        <Text title="checklists" />
-        <FlatList
-          keyExtractor={item => item.id}
-          data={this.items}
-          renderItem={({ item }) => 
-            <View style={{ flexDirection: "row" }}>
-              <Button
-                contained
-                title={item.id}
-                onPress={this.nav("checklistsList")}
-              />
-            </View>
-          }
-        />
-        <Button
-          right
-          contained
-          fab
-          icon="plus"
-          onPress={this.nav("checklistsListCreate")}
-        />
-      </Screen>
-    );
+const items: Items = [
+  {
+    id: "1",
+    title: "bob"
+  },
+  {
+    id: "2",
+    title: "steve"
+  },
+  {
+    id: "3",
+    title: "jill"
   }
-  private nav = (to: NavigationScreen) => () => this.props.navigate(to);
-}
-
-export default connect(
-  (state: RootState) => ({
-    state
-  }),
-  { navigate }
-)(Component);
+];
+export default memo(function Checklists() {
+  const nav = useNav();
+  return (
+    <Screen onLeftPress={nav.to("portfolioLanding")} title="checklists">
+      <FlatList
+        keyExtractor={item => item.id}
+        data={items}
+        renderItem={({ item }) => (
+          <View style={{ flexDirection: "row" }}>
+            <Button
+              contained
+              title={item.id}
+              onPress={nav.to("checklistsList")}
+            />
+          </View>
+        )}
+      />
+      <Button
+        right
+        contained
+        fab
+        icon="plus"
+        onPress={nav.to("checklistsListCreate")}
+      />
+    </Screen>
+  );
+});
