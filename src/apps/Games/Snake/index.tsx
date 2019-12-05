0a0739b02c635dda732d0fcc1db3ddab4ff77cad
Ethan Neff <ethan.neff@eneff.com>
2019-12-05 08:38:25 -0800

    feat: add snake controls

--- src/apps/Games/Snake/index.tsx
+++ src/apps/Games/Snake/index.tsx
@@ -1,20 +1,33 @@
-import React, { memo } from "react";
-import { StyleSheet, View } from "react-native";
-import { Screen } from "../../../components";
+import React, { memo, useState } from "react";
+import { View, PanResponder, PanResponderInstance } from "react-native";
+import { Screen, Text } from "../../../components";
 import { useColor, useNav } from "../../../hooks";
+
+type Direction = "left" | "right" | "up" | "down";
 
 export default memo(function Snake() {
   const color = useColor();
   const nav = useNav();
-  const styles = StyleSheet.create({
-    container: {
-      backgroundColor: color.background
+  const [direction, setDirection] = useState<Direction>("up");
+
+  const panGesture: PanResponderInstance = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderRelease: (_, g) => {
+      if (Math.abs(g.dx) >= Math.abs(g.dy)) {
+        setDirection(g.dx >= 0 ? "right" : "left");
+      } else {
+        setDirection(g.dy >= 0 ? "down" : "up");
+      }
     }
   });
+
   return (
-    <Screen onLeftPress={nav.to("portfolioLanding")}>
-      <View style={styles.container}>
-        <></>
+    <Screen onLeftPress={nav.to("portfolioLanding")} title="snake">
+      <View
+        style={{ flex: 1, backgroundColor: color.success }}
+        {...panGesture.panHandlers}
+      >
+        <Text title={direction} />
       </View>
     </Screen>
   );
