41d124b5081191acf6a0975e1522f43edef55956
Ethan Neff <ethan.neff@eneff.com>
2019-12-06 09:46:13 -0800

    feat: add snake cell

---
+++ src/apps/Games/Snake/Cell.tsx
@@ -0,0 +1,27 @@
+import React, { memo } from "react";
+import { useColor } from "../../../hooks";
+import { useRootSelector } from "../../../utils";
+import { View } from "react-native";
+import { getSmallestDimension } from "../../../models";
+
+interface CellProps {
+  value: number;
+  length: number;
+}
+
+export const Cell = memo(function Cell({ value, length }: CellProps) {
+  const color = useColor();
+  const width = useRootSelector(getSmallestDimension) / length;
+  const backgroundColor =
+    value === 0 ? color.surface : value === 1 ? color.success : color.danger;
+  return (
+    <View
+      style={{
+        flex: 1,
+        width,
+        height: width,
+        backgroundColor
+      }}
+    />
+  );
+});
