59aba5967ad83cc6c325322eca948ee3bb29bdfa
Ethan Neff <ethan.neff@eneff.com>
2019-12-06 09:47:13 -0800

    fix: relocated logic to isolated file

---
+++ src/apps/Games/Snake/utils.ts
@@ -0,0 +1,45 @@
+export type BoardObject = Array<Array<0 | 1 | 2>>;
+
+export const generateRandom = (size: number) =>
+  Math.floor(Math.random() * size);
+
+export const addFood = (
+  board: BoardObject,
+  used: { [key: string]: boolean } = {}
+): boolean => {
+  const x = generateRandom(board.length);
+  const y = generateRandom(board.length);
+
+  const combo = `${x}${y}`;
+  if (Object.keys(used).length === board.length) {
+    return false;
+  }
+  if (used.hasOwnProperty(combo) || board[x][y] !== 0) {
+    used[combo] = true;
+    addFood(board, used);
+    return false;
+  }
+  board[y][x] = 2;
+  return true;
+};
+
+export const addStarting = (board: BoardObject) => {
+  const center = Math.floor(board.length / 2);
+  board[center][center] = 1;
+};
+
+export const generateBoard = (size: number) => {
+  let board: BoardObject = [];
+  for (let i = 0; i < size; i++) {
+    board[i] = [];
+    for (let j = 0; j < size; j++) {
+      board[i][j] = 0;
+    }
+  }
+  return board;
+};
+
+export const nextSnakePosition = () => undefined;
+export const collision = () => undefined;
+export const endGame = () => undefined;
+export const eat = () => undefined;
