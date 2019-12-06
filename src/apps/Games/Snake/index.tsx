0fe96e1d6c901ae7d15feab123358b8059f40e60
Ethan Neff <ethan.neff@eneff.com>
2019-12-06 10:14:19 -0800

    fix: allow endgame

--- src/apps/Games/Snake/index.tsx
+++ src/apps/Games/Snake/index.tsx
@@ -1,6 +1,6 @@
-import React, { memo } from "react";
+import React, { memo, useState, useEffect, useCallback } from "react";
 import { View } from "react-native";
-import { Screen } from "../../../components";
+import { Screen, Text, Button, Modal } from "../../../components";
 import { useColor, useNav } from "../../../hooks";
 import {
   BoardObject,
@@ -8,51 +8,77 @@ import {
   generateBoard,
   addFood,
   collision,
-  endGame,
   nextSnakePosition,
   eat
 } from "./utils";
 import { Board } from "./Board";
-import { useTick } from "./useTick";
+import { Frame, useGameLoop } from "./useGameLoop";
 import { useGesture, Direction } from "./useGesture";
 
 export default memo(function Snake() {
   const color = useColor();
   const nav = useNav();
+  const [board, setBoard] = useState<BoardObject>(generateBoard(size));
+  const [endGame, setEndGame] = useState(false);
   const size = 20;
   let direction: Direction = "up";
   const gesture = useGesture(value => {
     direction = value;
   });
 
-  useTick(value => {
-    updateGame();
-    console.log(direction);
+  const gameLoop = useGameLoop(frame => {
+    updateGame(frame);
   });
 
-  let board: BoardObject = generateBoard(size);
-  addStarting(board);
-  addFood(board);
+  const updateGame = useCallback(
+    (frame: Frame) => {
+      if (endGame) return;
+      console.log(frame, direction);
+      const next = nextSnakePosition(direction, board);
+      if (collision(next) || frame.count >= 100) {
+        setEndGame(true);
+        gameLoop.stop();
+        return;
+      }
+      if (eat()) {
+        addFood(board);
+      }
+    },
+    [gameLoop, endGame]
+  );
 
-  const updateGame = () => {
-    const next = nextSnakePosition(direction, board);
-    if (collision(next)) {
-      endGame();
-      return;
-    }
-    if (eat()) {
-      addFood(board);
-    }
+  const startGame = () => {
+    const board = generateBoard(size);
+    addStarting(board);
+    addFood(board);
+    setEndGame(false);
+    setBoard(board);
+    gameLoop.start();
   };
 
+  useEffect(() => {
+    startGame();
+    return () => {
+      gameLoop.stop();
+    };
+  }, []);
+
   return (
-    <Screen onLeftPress={nav.to("portfolioLanding")} title="snake">
-      <View
-        style={{ flex: 1, backgroundColor: color.success }}
-        {...gesture.panHandlers}
-      >
-        <Board board={board} />
-      </View>
-    </Screen>
+    <>
+      <Screen onLeftPress={nav.to("portfolioLanding")} title="snake">
+        <View
+          style={{ flex: 1, backgroundColor: color.success }}
+          {...gesture.panHandlers}
+        >
+          <Board board={board} />
+        </View>
+      </Screen>
+      {endGame && (
+        <Modal>
+          <Text title="good try" />
+          <Button title="again" onPress={startGame} />
+        </Modal>
+      )}
+    </>
   );
 });
