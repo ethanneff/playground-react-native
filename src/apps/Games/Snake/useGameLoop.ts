8606e4afa5a788d1da1e5880f7a2ed27ebf9de2b
Ethan Neff <ethan.neff@eneff.com>
2019-12-06 10:13:56 -0800

    fix: rename to gameloop and add start and stop

---
+++ src/apps/Games/Snake/useGameLoop.ts
@@ -0,0 +1,43 @@
+import { useCallback } from "react";
+
+export type Frame = {
+  start: number;
+  current: number;
+  count: number;
+};
+
+export const useGameLoop = (next: (frame: Frame) => void) => {
+  const fps = 16;
+  const now = Date.now();
+  let off = false;
+  let frame: Frame = { start: now, current: now, count: 0 };
+  let timer: any;
+
+  const loop = useCallback(() => {
+    if (off) return;
+    timer = setTimeout(() => {
+      clearTimeout(timer);
+      if (off) return;
+      frame = { ...frame, current: Date.now(), count: frame.count + 1 };
+      next(frame);
+      loop();
+    }, fps);
+  }, [fps, frame]);
+
+  const start = useCallback(() => {
+    off = false;
+    loop();
+  }, [timer]);
+
+  const stop = useCallback(() => {
+    off = true;
+    const now = Date.now();
+    frame = { start: now, current: now, count: 0 };
+    clearTimeout(timer);
+  }, [frame, timer]);
+
+  return {
+    start,
+    stop
+  };
+};
