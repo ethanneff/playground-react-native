2eb6614c2f2d3926af0aeed55d97fec3d3cfbe5e
Ethan Neff <ethan.neff@eneff.com>
2019-12-11 19:50:34 -0800

    refactor: remove next for return

--- src/apps/Games/Snake/useGesture.ts
+++ src/apps/Games/Snake/useGesture.ts
@@ -1,20 +1,27 @@
-import { PanResponder, PanResponderInstance } from "react-native";
+import { useState } from "react";
+import {
+  PanResponder,
+  PanResponderInstance,
+  GestureResponderHandlers
+} from "react-native";
 
 export type Direction = "left" | "right" | "up" | "down";
 
-export const useGesture = (
-  next: (direction: Direction) => void
-): PanResponderInstance => {
-  const panGesture: PanResponderInstance = PanResponder.create({
+export const useGesture = (): {
+  panHandlers: GestureResponderHandlers;
+  direction: Direction;
+} => {
+  const [direction, setDirection] = useState<Direction>("up");
+  const panResponder: PanResponderInstance = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderRelease: (_, g) => {
       if (Math.abs(g.dx) >= Math.abs(g.dy)) {
-        next(g.dx >= 0 ? "right" : "left");
+        setDirection(g.dx >= 0 ? "right" : "left");
       } else {
-        next(g.dy >= 0 ? "down" : "up");
+        setDirection(g.dy >= 0 ? "down" : "up");
       }
     }
   });
 
-  return panGesture;
+  return { direction, panHandlers: panResponder.panHandlers };
 };
