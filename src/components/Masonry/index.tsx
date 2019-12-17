df906b4da6fb90581f48e5ce7152bad601ecfd84
Ethan Neff <ethan.neff@eneff.com>
2019-12-16 21:47:30 -0800

    feat: create masonry component

---
+++ src/components/Masonry/index.tsx
@@ -0,0 +1,42 @@
+import { ScrollView, View } from "react-native";
+import React, { memo } from "react";
+import { Theme } from "../../utils";
+
+interface Item<T> {
+  item: T;
+  index: number;
+}
+
+interface Props<T> {
+  data: ReadonlyArray<T>;
+  numColumns: number;
+  renderItem({ item, index }: Item<T>): void;
+}
+
+export const Masonry = memo(function Masonry<T>({
+  data,
+  numColumns,
+  renderItem
+}: Props<T>) {
+  const columns: ReadonlyArray<any> = [...Array(numColumns)].map(() => []);
+  data.forEach((item: T, index: number) =>
+    columns[index % numColumns].push(item)
+  );
+
+  return (
+    <ScrollView>
+      <View style={{ flexDirection: "row", padding: Theme.padding.p02 }}>
+        {columns.map((column, index) => 
+          <View
+            style={{ flex: 1, padding: Theme.padding.p02 }}
+            key={`column-${index}`}
+          >
+            {column.map((item: T, index: number) =>
+              renderItem({ item, index })
+            )}
+          </View>
+        )}
+      </View>
+    </ScrollView>
+  );
+});
