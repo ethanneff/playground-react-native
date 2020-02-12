274bac11b305d59fbf6c48dfa9a796c54f8afc5f
Ethan Neff <ethan.neff@eneff.com>
2020-02-11 22:14:48 -0800

    fix: faster navigation with loading component

--- src/apps/Debug/screens/GameOfLife/index.tsx
+++ src/apps/Debug/screens/GameOfLife/index.tsx
@@ -14,6 +14,7 @@ export default memo(function DebugGameOfLife() {
     delay: 16,
     count: 20
   });
+  const [loading, setLoading] = useState(true);
   const [board, setBoard] = useState(generateBoard(form.count, 0.5));
   const runRef = useRef(form.run);
   const delayRef = useRef(form.delay);
@@ -77,6 +78,7 @@ export default memo(function DebugGameOfLife() {
   }, []);
 
   useEffect(() => {
+    setLoading(false);
     return () => {
       if (timeoutRef.current) {
         clearTimeout(timeoutRef.current);
@@ -86,62 +88,66 @@ export default memo(function DebugGameOfLife() {
 
   return (
     <Screen onLeftPress={nav.to("debug")} title="Game of life">
-      <View style={{ flexDirection: "row" }}>
-        <Text
-          title={`count: ${form.count}`}
-          h4
-          style={{ alignSelf: "center" }}
-        />
-        <Slider
-          style={{ flex: 1 }}
-          value={10}
-          maximumValue={100}
-          minimumValue={1}
-          step={1}
-          onSlidingComplete={onCountSlide}
-        />
-      </View>
-      <View style={{ flexDirection: "row" }}>
-        <Text
-          title={`delay: ${Math.floor(form.delay)}`}
-          h4
-          style={{ alignSelf: "center" }}
-        />
-        <Slider
-          style={{ flex: 1 }}
-          value={10}
-          maximumValue={100}
-          minimumValue={1}
-          step={1}
-          onSlidingComplete={onDelaySlide}
-        />
-      </View>
-      <View style={styles.container}>
-        <View style={{ flexDirection: "row", justifyContent: "center" }}>
-          <Button title={form.run ? "stop" : "start"} onPress={onStart} />
-          <Button title="random" onPress={onRandom} />
-          <Button title="clear" onPress={onClear} />
-        </View>
-        <View>
-          {board.map((rows, x) => 
-            <View
-              key={`${x}`}
-              style={{ flexDirection: "row", justifyContent: "center" }}
-            >
-              {rows.map((row, y) => 
-                <Cell
-                  key={`${x}-${y}`}
-                  row={row}
-                  x={x}
-                  y={y}
-                  size={size}
-                  onItemPress={onItemPress}
-                />
-              )}
+      {loading ? (
+        <Text h5 medium title="loading..." />
+      ) : (
+        <>
+          <View style={{ flexDirection: "row" }}>
+            <Text
+              title={`count: ${form.count}`}
+              h4
+              style={{ alignSelf: "center" }}
+            />
+            <Slider
+              style={{ flex: 1 }}
+              value={10}
+              maximumValue={100}
+              minimumValue={1}
+              step={1}
+              onSlidingComplete={onCountSlide}
+            />
+          </View>
+          <View style={{ flexDirection: "row" }}>
+            <Text
+              title={`delay: ${Math.floor(form.delay)}`}
+              h4
+              style={{ alignSelf: "center" }}
+            />
+            <Slider
+              style={{ flex: 1 }}
+              value={10}
+              maximumValue={100}
+              minimumValue={1}
+              step={1}
+              onSlidingComplete={onDelaySlide}
+            />
+          </View>
+          <View style={styles.container}>
+            <View style={{ flexDirection: "row", justifyContent: "center" }}>
+              <Button title={form.run ? "stop" : "start"} onPress={onStart} />
+              <Button title="random" onPress={onRandom} />
+              <Button title="clear" onPress={onClear} />
             </View>
-          )}
-        </View>
-      </View>
+            {board.map((rows, x) => (
+              <View
+                key={`${x}`}
+                style={{ flexDirection: "row", justifyContent: "center" }}
+              >
+                {rows.map((row, y) => (
+                  <Cell
+                    key={`${x}-${y}`}
+                    row={row}
+                    x={x}
+                    y={y}
+                    size={size}
+                    onItemPress={onItemPress}
+                  />
+                ))}
+              </View>
+            ))}
+          </View>
+        </>
+      )}
     </Screen>
   );
 });
