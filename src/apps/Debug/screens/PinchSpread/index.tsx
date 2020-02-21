895ca2aa98387ac4bedeb6a7e97bc02aa5a70cd4
Ethan Neff <ethan.neff@eneff.com>
2020-02-20 22:24:47 -0800

    fix: update typescript

--- src/apps/Debug/screens/PinchSpread/index.tsx
+++ src/apps/Debug/screens/PinchSpread/index.tsx
@@ -1,48 +1,34 @@
 import React, { useState } from "react";
-import { Animated, StyleSheet } from "react-native";
+import { Animated, StyleSheet, PanResponder } from "react-native";
 import { Screen, Text } from "../../../../components";
 import { useNav, useColor } from "../../../../hooks";
-import { usePanGesture } from "./usePanGesture";
 import { GestureHandler } from "./logic";
 
 const minTouches = 2;
 const title = `pinch or spread the screen with ${minTouches} fingers minimum`;
 
 export default function PinchSpread() {
-  const [state, setState] = useState({
-    pinchCount: 0,
-    spreadCount: 0
-  });
+  const [state, setState] = useState({ pinchCount: 0, spreadCount: 0 });
   const color = useColor();
   const nav = useNav();
   const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: color.light
-    }
+    container: { flex: 1, backgroundColor: color.light }
   });
   const gestureHandler = new GestureHandler({ minTouches });
-  const panGesture = usePanGesture({
+  const panGesture = PanResponder.create({
     onMoveShouldSetPanResponderCapture: () => true,
-    onPanResponderMove: event => {
-      gestureHandler.onPanResponderMove(event);
-    },
+    onPanResponderMove: event => gestureHandler.onPanResponderMove(event),
     onPanResponderRelease: () => {
-      const { spreadCount, pinchCount } = state;
-      // TODO: fix cannot read property of undefined when 2+ touches
       const outcome = gestureHandler.onPanResponderRelease();
       if (outcome.spread) {
-        setState({ ...state, spreadCount: spreadCount + 1 });
+        setState(prev => ({ ...prev, spreadCount: prev.spreadCount++ }));
       }
       if (outcome.pinch) {
-        setState({ ...state, pinchCount: pinchCount + 1 });
+        setState(prev => ({ ...prev, pinchCount: prev.pinchCount++ }));
       }
     }
   });
 
-  if (!panGesture) {
-    return <></>;
-  }
   return (
     <Screen onLeftPress={nav.to("debug")}>
       <Text center title={title} />
