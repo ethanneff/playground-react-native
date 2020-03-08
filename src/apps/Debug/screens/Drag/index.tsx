c28f7dbd347639b8a370ccb0eba813706f82bd27
Ethan Neff <ethan.neff@eneff.com>
2020-03-08 15:48:43 -0700

    fix: overhaul debug drag to correct movement and boundaries

--- src/apps/Debug/screens/Drag/index.tsx
+++ src/apps/Debug/screens/Drag/index.tsx
@@ -1,49 +1,89 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import {
   Animated,
-  Dimensions,
   PanResponder,
   PanResponderInstance,
-  StyleSheet
+  StyleSheet,
+  View,
+  LayoutChangeEvent,
+  PanResponderGestureState
 } from "react-native";
 import { Screen, Text } from "../../../../components";
-import { useColor, useNav } from "../../../../hooks";
+import { useColor, useNav, useDropShadow } from "../../../../hooks";
+
+const getPosition = (
+  gestureState: PanResponderGestureState,
+  initialPosition: { x: number; y: number },
+  size: number
+) => {
+  const maxY = gestureState.dy - initialPosition.y + size;
+  const minY = gestureState.dy + initialPosition.y - size;
+  const maxX = gestureState.dx - initialPosition.x + size;
+  const minX = gestureState.dx + initialPosition.x - size;
+  const y =
+    minY <= 0
+      ? size
+      : maxY >= 0
+      ? initialPosition.y * 2 - size
+      : initialPosition.y + gestureState.dy;
+  const x =
+    minX <= 0
+      ? size
+      : maxX >= 0
+      ? initialPosition.x * 2 - size
+      : initialPosition.x + gestureState.dx;
+  return { x, y };
+};
 
 export default function DebugDrag() {
   const nav = useNav();
   const color = useColor();
-  const { width, height } = Dimensions.get("window");
+  const dropShadow = useDropShadow(10);
+  const [canvas, setCanvas] = useState({ x: 0, y: 0, height: 0, width: 0 });
+  const initialPosition = { x: canvas.width / 2, y: canvas.height / 2 };
+  const size = 30;
   const styles = StyleSheet.create({
     ball: {
-      borderColor: color.text,
-      borderRadius: 30,
-      borderWidth: 30,
-      height: 60,
-      marginLeft: -30,
-      marginTop: -30,
-      width: 60
+      borderColor: color.background,
+      borderRadius: size,
+      borderWidth: size,
+      height: size,
+      marginLeft: -size,
+      marginTop: -size,
+      width: size,
+      ...dropShadow
+    },
+    canvas: {
+      flex: 1,
+      backgroundColor: color.surface
     }
   });
-  const ballPosition: Animated.ValueXY = new Animated.ValueXY({
-    x: width / 2,
-    y: height / 2
-  });
+  const ballPosition: Animated.ValueXY = new Animated.ValueXY(initialPosition);
   const panGesture: PanResponderInstance = PanResponder.create({
     onMoveShouldSetPanResponderCapture: () => true,
     onPanResponderMove: (_, gestureState) => {
-      Animated.spring(ballPosition, {
-        toValue: { x: gestureState.moveX, y: gestureState.moveY }
-      }).start();
+      const toValue = getPosition(gestureState, initialPosition, size);
+      Animated.spring(ballPosition, { toValue }).start();
+    },
+    onPanResponderEnd: () => {
+      Animated.spring(ballPosition, { toValue: initialPosition }).start();
     }
   });
 
+  const handleCanvas = useCallback((event: LayoutChangeEvent) => {
+    const { x, y, height, width } = event.nativeEvent.layout;
+    setCanvas({ x, y, height, width });
+  }, []);
+
   return (
-    <Screen onLeftPress={nav.to("debug")}>
-      <Text center title="drag the circle" />
-      <Animated.View
-        style={[ballPosition.getLayout(), styles.ball]}
-        {...panGesture.panHandlers}
-      />
+    <Screen onLeftPress={nav.to("debug")} title="Drag">
+      <Text overline center title="drag the circle" />
+      <View style={styles.canvas} onLayout={handleCanvas}>
+        <Animated.View
+          style={[ballPosition.getLayout(), styles.ball]}
+          {...panGesture.panHandlers}
+        />
+      </View>
     </Screen>
   );
 }
