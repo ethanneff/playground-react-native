ea159d25357056c67ea9391c6230c8b4b74de94e
Ethan Neff <ethan.neff@eneff.com>
2020-03-31 19:18:05 -0700

    fix: add more styling for web landing page

--- src/apps/Portfolio/screens/Landing/index.web.tsx
+++ src/apps/Portfolio/screens/Landing/index.web.tsx
@@ -1,4 +1,5 @@
 import React, {memo} from 'react';
+import {Text as OriginalText, ViewStyle, StyleProp} from 'react-native';
 import {Text, TouchableOpacity, Icon} from '../../../../components';
 import {View, Image} from 'react-native';
 import {useColor} from '../../../../hooks';
@@ -23,11 +24,13 @@ const SignInButton = memo(function SignInButton({
         flexDirection: 'row',
         borderColor: color.text,
         borderWidth: 2,
+        padding: Theme.padding.p02,
+        marginBottom: Theme.padding.p02,
         borderRadius: Theme.padding.p10,
       }}
       onPress={onPress}>
-      <Icon name={icon} />
-      <Text title={title} />
+      <Icon name={icon} style={{width: '30px', height: '30px'}} />
+      <Text title={title} style={{paddingLeft: Theme.padding.p02}} h4 />
     </TouchableOpacity>
   );
 });
@@ -49,7 +52,7 @@ const NavButton = memo(function NavButton({
         justifyContent: 'center',
         borderRadius: Theme.padding.p10,
         paddingVertical: Theme.padding.p02,
-        paddingHorizontal: Theme.padding.p04,
+        paddingHorizontal: Theme.padding.p02,
         backgroundColor: inverted ? color.text : color.background,
       }}
       onPress={onPress}>
@@ -58,11 +61,53 @@ const NavButton = memo(function NavButton({
   );
 });
 
+type Word = {
+  title: string;
+  bold?: boolean;
+  type?: 'link' | 'text';
+  onPress?: () => void;
+};
+interface SentenceProps {
+  sentence: Word[];
+  style: StyleProp<ViewStyle>;
+}
+const Sentence = memo(function Sentence({sentence, style}: SentenceProps) {
+  return (
+    <OriginalText style={style}>
+      {sentence.map((word) => (
+        <Text
+          key={word.title}
+          title={word.title}
+          bold={word.bold}
+          onPress={word.onPress}
+        />
+      ))}
+    </OriginalText>
+  );
+});
+
 const image = require('../../../../assets/line-chart.png');
 export default memo(function PortfolioLanding() {
   const color = useColor();
+  const passwordSentence: Word[] = [
+    {title: 'Or use your password to '},
+    {
+      title: 'sign up ',
+      type: 'link',
+      bold: true,
+      onPress: () => undefined,
+    },
+    {title: 'or '},
+    {
+      title: 'sign in ',
+      type: 'link',
+      bold: true,
+      onPress: () => undefined,
+    },
+  ];
+
   return (
-    <View>
+    <View style={{height: '100vh'}}>
       <View
         style={{
           flexDirection: 'row',
@@ -72,7 +117,7 @@ export default memo(function PortfolioLanding() {
           borderBottomWidth: 1,
         }}>
         <View style={{flexDirection: 'row', alignItems: 'center'}}>
-          <Image source={image} style={{width: '50px', height: '50px'}} />
+          <Image source={image} style={{width: '30px', height: '30px'}} />
           <Text
             title="Core"
             h3
@@ -87,33 +132,54 @@ export default memo(function PortfolioLanding() {
           <NavButton inverted title="Sign Up" onPress={() => undefined} />
         </View>
       </View>
-      <Text title="Get started with Core" h1 />
-      <View style={{width: '200'}}>
-        <SignInButton
-          icon="google"
-          title="continue with google"
-          onPress={() => undefined}
+      <View style={{flex: 1}}>
+        <Text
+          title="Get started with Core"
+          h2
+          center
+          bold
+          style={{paddingVertical: Theme.padding.p08}}
         />
-        <SignInButton
-          icon="facebook"
-          title="continue with facebook"
-          onPress={() => undefined}
-        />
-      </View>
-      <Text title="Or use your password to sign up or sign in" />
-      <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-        <NavButton
-          title="About us"
-          onPress={() =>
-            window.open('https://www.linkedin.com/in/ethanneff', '_blank')
-          }
-        />
-        <NavButton
-          title="Contact us"
-          onPress={() => window.open('mailto:ethan.neff@eneff.com', '_blank')}
-        />
-        <NavButton title="Terms of Service" onPress={() => undefined} />
-        <NavButton title="Privacy Policy" onPress={() => undefined} />
+        <View style={{width: '400px', alignSelf: 'center'}}>
+          <SignInButton
+            icon="google"
+            title="Continue with Google"
+            onPress={() => undefined}
+          />
+          <SignInButton
+            icon="facebook"
+            title="Continue with Facebook"
+            onPress={() => undefined}
+          />
+          <Sentence
+            style={{
+              alignSelf: 'center',
+              paddingVertical: Theme.padding.p04,
+            }}
+            sentence={passwordSentence}
+          />
+        </View>
+        <View style={{flex: 1}}></View>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginBottom: Theme.padding.p04,
+          }}>
+          <NavButton
+            title="About us"
+            onPress={() =>
+              window.open('https://www.linkedin.com/in/ethanneff', '_blank')
+            }
+          />
+          <NavButton
+            title="Contact us"
+            onPress={() => window.open('mailto:ethan.neff@eneff.com', '_blank')}
+          />
+          <NavButton title="Terms of Service" onPress={() => undefined} />
+          <NavButton title="Privacy Policy" onPress={() => undefined} />
+        </View>
       </View>
     </View>
   );
