6c5dd0de2cd23509bfb3b06f99db0752bd9d116c
Ethan Neff <ethan.neff@eneff.com>
2020-04-27 21:51:24 -0700

    feat: use clock instead of game loop for precision renders

--- src/apps/Games/Snake/index.tsx
+++ src/apps/Games/Snake/index.tsx
@@ -1,12 +1,12 @@
-import React, {memo, useState, useCallback} from 'react';
+import React, {memo, useState, useCallback, useRef, useEffect} from 'react';
 import {View} from 'react-native';
 import {Screen, Button} from '../../../components';
 import {useColor, useNav} from '../../../hooks';
 import {BoardObject, generateBoard} from './utils';
 import {Board} from './Board';
-import {useGameLoop} from './useGameLoop';
 import {useGesture} from './useGesture';
 import {EndGame} from './EndGame';
+import {useClock} from './useClock';
 
 type State = 'on' | 'off';
 
@@ -25,34 +25,39 @@ export default memo(function Snake() {
     board: generateBoard(size),
     state: 'off',
   });
-  const gesture = useGesture();
+  const {direction, panHandlers} = useGesture();
 
-  const {start, stop} = useGameLoop((frame) => {
-    if (frame.count >= 200) {
-      setGame((prev) => ({...prev, state: 'off'}));
-      stop();
-    }
+  const {start, stop} = useClock({
+    frequency: 60,
+    onUpdate: useCallback(() => {
+      console.log('update', Date.now(), direction.current);
+    }, [direction]),
   });
 
-  const reset = useCallback(() => {
+  const onStart = () => {
     setGame({board: generateBoard(size), state: 'on'});
     start();
-  }, [start]);
+  };
+
+  const onStop = () => {
+    setGame({board: generateBoard(size), state: 'off'});
+    stop();
+  };
 
   return (
     <>
       <Screen onLeftPress={nav.to('portfolioLanding')} title="Snake">
         <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-          <Button title="start" onPress={start} />
-          <Button title="stop" onPress={stop} />
+          <Button title="start" onPress={onStart} />
+          <Button title="stop" onPress={onStop} />
         </View>
         <View
           style={{flex: 1, backgroundColor: color.success}}
-          {...gesture.panHandlers}>
+          {...panHandlers}>
           <Board board={game.board} />
         </View>
       </Screen>
-      {game.state === 'off' && <EndGame onPress={reset} />}
+      {game.state === 'off' && <EndGame onPress={onStart} />}
     </>
   );
 });
