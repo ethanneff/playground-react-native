ec34b7f4c221781a73e3d69f24b8c87c2ab6b801
Ethan Neff <ethan.neff@eneff.com>
2020-04-30 07:56:15 -0700

    style: change background color

--- src/apps/CantHurtMe/screens/Main/index.tsx
+++ src/apps/CantHurtMe/screens/Main/index.tsx
@@ -5,7 +5,7 @@ import {getLandscapeOrientation} from '../../../../models';
 import {Theme, useRootSelector} from '../../../../utils';
 import {DailyProgress, Header, ProfileLevel} from '../../components';
 import {app} from '../../data';
-import {useNav} from '../../../../hooks';
+import {useNav, useColor} from '../../../../hooks';
 import {Item} from './Item';
 
 const initialState = {settings: false, profile: false};
@@ -22,6 +22,7 @@ export default memo(function CantHurtMeMain() {
     setShowModal({...initialState, profile: true});
   const handleSettingsPress = () =>
     setShowModal({...initialState, settings: true});
+  const color = useColor();
 
   return (
     <>
@@ -29,35 +30,37 @@ export default memo(function CantHurtMeMain() {
         border
         onLeftPress={nav.to('portfolioLanding')}
         title="Can't Hurt Me">
-        <View style={{marginHorizontal: Theme.padding.p04}}>
-          <FlatList
-            keyExtractor={keyExtractor}
-            key={columns}
-            data={app.goals.orderById}
-            renderItem={Item}
-            numColumns={columns}
-            ListHeaderComponent={
-              <View>
-                <Header title="Progress" />
-                <Card>
-                  <View
-                    style={{
-                      flexDirection: 'row',
-                      justifyContent: 'space-between',
-                    }}>
-                    <ProfileLevel onPress={handleProfilePress} />
-                    <Icon name="settings" onPress={handleSettingsPress} />
-                  </View>
-                  <DailyProgress />
-                </Card>
-                <Header title="Challenges" />
-              </View>
-            }
-            ListFooterComponent={
-              <View style={{paddingBottom: Theme.padding.p04}} />
-            }
-          />
-        </View>
+        <FlatList
+          contentContainerStyle={{
+            backgroundColor: color.surface,
+            paddingHorizontal: Theme.padding.p04,
+          }}
+          keyExtractor={keyExtractor}
+          key={columns}
+          data={app.goals.orderById}
+          renderItem={Item}
+          numColumns={columns}
+          ListHeaderComponent={
+            <View>
+              <Header title="Progress" />
+              <Card>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                  }}>
+                  <ProfileLevel onPress={handleProfilePress} />
+                  <Icon name="settings" onPress={handleSettingsPress} />
+                </View>
+                <DailyProgress />
+              </Card>
+              <Header title="Challenges" />
+            </View>
+          }
+          ListFooterComponent={
+            <View style={{paddingBottom: Theme.padding.p04}} />
+          }
+        />
       </Screen>
       {showModal.profile && (
         <Dialog
