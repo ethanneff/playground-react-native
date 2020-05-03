1b5e9d9459b35e49f733bcea795678f7d79c36d5
Ethan Neff <ethan.neff@eneff.com>
2020-05-03 09:23:53 -0700

    feat: add papi character

--- src/apps/Games/PapiJump/index.tsx
+++ src/apps/Games/PapiJump/index.tsx
@@ -1,20 +1,58 @@
-import React, {memo} from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {memo, useEffect} from 'react';
+import {StyleSheet, View, Animated, Easing} from 'react-native';
 import {Screen} from '../../../components';
-import {useColor, useNav} from '../../../hooks';
+import {useColor, useNav, useDropShadow, useNativeDriver} from '../../../hooks';
+
+type PapiProps = {size: number; color: string};
+
+const Papi = memo(({size, color}: PapiProps) => {
+  const dropShadow = useDropShadow(10);
+  const useDriver = useNativeDriver();
+  const location = new Animated.ValueXY({x: 0, y: 0});
+
+  useEffect(() => {
+    const toValue = {x: 300, y: 600};
+    Animated.timing(location, {
+      toValue,
+      duration: 3000,
+      easing: Easing.bounce,
+      useNativeDriver: useDriver,
+    }).start();
+  }, [location, useDriver]);
+
+  useEffect(() => {
+    console.log(location);
+  }, [location]);
+
+  return (
+    <Animated.View
+      style={[
+        location.getLayout(),
+        {
+          height: size,
+          width: size,
+          borderRadius: size,
+          backgroundColor: color,
+          ...dropShadow,
+        },
+      ]}
+    />
+  );
+});
 
 export default memo(function PapiJump() {
   const color = useColor();
   const nav = useNav();
   const styles = StyleSheet.create({
     container: {
-      backgroundColor: color.background,
+      flex: 1,
+      backgroundColor: color.surface,
     },
   });
   return (
     <Screen onLeftPress={nav.to('portfolioLanding')} title="Papi Jump">
       <View style={styles.container}>
-        <></>
+        <Papi size={50} color={color.primary} />
       </View>
     </Screen>
   );
