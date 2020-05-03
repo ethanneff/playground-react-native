0f4de2e019dadf4000e7c9bf5a6eb409977cc036
Ethan Neff <ethan.neff@eneff.com>
2020-05-03 15:37:37 -0700

    fix: add login to checklist

--- src/apps/Checklists/index.tsx
+++ src/apps/Checklists/index.tsx
@@ -1,8 +1,9 @@
-import React, {memo} from 'react';
+import React, {memo, useState} from 'react';
 import {StyleSheet, ScrollView} from 'react-native';
-import {Screen, Text, Card, Icon} from '../../components';
+import {Screen, Text, Card, Icon, Button} from '../../components';
 import {useNav, useColor} from '../../hooks';
 import {Theme} from '../../utils';
+import {Login} from '../../features/Login';
 
 export default memo(function Checklists() {
   const color = useColor();
@@ -12,51 +13,57 @@ export default memo(function Checklists() {
       paddingBottom: Theme.padding.p04,
     },
   });
+  const [showLogin, setShowLogin] = useState(false);
+  const onToggleLogin = () => setShowLogin((prev) => !prev);
 
   return (
-    <Screen onLeftPress={nav.to('portfolioLanding')} title="Checklists">
-      <ScrollView
-        style={{backgroundColor: color.surface}}
-        contentContainerStyle={{
-          paddingHorizontal: Theme.padding.p04,
-          paddingVertical: Theme.padding.p02,
-        }}>
-        <Card>
-          <Text
-            style={styles.bottom}
-            type="h4"
-            emphasis="medium"
-            title="Weekly Goals"
-          />
-          <Text type="caption" title="1. complete learn plan profit" />
-          <Text type="caption" title="2. ship checklist" />
-          <Text type="caption" title="3. complete 30 leetcode" />
-        </Card>
-        <Card>
-          <Text
-            style={styles.bottom}
-            type="h4"
-            emphasis="medium"
-            title="Daily Habits and Routines"
-          />
-          <Text type="caption" title="After I wake up" />
-          <Text type="caption" title="After I shower" />
-          <Text type="caption" title="Before I one on one" />
-          <Text type="caption" title="Before I get on the train" />
-        </Card>
-      </ScrollView>
-      <Icon
-        name="plus"
-        fab
-        onPress={() => undefined}
-        color={color.background}
-        style={{
-          position: 'absolute',
-          bottom: 0,
-          right: 0,
-          margin: Theme.padding.p04,
-        }}
-      />
-    </Screen>
+    <>
+      <Screen onLeftPress={nav.to('portfolioLanding')} title="Checklists">
+        <ScrollView
+          style={{backgroundColor: color.surface}}
+          contentContainerStyle={{
+            paddingHorizontal: Theme.padding.p04,
+            paddingVertical: Theme.padding.p02,
+          }}>
+          <Card>
+            <Text
+              style={styles.bottom}
+              type="h4"
+              emphasis="medium"
+              title="Weekly Goals"
+            />
+            <Text type="caption" title="1. complete learn plan profit" />
+            <Text type="caption" title="2. ship checklist" />
+            <Text type="caption" title="3. complete 30 leetcode" />
+          </Card>
+          <Card>
+            <Text
+              style={styles.bottom}
+              type="h4"
+              emphasis="medium"
+              title="Daily Habits and Routines"
+            />
+            <Text type="caption" title="After I wake up" />
+            <Text type="caption" title="After I shower" />
+            <Text type="caption" title="Before I one on one" />
+            <Text type="caption" title="Before I get on the train" />
+          </Card>
+        </ScrollView>
+        <Icon
+          name="plus"
+          fab
+          onPress={() => undefined}
+          color={color.background}
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            right: 0,
+            margin: Theme.padding.p04,
+          }}
+        />
+        <Button title="showLogin" onPress={onToggleLogin} />
+      </Screen>
+      {showLogin && <Login onBackgroundPress={onToggleLogin} />}
+    </>
   );
 });
