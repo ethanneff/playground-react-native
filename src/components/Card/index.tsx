95c786f6afd4648a3ce4f6f10e3a66d1a58d0fd1
Ethan Neff <ethan.neff@eneff.com>
2020-06-01 15:53:21 -0700

    fix: correct re renders in components

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,4 +1,4 @@
-import React, {memo} from 'react';
+import React from 'react';
 import {StyleSheet, View, ViewStyle} from 'react-native';
 import {Theme} from '../../utils';
 import {useColor, useDropShadow} from '../../hooks';
@@ -19,68 +19,66 @@ interface Props {
 }
 
 const touchOpacity = 0.3;
-export const Card: React.FC<Props> = memo(
-  ({
-    borderRadius = Theme.sizing.borderRadius,
-    borderWidth = 1,
-    children,
-    elevation = 2,
-    onLongPress,
-    onPress,
-    noMargin,
-    noPadding,
-    flex,
-    selected,
-    testID,
-    style,
-  }) => {
-    const color = useColor();
-    const dropShadow = useDropShadow(elevation);
-    const opacity =
-      elevation === 0
-        ? 0
-        : elevation === 1
-        ? 0.05
-        : ((elevation - 2) * 0.01 + 0.07).toFixed(2);
-    const styles = StyleSheet.create({
-      containerStyle: {
-        backgroundColor: color.background,
-        borderColor: color.background,
-        borderRadius,
-        borderWidth,
-        marginVertical: noMargin ? 0 : Theme.padding.p02,
-        ...dropShadow,
-      },
-      contents: {
-        backgroundColor: `hsla(0,0%,100%,${opacity})`,
-        borderRadius,
-        padding: noPadding ? 0 : Theme.padding.p04,
-      },
-      flex: {
-        flex: 1,
-      },
-      selected: {
-        backgroundColor: color.primary,
-      },
-    });
+export const Card: React.FC<Props> = ({
+  borderRadius = Theme.sizing.borderRadius,
+  borderWidth = 1,
+  children,
+  elevation = 2,
+  onLongPress,
+  onPress,
+  noMargin,
+  noPadding,
+  flex,
+  selected,
+  testID,
+  style,
+}) => {
+  const color = useColor();
+  const dropShadow = useDropShadow(elevation);
+  const opacity =
+    elevation === 0
+      ? 0
+      : elevation === 1
+      ? 0.05
+      : ((elevation - 2) * 0.01 + 0.07).toFixed(2);
+  const styles = StyleSheet.create({
+    containerStyle: {
+      backgroundColor: color.background,
+      borderColor: color.background,
+      borderRadius,
+      borderWidth,
+      marginVertical: noMargin ? 0 : Theme.padding.p02,
+      ...dropShadow,
+    },
+    contents: {
+      backgroundColor: `hsla(0,0%,100%,${opacity})`,
+      borderRadius,
+      padding: noPadding ? 0 : Theme.padding.p04,
+    },
+    flex: {
+      flex: 1,
+    },
+    selected: {
+      backgroundColor: color.primary,
+    },
+  });
 
-    const containerStyles = [
-      styles.containerStyle,
-      selected ? styles.selected : undefined,
-      flex ? styles.flex : undefined,
-      style,
-    ];
-    const contentStyles = [styles.contents, flex ? styles.flex : undefined];
-    return (
-      <TouchableOpacity
-        testID={testID}
-        style={containerStyles}
-        onPress={onPress && onPress}
-        onLongPress={onLongPress && onLongPress}
-        activeOpacity={onPress ? touchOpacity : 1}
-        disabled={!onPress}>
-        <View style={contentStyles}>{children}</View>
-      </TouchableOpacity>
-    );
-  },
-);
+  const containerStyles = [
+    styles.containerStyle,
+    selected ? styles.selected : undefined,
+    flex ? styles.flex : undefined,
+    style,
+  ];
+  const contentStyles = [styles.contents, flex ? styles.flex : undefined];
+  return (
+    <TouchableOpacity
+      testID={testID}
+      style={containerStyles}
+      onPress={onPress && onPress}
+      onLongPress={onLongPress && onLongPress}
+      activeOpacity={onPress ? touchOpacity : 1}
+      disabled={!onPress}>
+      <View style={contentStyles}>{children}</View>
+    </TouchableOpacity>
+  );
+};
--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,13 +1,13 @@
-import React, {memo, useRef, useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {
+  KeyboardTypeOptions,
   TextInput as Original,
+  ReturnKeyTypeOptions,
+  StyleProp,
   StyleSheet,
   TextStyle,
   View,
   ViewStyle,
-  StyleProp,
-  KeyboardTypeOptions,
-  ReturnKeyTypeOptions,
 } from 'react-native';
 import {Theme} from '../../utils';
 import {Button} from '../Button';
@@ -44,156 +44,154 @@ interface Props {
   onSubmitEditing?(): void;
 }
 
-export const TextInput: React.FC<Props> = memo(
-  ({
-    autoCorrect,
-    blurOnSubmit = true,
-    clearIcon = 'close-circle',
-    containerStyle,
-    disableFullscreenUI = true,
-    editable = true,
-    error = '',
-    errorIcon = 'alert-circle',
-    flex,
-    keyboardType,
-    onChangeText,
-    onSubmitEditing,
-    optional,
-    placeholder,
-    returnKeyType,
-    secureTextEntry,
-    textContentType = 'none',
+export const TextInput: React.FC<Props> = ({
+  autoCorrect,
+  blurOnSubmit = true,
+  clearIcon = 'close-circle',
+  containerStyle,
+  disableFullscreenUI = true,
+  editable = true,
+  error = '',
+  errorIcon = 'alert-circle',
+  flex,
+  keyboardType,
+  onChangeText,
+  onSubmitEditing,
+  optional,
+  placeholder,
+  returnKeyType,
+  secureTextEntry,
+  textContentType = 'none',
+  textStyle,
+  title = '',
+  value,
+}) => {
+  const [focus, setFocus] = useState(false);
+  const color = useColor();
+  const focusColor = color.primary;
+  const styles = StyleSheet.create({
+    borderError: {
+      borderColor: color.danger,
+    },
+    borderFocus: {
+      borderColor: focusColor,
+    },
+    clear: {
+      width: 30,
+      position: 'absolute',
+      right: 0,
+      top: 6,
+    },
+    flex: {
+      flex: 1,
+    },
+    row: {
+      flexDirection: 'row',
+    },
+    textInput: {
+      backgroundColor: color.background,
+      borderColor: color.secondary,
+      borderRadius: Theme.padding.p01,
+      borderWidth: 2,
+      color: color.text,
+      padding: Theme.padding.p02,
+      paddingRight: Theme.padding.p08,
+      width: '100%',
+    },
+  });
+  const textInput = useRef<Original>(null);
+  const optionalText = ' - optional';
+  const textInputStyles = [
+    styles.textInput,
+    error ? styles.borderError : undefined,
+    focus ? styles.borderFocus : undefined,
+    Theme.fontSize.body2,
     textStyle,
-    title = '',
-    value,
-  }) => {
-    const [focus, setFocus] = useState(false);
-    const color = useColor();
-    const focusColor = color.primary;
-    const styles = StyleSheet.create({
-      borderError: {
-        borderColor: color.danger,
-      },
-      borderFocus: {
-        borderColor: focusColor,
-      },
-      clear: {
-        width: 30,
-        position: 'absolute',
-        right: 0,
-        top: 6,
-      },
-      flex: {
-        flex: 1,
-      },
-      row: {
-        flexDirection: 'row',
-      },
-      textInput: {
-        backgroundColor: color.background,
-        borderColor: color.secondary,
-        borderRadius: Theme.padding.p01,
-        borderWidth: 2,
-        color: color.text,
-        padding: Theme.padding.p02,
-        paddingRight: Theme.padding.p08,
-        width: '100%',
-      },
-    });
-    const textInput = useRef<Original>(null);
-    const optionalText = ' - optional';
-    const textInputStyles = [
-      styles.textInput,
-      error ? styles.borderError : undefined,
-      focus ? styles.borderFocus : undefined,
-      Theme.fontSize.body2,
-      textStyle,
-    ];
-    const noValue = value.length === 0;
-    const noError = error.length === 0;
-    const noTitle = title.length === 0;
-    const containerStyles = [flex && styles.flex, containerStyle];
+  ];
+  const noValue = value.length === 0;
+  const noError = error.length === 0;
+  const noTitle = title.length === 0;
+  const containerStyles = [flex && styles.flex, containerStyle];
 
-    const onFocus = () => setFocus(true);
-    const onBlur = () => setFocus(false);
-    const focusOnInput = () => textInput.current && textInput.current.focus();
-    const textClear = () => {
-      if (textInput.current) {
-        textInput.current.clear();
-      }
-      onChangeText('');
-    };
+  const onFocus = () => setFocus(true);
+  const onBlur = () => setFocus(false);
+  const focusOnInput = () => textInput.current && textInput.current.focus();
+  const textClear = () => {
+    if (textInput.current) {
+      textInput.current.clear();
+    }
+    onChangeText('');
+  };
 
-    return (
-      <View style={containerStyles}>
-        <View style={styles.row}>
-          <Button
-            activeOpacity={1}
-            hidden={noTitle}
-            lowercase
-            onPress={focusOnInput}
-            title={title}
-            noPadding
-          />
-          <Button
-            activeOpacity={1}
-            hidden={!optional}
-            lowercase
-            onPress={focusOnInput}
-            color="secondary"
-            title={optionalText}
-            noPadding
-          />
-        </View>
-        <View style={styles.row}>
-          <Original
-            textContentType={textContentType}
-            autoCorrect={autoCorrect}
-            blurOnSubmit={blurOnSubmit}
-            disableFullscreenUI={disableFullscreenUI}
-            editable={editable}
-            onSubmitEditing={onSubmitEditing}
-            keyboardType={keyboardType}
-            onBlur={onBlur}
-            onChangeText={onChangeText}
-            onFocus={onFocus}
-            placeholder={placeholder}
-            placeholderTextColor={color.secondary}
-            ref={textInput}
-            returnKeyType={returnKeyType}
-            secureTextEntry={secureTextEntry}
-            selectionColor={focusColor}
-            style={textInputStyles}
-            underlineColorAndroid="transparent"
-            value={value}
-          />
-          <Icon
-            style={styles.clear}
-            hidden={noValue}
-            name={clearIcon}
-            onPress={textClear}
-            color={color.secondary}
-          />
-        </View>
-        <View style={{flexDirection: 'row'}}>
-          <Icon
-            activeOpacity={1}
-            invisible={noError}
-            name={errorIcon}
-            onPress={focusOnInput}
-            color={color.danger}
-          />
-          <Button
-            activeOpacity={1}
-            color="danger"
-            invisible={noError}
-            lowercase
-            noPadding
-            onPress={focusOnInput}
-            title={error}
-          />
-        </View>
+  return (
+    <View style={containerStyles}>
+      <View style={styles.row}>
+        <Button
+          activeOpacity={1}
+          hidden={noTitle}
+          lowercase
+          onPress={focusOnInput}
+          title={title}
+          noPadding
+        />
+        <Button
+          activeOpacity={1}
+          hidden={!optional}
+          lowercase
+          onPress={focusOnInput}
+          color="secondary"
+          title={optionalText}
+          noPadding
+        />
       </View>
-    );
-  },
-);
+      <View style={styles.row}>
+        <Original
+          textContentType={textContentType}
+          autoCorrect={autoCorrect}
+          blurOnSubmit={blurOnSubmit}
+          disableFullscreenUI={disableFullscreenUI}
+          editable={editable}
+          onSubmitEditing={onSubmitEditing}
+          keyboardType={keyboardType}
+          onBlur={onBlur}
+          onChangeText={onChangeText}
+          onFocus={onFocus}
+          placeholder={placeholder}
+          placeholderTextColor={color.secondary}
+          ref={textInput}
+          returnKeyType={returnKeyType}
+          secureTextEntry={secureTextEntry}
+          selectionColor={focusColor}
+          style={textInputStyles}
+          underlineColorAndroid="transparent"
+          value={value}
+        />
+        <Icon
+          style={styles.clear}
+          hidden={noValue}
+          name={clearIcon}
+          onPress={textClear}
+          color={color.secondary}
+        />
+      </View>
+      <View style={{flexDirection: 'row'}}>
+        <Icon
+          activeOpacity={1}
+          invisible={noError}
+          name={errorIcon}
+          onPress={focusOnInput}
+          color={color.danger}
+        />
+        <Button
+          activeOpacity={1}
+          color="danger"
+          invisible={noError}
+          lowercase
+          noPadding
+          onPress={focusOnInput}
+          title={error}
+        />
+      </View>
+    </View>
+  );
+};
