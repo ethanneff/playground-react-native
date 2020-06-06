a2396410201d4059e33b393ef10c7f1ac8632fd3
Ethan Neff <ethan.neff@eneff.com>
2020-06-05 23:58:32 -0700

    fix: scroll to new item on submit

--- src/apps/Playground/screens/Chat/Items.tsx
+++ src/apps/Playground/screens/Chat/Items.tsx
@@ -1,10 +1,11 @@
-import React, {memo, useCallback} from 'react';
+import React, {memo, useCallback, useRef, useEffect} from 'react';
 import {FlatList} from 'react-native';
 import {useRootSelector} from '../../../../utils';
 import {Item} from './Item';
 import {Message, getActiveChatMessagesOrderByCreatedAt} from './Messages';
 
 export const Items = memo(function ChatMessageItems() {
+  const itemsRef = useRef<FlatList | null>(null);
   const messages = useRootSelector(getActiveChatMessagesOrderByCreatedAt);
   const renderItem = useCallback(
     ({item}: {item: Message}) => <Item item={item} />,
@@ -12,8 +13,13 @@ export const Items = memo(function ChatMessageItems() {
   );
   const keyExtractor = useCallback((item: Message) => item.id, []);
 
+  useEffect(() => {
+    itemsRef.current?.scrollToItem({item: messages[0]});
+  }, [messages]);
+
   return (
     <FlatList
+      ref={itemsRef}
       inverted
       data={messages}
       keyExtractor={keyExtractor}
