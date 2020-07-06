e2c1394010b01c0081a6ac2e14d64e7bac315fa7
Ethan Neff <ethan.neff@eneff.com>
2020-07-05 21:46:58 -0700

    feat: add the one thing

---
+++ src/apps/TheOneThing/data.tsx
@@ -0,0 +1,135 @@
+type Example = {
+  category: string;
+  goal: string;
+  examples: string[];
+};
+
+export const examples: Example[] = [
+  {
+    category: 'Your personal life',
+    goal:
+      "Let the ONE Thing bring clarity to the key areas of your life. Here's a short sampling.",
+    examples: [
+      "What's the ONE Thing I can do this week to discover or affirm my life's purpose... ?",
+      "What's the ONE Thing I can do in 90 days to get in the physical shape I want... ?",
+      "What's the ONE Thing I can do today to strengthen my spiritual faith... ?",
+      "What's the ONE Thing I can do to find time to practice the guitar 20 minutes a day... ?",
+      "What's the ONE Thing I can do to knock five strokes off my golf game in 90 days... ? ",
+      "What's the ONT thing I can do to to learn to paint in six months... ?",
+    ],
+  },
+  {
+    category: 'Your family',
+    goal:
+      'Use the ONE Thing with your family for fun and rewarding experiences',
+    examples: [
+      "What's the ONE Thing we can do this week to improve our marriage... ?",
+      "What's the ONE Thing we can do every week to spend more quality family time together... ?",
+      "What's the ONE Thing we can do tonight to support our kid's schoolwork... ?",
+      "What's the ONE Thing we can do to make our next vacation the best ever... ? Our next Christmas the best ever... ? Thanksgiving the best ever... ?",
+    ],
+  },
+  {
+    category: 'Your job',
+    goal: 'Use the ONE Thing to be effective and productive at work',
+    examples: [
+      "What's the ONE Thing I can do today to complete my current project ahead of schedule... ?",
+      "What's the ONE Thing I can do this month to produce better work... ?",
+      "What's the ONE Thing I can do before my next review to get the raise I want... ?",
+      "What's the ONE Thing I can do everyday to finish my work and still get home on time... ?",
+    ],
+  },
+  {
+    category: 'Your team',
+    goal:
+      'Use the ONE Thing thinking into your everyday work situations to drive productivity upward.',
+    examples: [
+      "What's the ONE Thing I accomplish in this meeting and end early... ?",
+      "What's the ONE Thing I can do in the next six months to find and develop incredible talent... ?",
+      "What's the ONE Thing we can do right now to accomplish our goals ahead of schedule and under budget... ?",
+      "What's the ONE Thing we can do in the next 90 days to create a ONE Thing culture... ?",
+    ],
+  },
+];
+
+export const oneThing =
+  "What's the ONE thing I can do such that by doing it everything else will be easier or unnecessary?";
+
+export const timeBlockCommitments = [
+  'Time block your time off.',
+  'Time block your ONE Thing.',
+  'Time block your planning time.',
+];
+
+export const oneThingCommitments = [
+  'Time block to gain mastery',
+  'Always look for ways to improve',
+  'Be accountable',
+];
+
+export const productivityCommitments = [
+  'Say NO',
+  'Embrace chaos',
+  'Maintain good health',
+  'Change your environment to support your goals',
+];
+
+export const dailyPlan = {
+  goal:
+    'When you spend the early hours energizing yourself, you get pulled through the rest of the day with little additional effort',
+  plan: [
+    'Meditate and pray for spiritual energy.',
+    'Eat right, exercise, and sleep sufficiently for physical energy.',
+    'Hug, kiss, and laugh with loved ones for emotional energy.',
+    'Set goals, plan, and calendar for mental energy.',
+    'Time block your ONE Thing for business energy.',
+  ],
+};
+
+export const yearlyPlan = {
+  goal: 'Strive for extraordinary. Strive for big, win big.',
+  plan: [
+    'Commit to be your best',
+    'Be purposeful about your ONE Thing.',
+    'Take ownership of your outcomes.',
+    'Find a coach.',
+  ],
+};
+
+export const quotes = [
+  '"There can only be one most important thing. Many things may be important, but only one can be the most important." — Ross Garber',
+  '"It is those who concentrate on but one thing at a time who advance in this world." — Og Mandino',
+  '"Be like a postage stamp — stick to one thing until you get there." — Josh Billings',
+  '"Every great change starts like falling dominoes." — BJ Thornton',
+  '"If you chase two rabbits, you will not catch either one." - Russian Proverb',
+  '"You must be single- minded. Drive for the one thing on which you have decided." — General George S. Patton',
+  '"Success demands singleness of purpose." — Vince Lombardi',
+  '"It ain’t what you don’t know that gets you into trouble. It’s what you know for sure that just ain’t so." — Mark Twain',
+  '"Things which matter most must never be at the mercy of things which matter least." — Johann Wolfgang von Goethe',
+  '"The things which are most important don’t always scream the loudest." — Bob Hawke',
+  '"To do two things at once is to do neither." — Publilius Syrus',
+  '"Multitasking is merely the opportunity to screw up more than one thing at a time." — Steve Uzzell',
+  '"It’s one of the most prevalent myths of our culture: self-discipline." — Leo Babauta',
+  '"The truth is, balance is bunk. It is an unattainable pipe dream... . The quest for balance between work and life, as we’ve come to think of it, isn’t just a losing proposition; it’s a hurtful, destructive one." — Keith H. Hammonds',
+  '"We are kept from our goal, not by obstacles but by a clear path to a lesser goal." — Robert Brault',
+  '"The rung of a ladder was never meant to rest upon, but only to hold a man’s foot long enough to enable him to put the other somewhat higher." — Thomas Henry Huxley',
+  '"Be careful how you interpret the world; it is like that." — Erich Heller',
+  '"There is an art to clearing away the clutter and focusing on what matters most. It is simple and it is transferable. It just requires the courage to take a different approach." — George Anders',
+  '"But those Woulda- Coulda-Shouldas all ran away and hid from one little Did." — Shel Silverstein',
+  '"Success is simple. Do what’s right, the right way, at the right time." — Arnold H. Glasow',
+  '"People do not decide their futures, they decide their habits and their habits decide their futures." — F. M. Alexander',
+  '"Even if you’re on the right track, you’ll get run over if you just sit there." — Will Rogers',
+  '"Life isn’t about finding yourself. Life is about creating yourself." — George Bernard Shaw',
+  '"Planning is bringing the future into the present so that you can do something about it now." — Alan Lakein',
+  '"My goal is no longer to get more done, but rather to have less to do." — Francine Jay',
+  '"Day, n. A period of twenty-four hours, mostly misspent." — Ambrose Bierce',
+  '"Efficiency is doing the thing right. Effectiveness is doing the right thing." — Peter Drucker',
+  '"Nobody who ever gave his best regretted it." — George Halas',
+  '"Focus is a matter of deciding what things you’re not going to do." — John Carmack',
+  '"If a cluttered desk is a sign of a cluttered mind, of what, then, is an empty desk a sign?" — Albert Einstein',
+  '"The art of being wise is the art of knowing what to overlook." — William James',
+  '"Surround yourself only with people who are going to lift you higher." — Oprah Winfrey',
+  '"To get through the hardest journey we need take only one step at a time, but we must keep on stepping." — Chinese Proverb',
+  '"Only those who will risk going too far can possibly find out how far one can go." — T. S. Eliot',
+  '"Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do. So throw off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails. Explore. Dream. Discover." — Mark Twain',
+];
---
+++ src/apps/TheOneThing/index.tsx
@@ -0,0 +1,153 @@
+import React, {memo, useState} from 'react';
+import {ScrollView, View} from 'react-native';
+import {
+  Button,
+  Card,
+  Icon,
+  Screen,
+  Text,
+  TouchableOpacity,
+} from '../../components';
+import {useColor, useNav} from '../../hooks';
+import {Theme} from '../../utils';
+
+interface SectionProps {
+  title: string;
+  description?: string;
+  items?: string[];
+}
+
+type ChecklistItemProps = {
+  item: string;
+  index: number;
+};
+
+const ChecklistItem = ({item, index}: ChecklistItemProps) => {
+  const [toggle, setToggle] = useState('checkbox-blank-outline');
+  const onPress = () => {
+    setToggle((prev) =>
+      prev === 'checkbox-marked-outline'
+        ? 'checkbox-blank-outline'
+        : 'checkbox-marked-outline',
+    );
+  };
+  const days = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+        <Icon name={toggle} style={{paddingRight: Theme.padding.p02}} />
+        <View style={{flex: 1}}>
+          <Text title={`${index + 1}. ${item}`} />
+        </View>
+      </View>
+      <View style={{flexDirection: 'row'}}>
+        {days.map((day) => (
+          <View key={day} style={{flex: 1}}>
+            <Text center emphasis="low" title={day} type="caption" />
+          </View>
+        ))}
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const Section = ({title, description, items = []}: SectionProps) => {
+  return (
+    <Card>
+      <Text emphasis="medium" title={title} type="caption" />
+
+      <View style={{paddingTop: Theme.padding.p02}}>
+        {description && <Text title={description} />}
+        {items &&
+          items.map((item, index) => (
+            <ChecklistItem index={index} item={item} key={item} />
+          ))}
+      </View>
+    </Card>
+  );
+};
+
+export default memo(function TheOneThing() {
+  const color = useColor();
+  const nav = useNav();
+
+  return (
+    <Screen onLeftPress={nav.to('portfolioLanding')} title="The One Thing">
+      <ScrollView
+        contentContainerStyle={{
+          backgroundColor: color.light,
+          paddingHorizontal: Theme.padding.p04,
+          paddingVertical: Theme.padding.p02,
+        }}>
+        <Text
+          emphasis="medium"
+          style={{paddingHorizontal: Theme.padding.p04}}
+          title="Purpose"
+          type="h3"
+        />
+        <Section
+          description="What’s the thing that gets you up in the morning and keeps you going when you’re tired and worn down - why you’re excited with your life?"
+          title="Your Purpose"
+        />
+        <Text
+          emphasis="medium"
+          style={{paddingHorizontal: Theme.padding.p04}}
+          title="Priority"
+          type="h3"
+        />
+        <Section
+          description="Your most important priority is the ONE Thing you can do right now that will help you achieve what matters most to you - big and specific"
+          title="Your ONE Goal"
+        />
+
+        <Text
+          emphasis="medium"
+          style={{paddingHorizontal: Theme.padding.p04}}
+          title="Productivity"
+          type="h3"
+        />
+        <Section
+          items={['purpose', 'goals', 'time block', 'share', 'notifications']}
+          title="Immediate Reflection"
+        />
+        <Section
+          items={[
+            'Meditate and pray for spiritual energy',
+            'Eat right, exercise, and sleep sufficiently for physical energy',
+            'Hug, kiss, and laugh with loved ones for emotional energy',
+            'Set goals, plan, and calendar for mental energy',
+            'Create success list (should do) instead of todo list (could do)',
+            'Prepare environment to support your goals',
+            'Time block your ONE Thing for business energy',
+          ]}
+          title="Morning Routine"
+        />
+        <Section
+          items={[
+            'Share daily progress texts with friends/family/coach',
+            'Ask great questions with big and specific',
+            'Find great answers with research and role models',
+            'Do your ONE goal for 4 hours each morning',
+            'Protect your time block (no distractions)',
+            'Protect your environment (no distractions)',
+            'Be accountable (own everything)',
+            'Commit to be your best with everything (mastery)',
+            'Maintain healthy habits',
+            'Surround yourself with people who support you',
+            'Live with no regrets (purpose, priority, and productivity)',
+            'Reflect on ways to improve each action',
+          ]}
+          title="Daily Reflection"
+        />
+        <Section
+          items={[
+            'Review your annual and monthly goals (to get on target)',
+            'Time block your next week calendar (time off, you ONE thing, 1hr planning, everything else)',
+          ]}
+          title="Weekly Reflection"
+        />
+        <Button color="primary" title="submit" />
+      </ScrollView>
+    </Screen>
+  );
+});
