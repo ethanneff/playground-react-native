527ebf0845be1a89ff7f8abd1b519d34d9a44020
Ethan Neff <ethan.neff@eneff.com>
2020-07-13 10:51:18 -0700

    fix: correct typings

--- src/components/Icon/Source.web.tsx
+++ src/components/Icon/Source.web.tsx
@@ -5,7 +5,7 @@ import {Icons} from './config';
 interface Props {
   name: keyof typeof Icons;
   color?: string;
-  style?: {};
+  style?: object;
 }
 
 export const Source = memo(function IconSource({name, color, style}: Props) {
