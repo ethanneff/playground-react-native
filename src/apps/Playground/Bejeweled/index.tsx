d1e8978eaacd36e123d17014452ee1cc8bb69696
Ethan Neff <ethan.neff@eneff.com>
2020-07-17 15:43:06 -0700

    fix: add calc for bejeweled matches

--- src/apps/Playground/Bejeweled/index.tsx
+++ src/apps/Playground/Bejeweled/index.tsx
@@ -9,19 +9,47 @@ const getRandom = (max: number) => {
   return Math.floor(Math.random() * max);
 };
 
-const getInitialState = (size: number, gems: Gem[]) => {
-  return Array(size)
-    .fill(0)
-    .map(() =>
-      Array(size)
-        .fill(0)
-        .map(() => gems[getRandom(gems.length)]),
-    );
+const getInitialState = (rows: number, cols: number, gems: Gem[]): Board => {
+  let col = [];
+  for (let i = 0; i < cols; i++) {
+    let row = [];
+    for (let j = 0; j < rows; j++) {
+      row.push(gems[getRandom(gems.length)]);
+    }
+    col.push(row);
+  }
+  return col;
 };
 
-type Gem = {
-  key: string;
-  color: string;
+type Vector = {x: number; y: number};
+type Gem = {key: string; color: string};
+type Board = Gem[][];
+type Matches = {[key: string]: Vector};
+
+const getMatches = (board: Board): Matches => {
+  let matches: Matches = {};
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      const mid = board[i][j];
+      const left = i - 1 < 0 ? undefined : board[i - 1][j];
+      const right = i + 1 >= board.length ? undefined : board[i + 1][j];
+      const up = j - 1 < 0 ? undefined : board[i][j - 1];
+      const down = j + 1 >= board[i].length ? undefined : board[i][j + 1];
+      const vertical = up?.key === mid.key && down?.key === mid.key;
+      const horizontal = left?.key === mid.key && right?.key === mid.key;
+      if (vertical) {
+        matches[`${i}${j - 1}`] = {x: i, y: j - 1};
+        matches[`${i}${j}`] = {x: i, y: j};
+        matches[`${i}${j + 1}`] = {x: i, y: j + 1};
+      }
+      if (horizontal) {
+        matches[`${i - 1}${j}`] = {x: i - 1, y: j};
+        matches[`${i}${j}`] = {x: i, y: j};
+        matches[`${i + 1}${j}`] = {x: i + 1, y: j};
+      }
+    }
+  }
+  return matches;
 };
 
 export type States = 'idle' | 'swap' | 'validate' | 'swap back' | 'fill gems';
@@ -33,13 +61,12 @@ export default memo(function PlaygroundBejeweled() {
 
   const color = useColor();
   const styles = StyleSheet.create({
-    container: {
-      backgroundColor: color.background,
-    },
+    container: {backgroundColor: color.surface},
   });
 
-  const count = 8;
-  const size = dimension / 8;
+  const width = 6;
+  const height = 6;
+  const size = dimension / width;
   const gems: Gem[] = [
     {key: 'primary', color: color.primary},
     {key: 'brand', color: color.brand},
@@ -49,7 +76,10 @@ export default memo(function PlaygroundBejeweled() {
     {key: 'danger', color: color.danger},
   ];
 
-  const [board, setBoard] = useState(() => getInitialState(count, gems));
+  const [board, setBoard] = useState<Board>(() =>
+    getInitialState(width, height, gems),
+  );
+  getMatches(board);
   const [selected, setSelected] = useState(initialSelected);
 
   const onPress = (x: number, y: number) => () => {
