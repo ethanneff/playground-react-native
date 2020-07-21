23aed381b2d429200d359a4cfde5846c6fb12b9e
Ethan Neff <ethan.neff@eneff.com>
2020-07-20 22:15:14 -0700

    docs: record coin master probability

---
+++ src/apps/Playground/SlotMachine/coinMaster.tsx
@@ -0,0 +1,36 @@
+export const symbols = {
+  oneCoin: '1111111111111111111111111111111111111111111111',
+  twoCoin: '1111111111111111111',
+  threeCoin: '1111111111111111111111111',
+  oneBag: '11111111111111111111111111111111111111111111',
+  twoBag: '1111111111111111111111111111111111',
+  threeBag: '1111111111111111111111',
+  threeHammer: '11111111111111',
+  threeEnergy: '1',
+  threeShield: '111111111',
+  threePig: '11111111',
+  nothing: '11111111',
+};
+
+const oneCoin = 13600; // 13800 (20)
+const twoCoin = 16400; // 16700 (20)
+const threeCoins = 102900; // 104900 (20)
+const oneBag = 29100; // 29600 (20)
+const twoBag = 61000; // 62200 (20)
+const threeBag = 260000; // 270000 (20)
+const hammer = 825000; // 225000
+const threePig = 5912400; // 7495800 1770000 254000 678000 112000 260000 390000
+const threeEnergy = 10;
+
+const bonus = ['threeShield', 'threeHammer']; // x3/4 each, 20 gift (10m, 120 energy)
+
+const wheel = [
+  250000,
+  12500000,
+  650000,
+  2500000,
+  400000,
+  50000000,
+  1200000,
+  4000000,
+];
