663c2e5c8102d782f76322bde7113f93e8ae5cb7
Ethan Neff <ethan.neff@eneff.com>
2020-10-14 11:15:20 -0700

    fix: hide highlight renders behind a dev menu

--- src/utils/Debugger/index.tsx
+++ src/utils/Debugger/index.tsx
@@ -4,11 +4,14 @@ export const debugDev = (): void => {
   if (!__DEV__) {
     return;
   }
-  require('react-native-highlight-updates');
-  const whyDidYouRender = require('@welldone-software/why-did-you-render');
-  const ReactRedux = require('react-redux');
-  whyDidYouRender(React, {
-    trackAllPureComponents: true,
-    trackExtraHooks: [[ReactRedux, 'useSelector']],
+  const DevMenu = require('react-native-dev-menu');
+  DevMenu.addItem('Highlight Renders', () => {
+    require('react-native-highlight-updates');
+    const whyDidYouRender = require('@welldone-software/why-did-you-render');
+    const ReactRedux = require('react-redux');
+    whyDidYouRender(React, {
+      trackAllPureComponents: true,
+      trackExtraHooks: [[ReactRedux, 'useSelector']],
+    });
   });
 };
