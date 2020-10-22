1431409093b695d7ba07ffe22a7ba80d1a379fc8
Ethan Neff <ethan.neff@eneff.com>
2020-10-21 19:39:58 -0700

    fix: add conversion for linear gradient

---
+++ src/conversions/LinearGradient/index.tsx
@@ -0,0 +1,2 @@
+import Dependency from 'react-native-linear-gradient';
+export const LinearGradient = Dependency;
---
+++ src/conversions/LinearGradient/index.web.tsx
@@ -0,0 +1,2 @@
+import Dependency from 'react-native-web-linear-gradient';
+export const LinearGradient = Dependency;
