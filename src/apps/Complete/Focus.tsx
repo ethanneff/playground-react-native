ed58558a8dd77c7b19c8d7216e110200a3aeaadf
Ethan Neff <ethan.neff@eneff.com>
2020-12-04 12:21:06 -0600

    docs: add todo

--- src/apps/Complete/Focus.tsx
+++ src/apps/Complete/Focus.tsx
@@ -14,6 +14,7 @@ import {Theme, useRootSelector} from '../../utils';
 // TODO: add list add text input
 // TODO: add card add text input
 // TODO: scroll down on card add
+// TODO: make color scheme similar to todoist
 
 type Column = {
   name: string;
