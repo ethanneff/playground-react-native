19fc25b6ec0a18d7945f5dd3368760323fe308d6
Ethan Neff <ethan.neff@eneff.com>
2020-12-05 21:43:15 -0600

    fix: rerender cells more effenciently

--- src/apps/Complete/Focus.tsx
+++ src/apps/Complete/Focus.tsx
@@ -1,6 +1,6 @@
 import React, {memo, useCallback, useRef, useState} from 'react';
 import {FlatList, View} from 'react-native';
-import {Button, Screen, Text, TouchableOpacity} from '../../components';
+import {Button, Icon, Screen, Text, TouchableOpacity} from '../../components';
 import {useColor} from '../../hooks';
 import {getSmallestDimension} from '../../models';
 import {Theme, useRootSelector} from '../../utils';
@@ -12,21 +12,135 @@ import {Theme, useRootSelector} from '../../utils';
 // TODO: add card add text input
 // TODO: make color scheme similar to todoist
 // TODO: figure out max height for list (not 500)
-// TODO: break up card and list into separate files
+// TODO: figure out centering of list
 
-type List = {
-  name: string;
-  id: string;
-  items: Card[];
-};
-
-type Card = {
+type CardObject = {
   id: string;
   name: string;
 };
+
+type ListObject = {
+  name: string;
+  id: string;
+  items: CardObject[];
+};
+
+type CardProps = {
+  card: CardObject;
+  padding: number;
+  borderRadius: number;
+  backgroundColor: string;
+};
+
+const Card = memo(function Card({
+  card,
+  padding,
+  borderRadius,
+  backgroundColor,
+}: CardProps) {
+  return (
+    <TouchableOpacity
+      key={card.id}
+      style={{
+        borderRadius,
+        padding: padding,
+        backgroundColor,
+        marginBottom: padding / 2,
+      }}>
+      <Text title={card.name} />
+    </TouchableOpacity>
+  );
+});
+
+type ListProps = {
+  list: ListObject;
+  listColor: string;
+  cardColor: string;
+  borderRadius: number;
+  listWidth: number;
+  padding: number;
+  maxHeight: number;
+};
+
+const List = memo(function List({
+  list,
+  borderRadius,
+  listWidth,
+  listColor,
+  cardColor,
+  padding,
+  maxHeight,
+}: ListProps) {
+  const [cards, setCards] = useState<CardObject[]>(list.items);
+  const cardsRef = useRef<FlatList | null>(null);
+  const cardsLength = useRef(cards.length);
+
+  const onKeyExtractor = useCallback((item) => item.id, []);
+
+  const onCardSizeChange = useCallback(() => {
+    if (cards.length > cardsLength.current) {
+      cardsRef.current?.scrollToEnd();
+      cardsLength.current = cards.length;
+    }
+  }, [cards.length]);
+
+  const addCard = useCallback(() => {
+    const date = String(Date.now());
+    setCards((p) => [...p, {id: date, name: date}]);
+  }, []);
+
+  const onRenderItem = useCallback(
+    ({item}) => (
+      <Card
+        backgroundColor={cardColor}
+        borderRadius={borderRadius}
+        card={item}
+        key={item.id}
+        padding={padding}
+      />
+    ),
+    [borderRadius, cardColor, padding],
+  );
+
+  return (
+    <View>
+      <View
+        style={{
+          borderRadius,
+          width: listWidth,
+          backgroundColor: listColor,
+          padding: padding / 2,
+          marginRight: padding,
+        }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            paddingBottom: padding,
+          }}>
+          <Text emphasis="medium" title={list.name} type="h4" />
+          <TouchableOpacity>
+            <Icon name="dots-vertical" />
+          </TouchableOpacity>
+        </View>
+        <FlatList
+          data={cards}
+          keyExtractor={onKeyExtractor}
+          onContentSizeChange={onCardSizeChange}
+          ref={cardsRef}
+          renderItem={onRenderItem}
+          showsVerticalScrollIndicator={false}
+          style={{maxHeight}}
+        />
+        <Button center color="primary" onPress={addCard} title="add card" />
+      </View>
+    </View>
+  );
+});
 
 export const Focus = memo(function Focus() {
-  const [lists, setLists] = useState<List[]>([
+  const [lists, setLists] = useState<ListObject[]>([
     {
       id: '1',
       name: 'Backlog',
@@ -63,12 +177,11 @@ export const Focus = memo(function Focus() {
   ]);
   const color = useColor();
   const width = useRootSelector(getSmallestDimension);
-  const listWidth = width * 0.6;
+  const listWidth = width * 0.66;
   const padding = Theme.padding.p04;
   const listSize = listWidth + padding;
   const borderRadius = Theme.padding.p02;
   const listsRef = useRef<FlatList | null>(null);
-  const cardsRef = useRef<{[key in string]: FlatList | null}>({});
   const listsCount = useRef(lists.length);
 
   const getItemId = useCallback((item) => item.id, []);
@@ -78,105 +191,22 @@ export const Focus = memo(function Focus() {
     setLists((p) => [...p, {id: date, name: date, items: []}]);
   }, []);
 
-  const addCard = useCallback(
-    (columnIndex: string) => () => {
-      const date = String(Date.now());
-      setLists((p) =>
-        p.map((column) => {
-          if (column.id !== columnIndex) {
-            return column;
-          }
-          const newItem = {id: date, name: date};
-          return {...column, items: [...column.items, newItem]};
-        }),
-      );
-    },
-    [],
-  );
-
-  const renderCard = useCallback(
-    ({item}) => {
-      return (
-        <TouchableOpacity
-          key={item.id}
-          style={{
-            borderRadius,
-            padding: padding,
-            backgroundColor: color.surface,
-            marginBottom: padding / 2,
-          }}>
-          <Text title={item.name} />
-        </TouchableOpacity>
-      );
-    },
-    [borderRadius, color.surface],
-  );
-
-  const onCardRef = useCallback(
-    (listId: string) => (ref: FlatList | null) => {
-      cardsRef.current[listId] = ref;
-    },
-    [],
-  );
-
-  const onCardSizeChange = useCallback(
-    (listId: string) => () => {
-      cardsRef.current[listId]?.scrollToEnd();
-    },
-    [],
-  );
-
   const renderList = useCallback(
     ({item}) => {
       return (
-        <View>
-          <View
-            key={item.id}
-            style={{
-              borderRadius,
-              width: listWidth,
-              backgroundColor: color.background,
-              padding: padding / 2,
-              marginRight: padding,
-            }}>
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-                paddingBottom: padding,
-              }}>
-              <Text emphasis="medium" title={item.name} type="h4" />
-              <Button title="edit" />
-            </View>
-            <FlatList
-              data={item.items}
-              keyExtractor={getItemId}
-              onContentSizeChange={onCardSizeChange(item.id)}
-              ref={onCardRef(item.id)}
-              renderItem={renderCard}
-              showsVerticalScrollIndicator={false}
-              style={{maxHeight: 500}}
-            />
-            <Button
-              center
-              color="primary"
-              onPress={addCard(item.id)}
-              title="add card"
-            />
-          </View>
-        </View>
+        <List
+          borderRadius={borderRadius}
+          cardColor={color.surface}
+          key={item.id}
+          list={item}
+          listColor={color.background}
+          listWidth={listWidth}
+          maxHeight={500}
+          padding={padding}
+        />
       );
     },
-    [
-      borderRadius,
-      listWidth,
-      color.background,
-      onCardRef,
-      getItemId,
-      renderCard,
-      addCard,
-    ],
+    [borderRadius, color.surface, color.background, listWidth, padding],
   );
 
   const renderAddList = useCallback(() => {
@@ -191,7 +221,7 @@ export const Focus = memo(function Focus() {
         <Button center color="primary" onPress={addColumn} title="add list" />
       </View>
     );
-  }, [addColumn, borderRadius, color.background, listWidth]);
+  }, [addColumn, borderRadius, color.background, listWidth, padding]);
 
   const getItemLayout = useCallback(
     (_, index) => ({
@@ -203,8 +233,11 @@ export const Focus = memo(function Focus() {
   );
 
   const onListSizeChange = useCallback(() => {
-    if (lists.length !== listsCount.current) {
-      listsRef.current?.scrollToIndex({index: lists.length - 1});
+    if (lists.length > listsCount.current) {
+      listsRef.current?.scrollToIndex({
+        index: lists.length - 1,
+        viewPosition: 0.5,
+      });
       listsCount.current = lists.length;
     }
   }, [listsRef, lists, listsCount]);
@@ -213,7 +246,7 @@ export const Focus = memo(function Focus() {
     <Screen title="Focus">
       <FlatList
         ListFooterComponent={renderAddList}
-        contentContainerStyle={{padding: padding}}
+        contentContainerStyle={{padding}}
         data={lists}
         decelerationRate="fast"
         getItemLayout={getItemLayout}
