2223dad0b9ab157b779543329d57b4be0d179686
Ethan Neff <ethan.neff@eneff.com>
2020-12-09 22:13:26 -0600

    fix: add sample graph to progress

---
+++ src/apps/Complete/screens/Reflect/Graph.tsx
@@ -0,0 +1,59 @@
+import {scaleLinear, scaleTime} from 'd3-scale';
+import * as shape from 'd3-shape';
+import * as React from 'react';
+import {StyleSheet, View} from 'react-native';
+import Svg, {Defs, LinearGradient, Path, Stop} from 'react-native-svg';
+
+interface DataPoint {
+  date: number;
+  value: number;
+}
+
+interface GraphProps {
+  data: DataPoint[];
+  width: number;
+  height: number;
+}
+
+const strokeWidth = 4;
+const padding = strokeWidth / 2;
+const getDomain = (domain: number[]) => [
+  Math.min(...domain),
+  Math.max(...domain),
+];
+
+export const Graph = ({
+  data,
+  width,
+  height,
+}: GraphProps): React.ReactElement => {
+  const scaleX = scaleTime()
+    .domain(getDomain(data.map((d) => d.date)))
+    .range([0, width]);
+  const scaleY = scaleLinear()
+    .domain(getDomain(data.map((d) => d.value)))
+    .range([height - padding, padding]);
+  const d = shape
+    .line<DataPoint>()
+    .x((p) => scaleX(p.date))
+    .y((p) => scaleY(p.value))
+    .curve(shape.curveBasis)(data) as string;
+  return (
+    <View style={{width, height}}>
+      <Svg style={StyleSheet.absoluteFill}>
+        <Defs>
+          <LinearGradient id="gradient" x1="50%" x2="50%" y1="0%" y2="100%">
+            <Stop offset="0%" stopColor="#cee3f9" />
+            <Stop offset="80%" stopColor="#ddedfa" />
+            <Stop offset="100%" stopColor="#feffff" />
+          </LinearGradient>
+        </Defs>
+        <Path
+          d={`${d}L ${width} ${height} L 0 ${height}`}
+          fill="url(#gradient)"
+        />
+        <Path fill="transparent" stroke="#3977e3" {...{d, strokeWidth}} />
+      </Svg>
+    </View>
+  );
+};
--- src/apps/Complete/screens/Reflect/index.tsx
+++ src/apps/Complete/screens/Reflect/index.tsx
@@ -1,10 +1,11 @@
 import React, {memo} from 'react';
-import {ScrollView, View} from 'react-native';
+import {Dimensions, ScrollView, View} from 'react-native';
 import {Calendar, Screen, Text} from '../../../../components';
 import {useColor} from '../../../../hooks';
 import {Theme} from '../../../../utils';
 import {Card} from '../../components/Card';
 import {config} from '../../configs';
+import {Graph} from './Graph';
 
 // TODO: add journal
 // TODO: add historical data
@@ -23,6 +24,15 @@ const Title = ({name}: TitleProps) => {
     />
   );
 };
+
+const data = [
+  {date: new Date(2018, 9, 1).getTime(), value: 0},
+  {date: new Date(2018, 9, 16).getTime(), value: 0},
+  {date: new Date(2018, 9, 17).getTime(), value: 200},
+  {date: new Date(2018, 10, 1).getTime(), value: 200},
+  {date: new Date(2018, 10, 2).getTime(), value: 300},
+  {date: new Date(2018, 10, 5).getTime(), value: 300},
+];
 
 export const Reflect = memo(function Reflect() {
   const color = useColor();
@@ -60,7 +70,17 @@ export const Reflect = memo(function Reflect() {
           backgroundColor={color.background}
           borderRadius={config.borderRadius}
           padding={config.padding}>
-          <Title name="Review (Progress)" />
+          <View>
+            <Title name="Review (Progress)" />
+            <View
+              style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+              <Graph
+                data={data}
+                height={200}
+                width={Dimensions.get('window').width - config.padding * 4}
+              />
+            </View>
+          </View>
         </Card>
         <Card
           backgroundColor={color.background}
