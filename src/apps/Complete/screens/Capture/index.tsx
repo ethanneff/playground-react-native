7624964673418e9539964411faf6c8541a8adc14
Ethan Neff <ethan.neff@eneff.com>
2020-12-15 19:56:57 -0700

    fix: make capture work based on keyboard height

--- src/apps/Complete/screens/Capture/index.tsx
+++ src/apps/Complete/screens/Capture/index.tsx
@@ -1,34 +1,44 @@
 import {useNavigation} from '@react-navigation/native';
 import React, {memo, useCallback, useState} from 'react';
-import {LayoutChangeEvent, View} from 'react-native';
+import {
+  Dimensions,
+  LayoutChangeEvent,
+  View,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import {Button, Screen, Text} from '../../../../components';
 import {useColor} from '../../../../hooks';
-import {useRootSelector} from '../../../../utils';
+import {Theme} from '../../../../utils';
 import {Card} from '../../components/Card';
 import {List} from '../../components/List';
 import {config} from '../../configs';
 import {ListObject} from '../../types';
+import {useKeyboard} from './useKeyboard';
 
 // TODO: update addItem to be used for organize button
 
+const width = Dimensions.get('window').width - config.padding * 2;
 export const Capture = memo(function Capture() {
   const color = useColor();
   const {navigate} = useNavigation();
   const navBack = useCallback(() => navigate('admin'), [navigate]);
+  const {keyboardHeight, dismissKeyboard} = useKeyboard();
   const [dimensions, setDimensions] = useState({
-    container: {width: 0, height: 0},
-    button: {width: 0, height: 0},
-    header: {width: 0, height: 0},
+    container: 0,
+    button: 0,
+    header: 0,
   });
-  const keyboardVisible = useRootSelector(
-    (state) => state.device.keyboardVisible,
-  );
-  const buttonHeight = keyboardVisible ? 0 : dimensions.button.height;
-  const listMaxHeight =
-    dimensions.container.height -
-    buttonHeight -
-    dimensions.header.height -
-    config.padding * 12;
+
+  const listHeight =
+    keyboardHeight === 0
+      ? dimensions.container -
+        dimensions.button -
+        dimensions.header -
+        config.padding * 15
+      : dimensions.container -
+        dimensions.header -
+        keyboardHeight -
+        config.padding * 8;
 
   const [list] = useState<ListObject>({
     id: '1',
@@ -44,70 +54,80 @@ export const Capture = memo(function Capture() {
 
   const onLayout = useCallback(
     (key: string) => (event: LayoutChangeEvent) => {
-      const {width, height} = event.nativeEvent.layout;
-      setDimensions((p) => ({...p, [key]: {width, height}}));
+      const {height} = event.nativeEvent.layout;
+      if (key === 'container' && dimensions.container > 0) {
+        return;
+      }
+      setDimensions((p) => ({...p, [key]: height}));
     },
-    [],
+    [dimensions.container],
   );
 
   return (
     <Screen onLeftPress={navBack} title="Capture">
-      <View
+      <TouchableWithoutFeedback
         onLayout={onLayout('container')}
-        style={{
-          backgroundColor: color.surface,
-          flex: 1,
-          padding: config.padding,
-        }}>
-        <Card
-          backgroundColor={color.background}
-          borderRadius={config.borderRadius}
-          onLayout={onLayout('header')}
-          padding={config.padding}>
-          <View>
-            <Text
-              center
-              emphasis="high"
-              style={{paddingBottom: config.padding}}
-              title="Record every thought"
-              type="h4"
-            />
-            <Text
-              center
-              emphasis="medium"
-              title="Clear your mind. Keep focus on the most important. Organize periodically."
-            />
-          </View>
-        </Card>
-        <View>
-          <List
-            addButtonPlaceholder="Item title..."
-            addButtonTitle="Add item"
-            borderRadius={config.borderRadius}
-            cardColor={color.surface}
-            key={list.id}
-            list={list}
-            listColor={color.background}
-            maxHeight={listMaxHeight}
-            padding={config.padding}
-          />
+        onPress={dismissKeyboard}
+        style={{flex: 1}}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: color.surface,
+            padding: config.padding,
+          }}>
+          {!dimensions.container ? null : (
+            <View>
+              <Card
+                backgroundColor={color.background}
+                borderRadius={config.borderRadius}
+                onLayout={onLayout('header')}
+                padding={config.padding}>
+                <View>
+                  <Text
+                    center
+                    emphasis="high"
+                    style={{paddingBottom: config.padding}}
+                    title="Record every thought"
+                    type="h4"
+                  />
+                  <Text
+                    center
+                    emphasis="medium"
+                    title="Clear your mind. Focus on the most important. Organize periodically."
+                  />
+                </View>
+              </Card>
+              <List
+                addButtonPlaceholder="Item title..."
+                addButtonTitle="Add item"
+                borderRadius={config.borderRadius}
+                itemColor={color.surface}
+                itemHeight={Theme.padding.p18}
+                itemWidth={width - config.padding * 2}
+                key={list.id}
+                list={list}
+                listColor={color.background}
+                listHeight={listHeight}
+                listWidth={width}
+                padding={config.padding}
+              />
+              <Card
+                backgroundColor={color.background}
+                borderRadius={config.borderRadius}
+                onLayout={onLayout('button')}
+                padding={config.padding / 2}>
+                <Button
+                  center
+                  color="primary"
+                  disable={list.items.length === 0}
+                  onPress={onOrganize}
+                  title="Organize"
+                />
+              </Card>
+            </View>
+          )}
         </View>
-        {keyboardVisible ? null : (
-          <Card
-            backgroundColor={color.background}
-            borderRadius={config.borderRadius}
-            onLayout={onLayout('button')}
-            padding={config.padding / 2}>
-            <Button
-              center
-              color="primary"
-              disable={list.items.length === 0}
-              onPress={onOrganize}
-              title="Organize"
-            />
-          </Card>
-        )}
-      </View>
+      </TouchableWithoutFeedback>
     </Screen>
   );
 });
