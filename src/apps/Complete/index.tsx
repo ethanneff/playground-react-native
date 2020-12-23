50681a3744024d7511c6394a8ce0d28eaaf148fd
Ethan Neff <ethan.neff@eneff.com>
2020-12-23 14:12:26 -0700

    docs: add notes on how everything works

--- src/apps/Complete/index.tsx
+++ src/apps/Complete/index.tsx
@@ -92,3 +92,69 @@ export default memo(function Complete() {
     </RootStack.Navigator>
   );
 });
+
+/*
+
+# Data
+
+- user
+-> collections [projects, lists]
+-> boards [home, work, town] | [meeting notes, book summaries, checklists]
+-> lists [backlog, todo, in progress, done] | [draft, final]
+-> item [find comb, clear emails, daily standup]
+
+# Screens
+
+## Capture
+
+### Inbox (list -> item -> details)
+- "add grey list" -> @app
+- "find comb" -> @home
+- "clear emails" -> @work
+- "put $20 in phone" -> @town
+- "drink water" -> @after-waking-up
+- "intensity + focus = deep work" -> book summaries
+
+## Organize
+
+### Projects (item -> lists -> item -> details )
+- @home -> (backlog, todo, in progress, done xx, done xx)
+- @town
+- @work
+- @gym
+- @app
+
+### Lists (item -> list -> item -> detail)
+- meeting notes
+ - one-on-one
+ - daily standup
+- book summaries
+ - the one thing
+ - getting things done
+ - eat that frog
+- gift ideas
+ - mom
+ - dad
+ - girlfriend
+- checklists
+ - after waking up
+ - after entering bathroom
+
+## Reflect
+
+### Purpose
+
+### Goals
+
+### Review (Progress)
+
+### Reflect (Journal)
+
+## Account
+
+### Profile
+
+### Notifications
+
+### Payment
+*/
