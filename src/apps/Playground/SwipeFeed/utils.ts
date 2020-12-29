cac4451b46a45e7eaecd5993d82346836c71d09e
Ethan Neff <ethan.neff@eneff.com>
2020-12-29 15:17:08 -0800

    fix: update tests

---
+++ src/apps/Playground/SwipeFeed/utils.ts
@@ -0,0 +1,21 @@
+import dayjs, {Dayjs} from 'dayjs';
+
+export const formatRelativeDate = (date: Dayjs): string => {
+  const now = dayjs();
+  const years = now.diff(date, 'year');
+  const weeks = now.diff(date, 'week');
+  const days = now.diff(date, 'day');
+  const hours = now.diff(date, 'hour');
+  const minutes = now.diff(date, 'minute');
+  return years
+    ? `${years}y`
+    : weeks
+    ? `${weeks}w`
+    : days
+    ? `${days}d`
+    : hours
+    ? `${hours}h`
+    : minutes
+    ? `${minutes}m`
+    : '';
+};
