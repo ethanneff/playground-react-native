4c3022d00fec122e1297b47dce1593d16f0d49b2
Ethan Neff <ethan.neff@eneff.com>
2020-12-28 21:45:43 -0800

    fix: link project to redux

--- src/apps/Complete/screens/Project/index.tsx
+++ src/apps/Complete/screens/Project/index.tsx
@@ -1,79 +1,33 @@
 import {useNavigation} from '@react-navigation/native';
-import React, {memo, useCallback, useRef, useState} from 'react';
-import {FlatList, LayoutChangeEvent, View} from 'react-native';
+import React, {memo, useCallback, useState} from 'react';
+import {LayoutChangeEvent, Platform} from 'react-native';
 import {Screen} from '../../../../components';
 import {useColor} from '../../../../hooks';
 import {getSmallestDimension} from '../../../../models';
 import {useRootSelector} from '../../../../utils';
-import {AddItem} from '../../components/AddItem';
-import {List} from '../../components/List';
+import {Board, HandleKeyboard} from '../../components';
 import {config} from '../../configs';
-import {ListObject} from '../../types';
 
 // TODO: add landing page (actionables + record)
-// TODO: create data layer
-
-// TODO: fix keyboard scrolling (keyboard aware + recycler view)
-
 // TODO: figure out centering of list
 
 export const Project = memo(function Project() {
-  const [lists, setLists] = useState<ListObject[]>([
-    {
-      id: '1',
-      title: 'Backlog',
-      items: [
-        {
-          id: '11',
-          title: 'clean desk',
-        },
-        {
-          id: '22',
-          title: 'clean room',
-        },
-        {
-          id: '33',
-          title: 'brush teeth',
-        },
-      ],
-    },
-    {
-      id: '2',
-      title: 'Todo',
-      items: [],
-    },
-    {
-      id: '3',
-      title: 'In Progress',
-      items: [],
-    },
-    {
-      id: '4',
-      title: 'Done',
-      items: [],
-    },
-  ]);
   const {goBack} = useNavigation();
   const color = useColor();
   const screenWidth = useRootSelector(getSmallestDimension);
   const listWidth = screenWidth * 0.7;
-  const listSize = listWidth + config.padding; // TODO: deal with marginRight and FlatList padding
-  const listsRef = useRef<FlatList | null>(null);
-  const listsCount = useRef(lists.length);
+  const [container, setContainer] = useState(0);
+  const android = Platform.OS === 'android';
   const keyboardHeight = useRootSelector(
     (state) => state.device.keyboardHeight,
   );
-  const [container, setContainer] = useState(0);
-  const keyboardPadding = keyboardHeight
-    ? config.padding * 8
-    : config.padding * 13;
-  const listMaxHeight = container - keyboardPadding - keyboardHeight;
-  const getItemId = useCallback((item) => item.id, []);
 
-  const addList = useCallback((title: string) => {
-    const date = String(Date.now());
-    setLists((p) => [...p, {id: date, title, items: []}]);
-  }, []);
+  const listMaxHeight =
+    keyboardHeight === 0
+      ? container - (android ? config.padding * 8 : config.padding * 12)
+      : container -
+        keyboardHeight -
+        (android ? config.padding * 3 : config.padding * 7);
 
   const onLayout = useCallback(
     (event: LayoutChangeEvent) => {
@@ -86,88 +40,22 @@ export const Project = memo(function Project() {
     [container],
   );
 
-  const renderList = useCallback(
-    ({item}) => {
-      return (
-        <List
-          addButtonPlaceholder="Card title..."
-          addButtonTitle="Add card"
-          borderRadius={config.borderRadius}
-          itemColor={color.surface}
-          key={item.id}
-          list={item}
-          listColor={color.background}
-          listWidth={listWidth}
-          maxHeight={listMaxHeight}
-          orientation="horizontal"
-          padding={config.padding}
-        />
-      );
-    },
-    [color.surface, color.background, listWidth, listMaxHeight],
-  );
-
-  const renderAddList = useCallback(() => {
-    return (
-      <AddItem
-        backgroundColor={color.background}
-        borderRadius={config.borderRadius}
-        buttonTitle="Add List"
-        inputPlaceholder="List title..."
-        inputType="h4"
-        itemWidth={listWidth}
-        onAdd={addList}
-      />
-    );
-  }, [addList, color.background, listWidth]);
-
-  const getItemLayout = useCallback(
-    (_, index) => ({
-      length: listSize,
-      offset: listSize * index,
-      index,
-    }),
-    [listSize],
-  );
-
-  const onListSizeChange = useCallback(() => {
-    if (lists.length > listsCount.current) {
-      listsRef.current?.scrollToIndex({
-        index: lists.length - 1,
-        viewPosition: 0.5,
-      });
-      listsCount.current = lists.length;
-    }
-  }, [lists.length]);
-
   const navBack = useCallback(() => {
     goBack();
   }, [goBack]);
 
   return (
     <Screen onLeftPress={navBack} title="Focus">
-      <View
+      <HandleKeyboard
+        backgroundColor={color.surface}
         onLayout={onLayout}
-        style={{flex: 1, backgroundColor: color.surface}}>
-        {!container ? null : (
-          <FlatList
-            ListFooterComponent={renderAddList} // TODO: make default FlatList with this setting
-            contentContainerStyle={{padding: config.padding}}
-            data={lists}
-            decelerationRate="fast"
-            getItemLayout={getItemLayout}
-            horizontal
-            keyExtractor={getItemId}
-            keyboardShouldPersistTaps="handled"
-            onContentSizeChange={onListSizeChange}
-            ref={listsRef}
-            renderItem={renderList}
-            showsHorizontalScrollIndicator={false}
-            snapToAlignment="center"
-            snapToInterval={listSize}
-          />
-        )}
-      </View>
+        render={container > 0}>
+        <Board
+          boardId="4887e24c-9871-4deb-9eae-0e0bb4b9b860"
+          listMaxHeight={listMaxHeight}
+          listWidth={listWidth}
+        />
+      </HandleKeyboard>
     </Screen>
   );
 });
