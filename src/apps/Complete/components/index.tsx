79fe2167b2024071d7cfd92b4eed4a2f71107902
Ethan Neff <ethan.neff@eneff.com>
2020-12-31 14:20:29 -0800

    fix: remove unused components

--- src/apps/Complete/components/index.tsx
+++ src/apps/Complete/components/index.tsx
@@ -4,10 +4,10 @@ export * from './AddList';
 export * from './Board';
 export * from './Card';
 export * from './HandleKeyboard';
-export * from './Item';
 export * from './List';
 export * from './ListHeader';
 export * from './ListHeaderInput';
 export * from './ListItem';
 export * from './ListItems';
 export * from './OrganizeButton';
+
