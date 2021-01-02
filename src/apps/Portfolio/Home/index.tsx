36a2fb8c94d521c4b27a9cdb8260aad96be7e9e2
Ethan Neff <ethan.neff@eneff.com>
2021-01-02 09:31:21 -0800

    fix: make portfolio navigate to admin

--- src/apps/Portfolio/Home/index.tsx
+++ src/apps/Portfolio/Home/index.tsx
@@ -2,11 +2,14 @@ import {useNavigation} from '@react-navigation/native';
 import React, {memo, useCallback} from 'react';
 import {Button, Screen} from '../../../components';
 
+// TODO: add state for login and logout
+
 export const Home = memo(function PortfolioHome() {
   const {navigate} = useNavigation();
   const navTo = useCallback((to: string) => () => navigate(to), [navigate]);
+  const navBack = useCallback(() => navigate('admin'), [navigate]);
   return (
-    <Screen gutter title="Home">
+    <Screen gutter onLeftPress={navBack} title="Home">
       <Button onPress={navTo('login')} title="logout" />
       <Button onPress={navTo('settings')} title="settings" />
       <Button onPress={navTo('notFound')} title="not found" />
