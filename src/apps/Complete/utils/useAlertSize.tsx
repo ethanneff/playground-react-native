ab2429ff4920c46eddc66135059d3478951aba2b
Ethan Neff <ethan.neff@eneff.com>
2021-01-03 12:41:57 -0800

    fix: change alert height to dynamic maxheight

--- src/apps/Complete/utils/useAlertSize.tsx
+++ src/apps/Complete/utils/useAlertSize.tsx
@@ -1,11 +1,11 @@
 import {useRootSelector} from '../../../utils';
 import {config} from '../configs';
 
-type UseAlertSize = {width: number; height: number};
+type UseAlertSize = {width: number; maxHeight: number};
 
 export const useAlertSize = (): UseAlertSize => {
   const dimensions = useRootSelector((s) => s.dimension.screen);
   const width = dimensions.width * config.alert.width;
-  const height = dimensions.height * config.alert.height;
-  return {width, height};
+  const maxHeight = dimensions.height * config.alert.height;
+  return {width, maxHeight};
 };
