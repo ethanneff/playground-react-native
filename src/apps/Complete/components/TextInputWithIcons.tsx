32d59b1323ab14bd9e436ddf5011f295de183e65
Ethan Neff <ethan.neff@eneff.com>
2021-01-03 20:39:43 -0800

    fix: remove child textinput so icon button can submit payload

--- src/apps/Complete/components/TextInputWithIcons.tsx
+++ src/apps/Complete/components/TextInputWithIcons.tsx
@@ -1,7 +1,9 @@
 import React, {memo, MutableRefObject, useCallback, useState} from 'react';
 import {TextInput as Original, View} from 'react-native';
 import {Icon, TextInput} from '../../../components';
+import {PointerEvents} from '../../../components/TextInput/types';
 import {useColor} from '../../../hooks';
+import {FontType} from '../../../utils';
 
 type Icon = {
   name: string;
@@ -13,28 +15,37 @@ type Icon = {
 
 type TextInputWithIconsProps = {
   value: string;
-  onRef: MutableRefObject<Original | null>;
+  placeholder: string;
   icons: Icon[];
+  onRef?: MutableRefObject<Original | null>;
+  onSubmit: (text: string) => void;
+  backgroundColor?: string;
+  pointerEvents?: PointerEvents;
+  type?: FontType;
 };
 
-const TextInputIsolated = memo(function TextInputIsolated({
-  onBlur,
-  onFocus,
-  onSubmit,
-  placeholder,
+export const TextInputWithIcons = memo(function TextInputWithIcons({
   value,
+  placeholder,
+  backgroundColor,
+  pointerEvents,
+  icons,
+  type,
+  onSubmit,
   onRef,
-}: TextInputIsolatedProps) {
+}: TextInputWithIconsProps) {
   const color = useColor();
+  const bgColor = backgroundColor || color.background;
   const [text, setText] = useState(value);
+  const [showControls, setShowControls] = useState(false);
 
   const onTextChange = useCallback((change) => {
     setText(change);
   }, []);
 
   const onFocusInternal = useCallback(() => {
-    onFocus();
-  }, [onFocus]);
+    setShowControls(true);
+  }, []);
 
   const onSubmitInternal = useCallback(() => {
     onSubmit(text);
@@ -42,67 +53,26 @@ const TextInputIsolated = memo(function TextInputIsolated({
 
   const onBlurInternal = useCallback(() => {
     setText(value);
-    onBlur();
-  }, [onBlur, value]);
+    setShowControls(false);
+  }, [value]);
 
-  return (
-    <TextInput
-      backgroundColor={color.surface}
-      flex
-      onBlur={onBlurInternal}
-      onChangeText={onTextChange}
-      onFocus={onFocusInternal}
-      onRef={onRef}
-      onSubmitEditing={onSubmitInternal}
-      placeholder={placeholder}
-      pointerEvents="none"
-      returnKeyType="done"
-      value={text}
-    />
-  );
-});
-
-type Icon = {
-  name: string;
-  handlePress: () => void;
-  hidden?: boolean;
-};
-
-type Icons = {
-  focus: Icon[];
-  blur: Icon[];
-};
-
-type TextInputWithIconsProps = {
-  value: string;
-  placeholder: string;
-  icons: Icons;
-  onRef: MutableRefObject<Original | null>;
-  onSubmit: (text: string) => void;
-};
-
-export const TextInputWithIcons = memo(function TextInputWithIcons({
-  value,
-  placeholder,
-  icons,
-  onSubmit,
-  onRef,
-}: TextInputWithIconsProps) {
-  const [showControls, setShowControls] = useState(false);
-
-  const onFocus = useCallback(() => setShowControls(true), []);
-  const onBlur = useCallback(() => setShowControls(false), []);
   const onIconPressInternal = useCallback((c) => () => c(text), [text]);
 
   return (
     <View style={{flex: 1, flexDirection: 'row'}}>
-      <TextInputIsolated
-        onBlur={onBlur}
-        onFocus={onFocus}
+      <TextInput
+        backgroundColor={bgColor}
+        flex
+        onBlur={onBlurInternal}
+        onChangeText={onTextChange}
+        onFocus={onFocusInternal}
         onRef={onRef}
-        onSubmit={onSubmit}
+        onSubmitEditing={onSubmitInternal}
         placeholder={placeholder}
-        value={value}
+        pointerEvents={pointerEvents}
+        returnKeyType="done"
+        type={type}
+        value={text}
       />
       <View style={{flexDirection: 'row', alignItems: 'center'}}>
         {icons.map((icon) =>
