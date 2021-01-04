900a46091de0e09e244b43d7e0836dfd22e52e8b
Ethan Neff <ethan.neff@eneff.com>
2021-01-04 08:18:38 -0800

    fix: update text input with icon on detail screen

--- src/apps/Complete/screens/ItemDetail/index.tsx
+++ src/apps/Complete/screens/ItemDetail/index.tsx
@@ -1,10 +1,10 @@
 import {useNavigation} from '@react-navigation/native';
-import React, {memo, useCallback, useState} from 'react';
+import React, {memo, useCallback} from 'react';
 import {Keyboard, View} from 'react-native';
-import {Button, Modal, Text, TextInput} from '../../../../components';
+import {Button, Modal, Text} from '../../../../components';
 import {useColor} from '../../../../hooks';
 import {Theme, useRootDispatch, useRootSelector} from '../../../../utils';
-import {Card} from '../../components';
+import {Card, TextInputWithIcons} from '../../components';
 import {removeItem, updateItem, updateListRemoveItem} from '../../models';
 
 // TODO: update does not go to previous screen
@@ -15,21 +15,13 @@ export const ItemDetail = memo(function ItemDetail() {
   const dispatch = useRootDispatch();
   const {goBack} = useNavigation();
   const color = useColor();
-  const navBack = useCallback(() => goBack(), [goBack]);
+  const navBack = useCallback(() => {
+    console.log('black');
+    goBack();
+  }, [goBack]);
   const itemId = useRootSelector((s) => s.completeItem.active);
   const listId = useRootSelector((s) => s.completeList.active);
   const item = useRootSelector((s) => s.completeItem.items[itemId || '']);
-
-  const [title, setTitle] = useState(item.title);
-  const [description, setDescription] = useState(item.description || '');
-
-  const onTitleChange = useCallback((v) => setTitle(v), []);
-  const onDescriptionChange = useCallback((v) => setDescription(v), []);
-
-  const onItemSave = useCallback(() => {
-    dispatch(updateItem({...item, title, description}));
-    Keyboard.dismiss();
-  }, [description, dispatch, item, title]);
 
   const onItemDelete = useCallback(() => {
     if (!itemId || !listId)
@@ -38,46 +30,55 @@ export const ItemDetail = memo(function ItemDetail() {
     dispatch(updateListRemoveItem({listId, itemId}));
   }, [dispatch, itemId, listId]);
 
+  const onItemClose = useCallback(() => {
+    console.log('here');
+    Keyboard.dismiss();
+  }, []);
+
+  const onItemSubmit = useCallback(
+    (type: string) => (text: string) => {
+      console.log(type, text);
+      dispatch(updateItem({...item, [type]: text}));
+      Keyboard.dismiss();
+    },
+    [dispatch, item],
+  );
+
+  const icons = useCallback(
+    (type: string) => [
+      {name: 'close', onPress: onItemClose, focus: true},
+      {
+        name: 'send',
+        onPress: onItemSubmit(type),
+        color: color.primary,
+        focus: true,
+        required: true,
+      },
+    ],
+    [color.primary, onItemClose, onItemSubmit],
+  );
+
   return !item ? null : (
     <Modal backgroundColor={color.surface} onBackgroundPress={navBack}>
       {!item ? (
-        <View>
-          <Text title="missing item" />
-        </View>
+        <Text title="missing item" />
       ) : (
         <View>
           <Card>
-            <Text
-              style={{paddingBottom: Theme.padding.p04}}
-              title="Title"
-              type="h3"
-            />
-            <TextInput
-              emphasis="medium"
-              flex
-              onBlur={onItemSave}
-              onChangeText={onTitleChange}
-              placeholder="list name..."
-              returnKeyType="done"
+            <TextInputWithIcons
+              icons={icons('title')}
+              onSubmit={onItemSubmit('title')}
+              placeholder="item title..."
               type="h4"
-              value={title}
+              value={item.title}
             />
           </Card>
           <Card>
-            <Text
-              style={{paddingBottom: Theme.padding.p04}}
-              title="Description"
-              type="h3"
-            />
-            <TextInput
-              emphasis="medium"
-              flex
-              onBlur={onItemSave}
-              onChangeText={onDescriptionChange}
-              placeholder="details..."
-              returnKeyType="done"
-              type="h4"
-              value={description}
+            <TextInputWithIcons
+              icons={icons('description')}
+              onSubmit={onItemSubmit('description')}
+              placeholder="item details..."
+              value={item.description || ''}
             />
           </Card>
           <Card>
