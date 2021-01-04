72b54c44f4e6723ac9fc5d124bbf37817b598a98
Ethan Neff <ethan.neff@eneff.com>
2021-01-04 09:10:37 -0800

    fix: add confirmation modal to delete item

--- src/apps/Complete/screens/ItemDetail/index.tsx
+++ src/apps/Complete/screens/ItemDetail/index.tsx
@@ -1,5 +1,5 @@
 import {useNavigation} from '@react-navigation/native';
-import React, {memo, useCallback} from 'react';
+import React, {memo, useCallback, useState} from 'react';
 import {Keyboard, View} from 'react-native';
 import {Button, Modal, Text} from '../../../../components';
 import {useColor} from '../../../../hooks';
@@ -9,7 +9,6 @@ import {removeItem, updateItem, updateListRemoveItem} from '../../models';
 
 // TODO: update does not go to previous screen
 // TODO: delete does not go to previous screen
-// TODO: delete needs confirmation alert
 
 export const ItemDetail = memo(function ItemDetail() {
   const dispatch = useRootDispatch();
@@ -22,13 +21,16 @@ export const ItemDetail = memo(function ItemDetail() {
   const itemId = useRootSelector((s) => s.completeItem.active);
   const listId = useRootSelector((s) => s.completeList.active);
   const item = useRootSelector((s) => s.completeItem.items[itemId || '']);
+  const [deleteModal, setDeleteModal] = useState(false);
 
   const onItemDelete = useCallback(() => {
     if (!itemId || !listId)
       throw new Error('missing listId or itemId on item detail screen');
     dispatch(removeItem(itemId));
     dispatch(updateListRemoveItem({listId, itemId}));
-  }, [dispatch, itemId, listId]);
+    setDeleteModal(false);
+    goBack();
+  }, [dispatch, goBack, itemId, listId]);
 
   const onItemClose = useCallback(() => {
     console.log('here');
@@ -58,6 +60,14 @@ export const ItemDetail = memo(function ItemDetail() {
     [color.primary, onItemClose, onItemSubmit],
   );
 
+  const onDeletePress = useCallback(() => {
+    setDeleteModal(true);
+  }, []);
+
+  const onDeleteClose = useCallback(() => {
+    setDeleteModal(false);
+  }, []);
+
   return !item ? null : (
     <Modal backgroundColor={color.surface} onBackgroundPress={navBack}>
       {!item ? (
@@ -82,17 +92,28 @@ export const ItemDetail = memo(function ItemDetail() {
             />
           </Card>
           <Card margin="bottom">
+      {!deleteModal ? null : (
+        <Modal
+          onBackgroundPress={onDeleteClose}
+          overlayColor
+          widthPercent={0.5}>
+          <View style={{alignItems: 'center'}}>
             <Text
               style={{paddingBottom: Theme.padding.p04}}
               title="Reminders"
               type="h3"
             />
-          </Card>
-          <Card>
-            <Button center onPress={onItemDelete} title="delete" />
-          </Card>
-        </View>
+            <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+              <Button
+                color="secondary"
+                onPress={onDeleteClose}
+                title="cancel"
+              />
+              <Button onPress={onItemDelete} title="delete" />
+            </View>
+          </View>
+        </Modal>
       )}
-    </Modal>
+    </>
   );
 });
