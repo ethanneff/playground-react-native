88e5b341bf88ff52d6492f7785c4f17184f34189
Ethan Neff <ethan.neff@eneff.com>
2021-01-03 20:40:25 -0800

    fix: make header use textinputwithicons

--- src/apps/Complete/components/ListHeader.tsx
+++ src/apps/Complete/components/ListHeader.tsx
@@ -1,13 +1,37 @@
-import React, {memo} from 'react';
-import {View} from 'react-native';
-import {Icon} from '../../../components';
-import {ListHeaderInput} from './ListHeaderInput';
+import React, {memo, useCallback} from 'react';
+import {Keyboard, View} from 'react-native';
+import {useColor} from '../../../hooks';
+import {useRootDispatch, useRootSelector} from '../../../utils';
+import {updateListTitle} from '../models';
+import {TextInputWithIcons} from './TextInputWithIcons';
 
 type ListHeaderProps = {
   listId: string;
 };
 
 export const ListHeader = memo(function ListHeader({listId}: ListHeaderProps) {
+  const dispatch = useRootDispatch();
+  const color = useColor();
+  const listTitle = useRootSelector((s) => s.completeList.items[listId].title);
+
+  const onSave = useCallback(
+    (title) => {
+      dispatch(updateListTitle({listId, title}));
+      Keyboard.dismiss();
+    },
+    [dispatch, listId],
+  );
+
+  const onDetail = useCallback(() => undefined, []);
+
+  const onClose = useCallback(() => Keyboard.dismiss(), []);
+
+  const icons = [
+    {name: 'close', onPress: onClose, focus: true},
+    {name: 'send', onPress: onSave, color: color.primary, focus: true},
+    {name: 'dots-horizontal', onPress: onDetail},
+  ];
+
   return (
     <View
       style={{
@@ -15,8 +39,13 @@ export const ListHeader = memo(function ListHeader({listId}: ListHeaderProps) {
         alignItems: 'center',
         justifyContent: 'space-between',
       }}>
-      <ListHeaderInput listId={listId} />
-      <Icon name="dots-horizontal" padded />
+      <TextInputWithIcons
+        icons={icons}
+        onSubmit={onSave}
+        placeholder="List title..."
+        type="h4"
+        value={listTitle}
+      />
     </View>
   );
 });
