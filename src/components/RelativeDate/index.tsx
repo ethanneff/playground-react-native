29bf2cb7ded7ba61fa0f54d2dc1596d07d919604
Ethan Neff <ethan.neff@eneff.com>
2021-01-05 14:23:20 -0800

    fix: update relative date to pass text props

--- src/components/RelativeDate/index.tsx
+++ src/components/RelativeDate/index.tsx
@@ -1,18 +1,31 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import React, {memo, useCallback, useEffect, useState} from 'react';
+import {FontEmphasis, FontType} from '../../utils';
 import {Text} from '../Text';
 import {TouchableOpacity} from '../TouchableOpacity';
 dayjs.extend(relativeTime);
 
 interface Props {
   date: number;
+  type?: FontType;
+  emphasis?: FontEmphasis;
+  format?: string;
 }
 
 const minute = 60 * 1000;
-export const RelativeDate = memo(function RelativeDate({date}: Props) {
+export const RelativeDate = memo(function RelativeDate({
+  date,
+  type,
+  emphasis,
+  format = 'MMM D YYYY, h:mm a',
+}: Props) {
   const [showRelativeDate, setShowRelativeDate] = useState(true);
   const [update, setUpdate] = useState(1);
+  const text =
+    showRelativeDate && update
+      ? dayjs(date).fromNow()
+      : dayjs(date).format(format);
 
   const toggleRelativeDate = useCallback(() => {
     setShowRelativeDate((prev) => !prev);
@@ -25,11 +38,7 @@ export const RelativeDate = memo(function RelativeDate({date}: Props) {
 
   return (
     <TouchableOpacity onPress={toggleRelativeDate}>
-      {showRelativeDate && update ? (
-        <Text title={dayjs(date).fromNow()} />
-      ) : (
-        <Text title={dayjs(date).format('MMM D YYYY, h:mm a')} />
-      )}
+      <Text emphasis={emphasis} title={text} type={type} />
     </TouchableOpacity>
   );
 });
