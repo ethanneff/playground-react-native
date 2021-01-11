2f388134a7ce802b3d4be1fea2eb99838f2d8d8e
Ethan Neff <ethan.neff@eneff.com>
2021-01-11 15:25:48 -0800

    docs: add todo to checklists

--- src/apps/Checklists/index.tsx
+++ src/apps/Checklists/index.tsx
@@ -1,9 +1,11 @@
-import React, {memo} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import React, {memo} from 'react';
 import {Home} from './Home';
 
 const noHeader = {headerShown: false};
 const Stack = createStackNavigator();
+
+// TODO: remove unused checklist screens and redux
 
 export default memo(function Checklists() {
   return (
