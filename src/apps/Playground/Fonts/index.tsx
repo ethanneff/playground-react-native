41d59add31c37e46feb39ea6e49a5fa664ec8ae4
Ethan Neff <ethan.neff@eneff.com>
2021-01-10 23:59:21 -0800

    fix: add scrollview to to fonts

--- src/apps/Playground/Fonts/index.tsx
+++ src/apps/Playground/Fonts/index.tsx
@@ -1,6 +1,7 @@
 import {useNavigation} from '@react-navigation/native';
 import React, {memo, useCallback} from 'react';
 import {Screen, Text} from '../../../components';
+import {ScrollView} from '../../../conversions';
 
 export const Fonts = memo(function PlaygroundFonts() {
   const {goBack} = useNavigation();
@@ -8,56 +9,61 @@ export const Fonts = memo(function PlaygroundFonts() {
 
   return (
     <Screen onLeftPress={navBack} title="Fonts">
-      <Text
-        title="H1: The quick brown fox jumps over the lazy dog."
-        type="h1"
-      />
-      <Text title="h2 The quick brown fox jumps over the lazy dog." type="h2" />
-      <Text
-        title="H3: The quick brown fox jumps over the lazy dog."
-        type="h3"
-      />
-      <Text
-        title="H4: The quick brown fox jumps over the lazy dog."
-        type="h4"
-      />
-      <Text
-        title="H5: The quick brown fox jumps over the lazy dog."
-        type="h5"
-      />
-      <Text
-        title="H6: The quick brown fox jumps over the lazy dog."
-        type="h6"
-      />
-      <Text
-        title="SUBTITLE1: The quick brown fox jumps over the lazy dog."
-        type="subtitle1"
-      />
-      <Text
-        title="SUBTITLE2: The quick brown fox jumps over the lazy dog."
-        type="subtitle2"
-      />
-      <Text
-        title="BODY1: The quick brown fox jumps over the lazy dog."
-        type="body1"
-      />
-      <Text
-        title="BODY2: The quick brown fox jumps over the lazy dog."
-        type="body2"
-      />
-      <Text
-        title="OVERLINE: The quick brown fox jumps over the lazy dog."
-        type="overline"
-      />
-      <Text
-        title="CAPTION: The quick brown fox jumps over the lazy dog."
-        type="caption"
-      />
-      <Text
-        title="BUTTON: The quick brown fox jumps over the lazy dog."
-        type="button"
-      />
-      <Text title="NOTHING: The quick brown fox jumps over the lazy dog." />
+      <ScrollView>
+        <Text
+          title="H1: The quick brown fox jumps over the lazy dog."
+          type="h1"
+        />
+        <Text
+          title="h2 The quick brown fox jumps over the lazy dog."
+          type="h2"
+        />
+        <Text
+          title="H3: The quick brown fox jumps over the lazy dog."
+          type="h3"
+        />
+        <Text
+          title="H4: The quick brown fox jumps over the lazy dog."
+          type="h4"
+        />
+        <Text
+          title="H5: The quick brown fox jumps over the lazy dog."
+          type="h5"
+        />
+        <Text
+          title="H6: The quick brown fox jumps over the lazy dog."
+          type="h6"
+        />
+        <Text
+          title="SUBTITLE1: The quick brown fox jumps over the lazy dog."
+          type="subtitle1"
+        />
+        <Text
+          title="SUBTITLE2: The quick brown fox jumps over the lazy dog."
+          type="subtitle2"
+        />
+        <Text
+          title="BODY1: The quick brown fox jumps over the lazy dog."
+          type="body1"
+        />
+        <Text
+          title="BODY2: The quick brown fox jumps over the lazy dog."
+          type="body2"
+        />
+        <Text
+          title="OVERLINE: The quick brown fox jumps over the lazy dog."
+          type="overline"
+        />
+        <Text
+          title="CAPTION: The quick brown fox jumps over the lazy dog."
+          type="caption"
+        />
+        <Text
+          title="BUTTON: The quick brown fox jumps over the lazy dog."
+          type="button"
+        />
+        <Text title="NOTHING: The quick brown fox jumps over the lazy dog." />
+      </ScrollView>
     </Screen>
   );
 });
