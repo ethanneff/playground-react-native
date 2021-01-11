b13be2c007864d5fd8d6f1fee43792379451bad0
Ethan Neff <ethan.neff@eneff.com>
2021-01-11 15:26:15 -0800

    fix: make style improvements to cant hurt me

--- src/apps/CantHurtMe/ProfileLevel.tsx
+++ src/apps/CantHurtMe/ProfileLevel.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {Image, StyleSheet, View} from 'react-native';
-import {Theme} from '../../utils';
-import {useColor} from '../../hooks';
 import {Text, TouchableOpacity} from '../../components';
+import {useColor} from '../../hooks';
+import {Theme} from '../../utils';
 
 interface Props {
   onPress?(): void;
@@ -25,12 +25,14 @@ export const ProfileLevel = ({onPress}: Props): JSX.Element => {
       width: Theme.padding.p18,
     },
     levelContainer: {
+      alignItems: 'center',
       backgroundColor: color.background,
       borderColor: color.secondary,
       borderRadius: Theme.padding.p20,
       borderWidth: 1,
       bottom: -Theme.padding.p02,
       elevation: 2,
+      flex: 1,
       height: Theme.padding.p08,
       justifyContent: 'center',
       position: 'absolute',
@@ -61,12 +63,12 @@ export const ProfileLevel = ({onPress}: Props): JSX.Element => {
   return (
     <TouchableOpacity disabled={!onPress} onPress={onPress}>
       <Image source={image} style={styles.imageContainer} />
-      <View>
+      <View style={{flex: 1}}>
         <View style={styles.progressContainer}>
           <View style={[styles.progressBar, {width}]} />
         </View>
         <View style={styles.levelContainer}>
-          <Text center title={String(level)} type="subtitle2" />
+          <Text title={String(level)} type="subtitle2" />
         </View>
       </View>
     </TouchableOpacity>
