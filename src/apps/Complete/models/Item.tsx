c7c09df7dd224a0260240e82b59cc99fc46ee345
Ethan Neff <ethan.neff@eneff.com>
2021-01-13 12:49:33 -0800

    fix: make item generic to work for boards and lists

--- src/apps/Complete/models/Item.tsx
+++ src/apps/Complete/models/Item.tsx
@@ -3,26 +3,43 @@ import {RootAction, RootState} from 'root-types';
 import {createAction, getType} from 'typesafe-actions';
 import {removeUser} from './User';
 
+type ItemUpdateIds = {parentItemId: string; itemId: string};
+type ItemDetailsIds = {parentItemId: string | null; itemId: string};
+type ItemProjectIds = {projectItemId: string};
+type ItemNav = {
+  parentItemId: string | null;
+  itemId: string | null;
+  projectItemId: string | null;
+};
+
 /* ACTIONS */
 export const createItem = createAction('complete/item/create')<Item>();
 export const updateItem = createAction('complete/item/update')<Item>();
 export const removeItem = createAction('complete/item/remove')<string>();
-export const setActiveItem = createAction('complete/item/setActive')<string>();
+export const updateItemAddItem = createAction(
+  'complete/item/addItem',
+)<ItemUpdateIds>();
+export const setNavItemProject = createAction(
+  'complete/item/setNavItemProject',
+)<ItemProjectIds>();
+export const setNavItemDetail = createAction(
+  'complete/item/setNavItemDetail',
+)<ItemDetailsIds>();
+export const updateItemRemoveItem = createAction(
+  'complete/item/removeItem',
+)<ItemUpdateIds>();
 export const completeItemActions = {
   createItem,
   removeItem,
   updateItem,
-  setActiveItem,
+  setNavItemProject,
+  setNavItemDetail,
+  updateItemAddItem,
+  updateItemRemoveItem,
 };
 
 /* SELECTORS */
 export const getItems = (state: RootState): Items => state.completeItem.items;
-export const getCurrentItem = (state: RootState): Item => {
-  const active = state.completeItem.active;
-  if (!active) throw new Error('missing current item');
-
-  return state.completeItem.items[active];
-};
 export const getActiveItemOrderByCreatedAt = createSelector(
   [getItems],
   (items) =>
@@ -33,26 +50,27 @@ export const getActiveItemOrderByCreatedAt = createSelector(
 
 /* INTERFACES */
 export type CompleteItemReducer = {
-  active: string | undefined;
+  nav: ItemNav;
   items: Items;
 };
 
 export type Item = {
   id: string;
   title: string;
-  description?: string;
+  description: string;
   active: boolean;
   createdAt: number;
   updatedAt: number;
-  childBoardId?: string;
   userId: string;
-  tags?: ReadonlyArray<string>;
+  tags: ReadonlyArray<string>;
+  editable: boolean;
+  children: ReadonlyArray<string>;
 };
 export type Items = {[key: string]: Item};
 
 /* REDUCER */
 const initialState: CompleteItemReducer = {
-  active: undefined,
+  nav: {projectItemId: null, parentItemId: null, itemId: null},
   items: {},
 };
 export const completeItemReducer = (
@@ -60,8 +78,15 @@ export const completeItemReducer = (
   action: RootAction,
 ): CompleteItemReducer => {
   switch (action.type) {
-    case getType(setActiveItem):
-      return {...state, active: action.payload};
+    case getType(setNavItemProject):
+    case getType(setNavItemDetail):
+      return {
+        ...state,
+        nav: {
+          ...state.nav,
+          ...action.payload,
+        },
+      };
     case getType(createItem):
       return {
         ...state,
@@ -78,6 +103,34 @@ export const completeItemReducer = (
           [action.payload.id]: {
             ...state.items[action.payload.id],
             ...action.payload,
+            updatedAt: Date.now(),
+          },
+        },
+      };
+    case getType(updateItemAddItem):
+      const addParent = state.items[action.payload.parentItemId];
+      return {
+        ...state,
+        items: {
+          ...state.items,
+          [action.payload.parentItemId]: {
+            ...addParent,
+            children: [...addParent.children, action.payload.itemId],
+            updatedAt: Date.now(),
+          },
+        },
+      };
+    case getType(updateItemRemoveItem):
+      const deleteParent = state.items[action.payload.parentItemId];
+      return {
+        ...state,
+        items: {
+          ...state.items,
+          [action.payload.parentItemId]: {
+            ...deleteParent,
+            children: deleteParent.children.filter(
+              (itemId) => itemId !== action.payload.itemId,
+            ),
             updatedAt: Date.now(),
           },
         },
