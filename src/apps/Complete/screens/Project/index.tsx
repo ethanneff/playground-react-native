fe483b8f3829ea2628a6fb1f04f20885e6b097cd
Ethan Neff <ethan.neff@eneff.com>
2021-01-14 13:37:02 -0800

    fix: update keyboard padding based on item type

--- src/apps/Complete/screens/Project/index.tsx
+++ src/apps/Complete/screens/Project/index.tsx
@@ -16,13 +16,21 @@ export const Project = memo(function Project() {
   const screenWidth = useRootSelector(getSmallestDimension);
   const {projectItemId} = useRootSelector((s) => s.completeItem.nav);
   if (!projectItemId) throw new Error('missing projectItemId on board screen');
+  const projectItemType = useRootSelector(
+    (s) => s.completeItem.items[projectItemId].type,
+  );
+  const projectItemTitle = useRootSelector(
+    (s) => s.completeItem.items[projectItemId].title,
+  );
   if (!projectItemType)
     throw new Error('missing projectItemType on board screen');
   const listWidth = screenWidth * 0.7;
   const [container, setContainer] = useState(0);
   const keyboardHeight = useKeyboardHeight();
+  const typePadding = config.padding(projectItemType === 'list' ? 12 : 0);
   const keyboardPadding = config.padding(keyboardHeight ? 28 : 48);
-  const listMaxHeight = container - keyboardHeight - keyboardPadding;
+  const listMaxHeight =
+    container - keyboardHeight - keyboardPadding + typePadding;
 
   const onLayout = useCallback(
     (event: LayoutChangeEvent) => {
