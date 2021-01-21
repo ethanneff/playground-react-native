b87fe97eadaa13ccb540a6b5c62e619aebe9af2b
Ethan Neff <ethan.neff@eneff.com>
2021-01-20 22:55:38 -0800

    fix: remove login from account

--- src/apps/Complete/screens/Account/index.tsx
+++ src/apps/Complete/screens/Account/index.tsx
@@ -4,8 +4,7 @@ import {Button, Modal, Text} from '../../../../components';
 import {useColor} from '../../../../hooks';
 import {config, useRootDispatch} from '../../../../utils';
 import {Card} from '../../components';
-import {createItem, createUser, removeUser, setActiveUser} from '../../models';
-import {getDefaultUserTemplate} from '../../utils/';
+import {removeUser} from '../../models';
 
 // TODO: figure out a place for this
 // TODO: add reminders
@@ -18,18 +17,8 @@ export const Account = memo(function Account() {
   const {goBack, navigate} = useNavigation();
   const navBack = useCallback(() => goBack(), [goBack]);
 
-  const onLogin = useCallback(() => {
-    const {user, items} = getDefaultUserTemplate();
-    items.map((item) => dispatch(createItem(item)));
-    dispatch(createUser(user));
-    navigate('main');
-  }, [dispatch, navigate]);
   const onLogout = useCallback(() => {
     dispatch(removeUser());
-    navigate('main');
-  }, [dispatch, navigate]);
-  const onSetActive = useCallback(() => {
-    dispatch(setActiveUser(true));
   }, [dispatch]);
 
   const onNavToAdmin = useCallback(() => navigate('admin'), [navigate]);
@@ -72,9 +61,7 @@ export const Account = memo(function Account() {
         />
         <Text center emphasis="medium" title="..." type="h4" />
       </Card>
-      <Button onPress={onLogin} title="login" />
       <Button onPress={onLogout} title="logout" />
-      <Button onPress={onSetActive} title="set active" />
       <Button onPress={onNavToAdmin} title="go to admin" />
     </Modal>
   );
