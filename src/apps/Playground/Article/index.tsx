7660755dae85447c9fbf06b70040c28709d3b494
Ethan Neff <ethan.neff@eneff.com>
2021-01-21 19:17:22 -0800

    fix: update article with scrollview and card

--- src/apps/Playground/Article/index.tsx
+++ src/apps/Playground/Article/index.tsx
@@ -1,15 +1,30 @@
 import {useNavigation} from '@react-navigation/native';
 import React, {memo, useCallback} from 'react';
-import {Screen} from '../../../components';
+import {Card, Screen} from '../../../components';
 import {Content} from '../../../components/Content';
+import {ScrollView} from '../../../conversions';
+import {useColor} from '../../../hooks';
+import {config} from '../../../utils';
 import {data} from './data';
 
 export const Article = memo(function PlaygroundArticle() {
   const {goBack} = useNavigation();
   const navBack = useCallback(() => goBack(), [goBack]);
+  const color = useColor();
   return (
-    <Screen onLeftPress={navBack} title="Article">
-      <Content body={data} />
+    <Screen dropShadow onLeftPress={navBack} title="Article">
+      <ScrollView
+        contentContainerStyle={{
+          paddingHorizontal: config.padding(4),
+          paddingVertical: config.padding(2),
+        }}
+        keyboardShouldPersistTaps="handled"
+        showsVerticalScrollIndicator={false}
+        style={{backgroundColor: color.surface}}>
+        <Card>
+          <Content sections={data} />
+        </Card>
+      </ScrollView>
     </Screen>
   );
 });
