a44657b045b7e8633cf41cea6061d213b39ace80
Ethan Neff <ethan.neff@eneff.com>
2021-09-22 22:15:40 -0600

    fix: deep readonly redux

--- src/types/root-types.d.ts
+++ src/types/root-types.d.ts
@@ -1,12 +1,17 @@
 declare module 'root-types' {
   import {ThunkAction} from 'redux-thunk';
+  import {DeepReadonly} from 'ts-essentials';
   import {ActionType, StateType} from 'typesafe-actions';
 
   export type RootStore = StateType<typeof import('../redux/core').store>;
 
-  export type RootState = StateType<typeof import('../redux/core').reducers>;
+  export type RootState = DeepReadonly<
+    StateType<typeof import('../redux/core').reducers>
+  >;
 
   export type RootAction = ActionType<typeof import('../redux/core').actions>;
+
+  export type RootDispatch = typeof import('../redux/root').store.dispatch;
 
   export type RootThunkAction<R> = ThunkAction<R, RootState, any, RootAction>;
 }
