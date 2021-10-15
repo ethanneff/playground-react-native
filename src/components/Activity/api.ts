5b5ae1de1b4115b47268db623e17dd7450773d8a
Ethan Neff <ethan.neff@eneff.com>
2021-10-15 00:00:26 -0600

    fix: update to RN 66

--- src/components/Activity/api.ts
+++ src/components/Activity/api.ts
@@ -5,19 +5,19 @@ const endpoint = 'https://coding-contributions.vercel.app/api/';
 
 const getGithubActivity = async (username: string): Promise<ApiResponse> => {
   const url = `${endpoint}/github?username=${username}`;
-  const { data } = await axios.get(url);
+  const { data } = await axios.get<ApiResponse>(url);
   return data;
 };
 
 const getLeetCodeActivity = async (username: string) => {
   const url = `${endpoint}/leetcode?username=${username}`;
-  const { data } = await axios.get(url);
+  const { data } = await axios.get<ApiResponse>(url);
   return data;
 };
 
 const getHackerRankActivity = async (username: string) => {
   const url = `${endpoint}/hackerrank?username=${username}`;
-  const { data } = await axios.get(url);
+  const { data } = await axios.get<ApiResponse>(url);
   return data;
 };
 
--- src/features/Animation/useDriver.tsx
+++ src/features/Animation/useDriver.tsx
@@ -1,8 +1,5 @@
 import { Platform } from 'react-native';
-import { useRootSelector } from '../../redux';
 
 export const useDriver = (): boolean => {
-  const emulator = useRootSelector((state) => state.device.isEmulator);
-
-  return !(process.env.JEST_WORKER_ID || emulator || Platform.OS === 'web');
+  return !(process.env.JEST_WORKER_ID || Platform.OS === 'web');
 };
