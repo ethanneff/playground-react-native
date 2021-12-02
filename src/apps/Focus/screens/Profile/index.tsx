f59b12d660d27f9509a2ad3b1f9fe4975d3d2cb3
Ethan Neff <ethan.neff@eneff.com>
2021-12-02 12:47:34 -0800

    fix: add profile template

--- src/apps/Focus/screens/Profile/index.tsx
+++ src/apps/Focus/screens/Profile/index.tsx
@@ -1,17 +1,72 @@
 import React, { memo } from 'react';
-import { ScrollView } from 'react-native';
-import { Button, Screen, Text } from '../../../../components';
+import { ScrollView, View } from 'react-native';
+import { Button, Card, Screen, Text } from '../../../../components';
+import { padding, useColor } from '../../../../features';
 
 export const Profile = memo(function Profile() {
+  const color = useColor();
+
   return (
     <Screen dropShadow title="Profile">
-      <ScrollView>
-        <Text title="Profile" />
-        <Button title="delete account" />
-        <Button title="log out" />
-        <Button title="notifications settings" />
-        <Button title="theme" />
-        <Button title="restore purchases" />
+      <ScrollView
+        contentContainerStyle={{
+          paddingHorizontal: padding(4),
+          paddingVertical: padding(2),
+        }}
+        style={{ backgroundColor: color.background.secondary }}
+      >
+        <Card>
+          <Text
+            emphasis="medium"
+            style={{ paddingBottom: padding(2) }}
+            title="Profile"
+            type="h4"
+          />
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Text title="avatar" />
+            <View>
+              <Text title="name" />
+              <Text title="email" />
+            </View>
+          </View>
+          <Button color="accent" title="upgrade to premium" />
+          <Button title="reset password" />
+          <Button title="delete account" />
+          <Button title="restore transactions" />
+          <Button title="log out" />
+        </Card>
+
+        <Card>
+          <Text
+            emphasis="medium"
+            style={{ paddingBottom: padding(2) }}
+            title="Preferences"
+            type="h4"
+          />
+          <Button title="intervals" />
+          <Button title="notifications" />
+          <Button title="theme" />
+        </Card>
+
+        <Card>
+          <Text
+            emphasis="medium"
+            style={{ paddingBottom: padding(2) }}
+            title="App"
+            type="h4"
+          />
+          <Button title="support" />
+          <Button title="feedback" />
+          <Button title="about" />
+          <Button title="terms of use" />
+          <Button title="privacy policy" />
+        </Card>
       </ScrollView>
     </Screen>
   );
