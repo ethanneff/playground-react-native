163f8cc6f104bffe0fd356a25d1db2c94b146ccf
Ethan Neff <ethan.neff@eneff.com>
2022-02-18 13:24:16 -0800

    fix: update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -54,6 +54,7 @@ module.exports = {
     'react/jsx-filename-extension': 'off',
     'react/require-default-props': 'off',
     // style
+    'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': [
       'error',
       { enableDangerousAutofixThisMayCauseInfiniteLoops: true },
