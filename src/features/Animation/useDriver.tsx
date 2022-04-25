c2eddb4d07cb4a732a946e1c02a76df138790e79
Ethan Neff <ethan.neff@eneff.com>
2022-04-24 20:07:17 -0700

    fix: disable useDriver on emulators

--- src/features/Animation/useDriver.tsx
+++ src/features/Animation/useDriver.tsx
@@ -1,5 +1,10 @@
 import { Platform } from 'react-native';
+import DeviceInfo from 'react-native-device-info';
 
 export const useDriver = (): boolean => {
-  return !(process.env.JEST_WORKER_ID || Platform.OS === 'web');
+  return !(
+    process.env.JEST_WORKER_ID ||
+    Platform.OS === 'web' ||
+    DeviceInfo.isEmulator()
+  );
 };
