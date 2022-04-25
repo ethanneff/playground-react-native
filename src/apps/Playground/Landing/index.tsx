c9ffed41b5a138727c4922cf3e3506cb89f0546e
Ethan Neff <ethan.neff@eneff.com>
2022-04-25 07:55:14 -0700

    fix: add 4 sections to playground

--- src/apps/Playground/Landing/index.tsx
+++ src/apps/Playground/Landing/index.tsx
@@ -1,11 +1,44 @@
 import { useNavigation } from '@react-navigation/native';
-import React, { memo, useCallback } from 'react';
-import { Button, FlatList, Screen } from '../../../components';
+import React, { memo, ReactNode, useCallback } from 'react';
+import { View } from 'react-native';
+import {
+  Card,
+  FlatList,
+  Screen,
+  Text,
+  TouchableOpacity,
+} from '../../../components';
 import { padding, useColors } from '../../../features';
 import { PortfolioNavigation, PortfolioRoutes } from '../../Portfolio/types';
 import { stackParams } from '../navParams';
 
 const screens = Object.keys(stackParams);
+
+type TitleProps = { title: string };
+
+const Title = ({ title }: TitleProps) => {
+  return (
+    <>
+      <Text emphasis="medium" title={title} type="h5" />
+      <View style={{ padding: padding(1) }} />
+    </>
+  );
+};
+
+type RowProps = { children: ReactNode };
+const Row = ({ children }: RowProps) => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'row',
+        paddingHorizontal: padding(4),
+      }}
+    >
+      {children}
+    </View>
+  );
+};
 
 export const Landing = memo(function Playground() {
   const colors = useColors();
@@ -15,21 +48,71 @@ export const Landing = memo(function Playground() {
     [navigate],
   );
   const renderItem = useCallback(
-    ({ item }) => <Button key={item} onPress={navToItem(item)} title={item} />,
+    ({ item }) => (
+      <TouchableOpacity key={item} onPress={navToItem(item)}>
+        <View style={{ paddingVertical: padding(2) }}>
+          <Text bold title={item} type="body2" />
+        </View>
+      </TouchableOpacity>
+    ),
     [navToItem],
   );
   const keyExtractor = useCallback((item: string) => item, []);
 
   return (
     <Screen dropShadow onLeftPress={goBack} title="Playground">
-      <FlatList
-        contentContainerStyle={{ paddingHorizontal: padding(4) }}
-        data={screens}
-        keyExtractor={keyExtractor}
-        keyboardShouldPersistTaps="handled"
-        renderItem={renderItem}
-        style={{ backgroundColor: colors.background.secondary }}
-      />
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: colors.background.secondary,
+          paddingVertical: padding(2),
+        }}
+      >
+        <Row>
+          <Card flex>
+            <Title title="components" />
+
+            <FlatList
+              data={screens}
+              keyExtractor={keyExtractor}
+              keyboardShouldPersistTaps="handled"
+              renderItem={renderItem}
+            />
+          </Card>
+          <View style={{ padding: padding(2) }} />
+          <Card flex>
+            <Title title="features" />
+
+            <FlatList
+              data={screens}
+              keyExtractor={keyExtractor}
+              keyboardShouldPersistTaps="handled"
+              renderItem={renderItem}
+            />
+          </Card>
+        </Row>
+        <Row>
+          <Card flex>
+            <Title title="games" />
+            <FlatList
+              data={screens}
+              keyExtractor={keyExtractor}
+              keyboardShouldPersistTaps="handled"
+              renderItem={renderItem}
+            />
+          </Card>
+          <View style={{ padding: padding(2) }} />
+          <Card flex>
+            <Title title="random" />
+            <FlatList
+              data={screens}
+              keyExtractor={keyExtractor}
+              keyboardShouldPersistTaps="handled"
+              renderItem={renderItem}
+            />
+          </Card>
+        </Row>
+      </View>
     </Screen>
   );
 });
