a8f0062cad0c432297d9e4eeb3825ec7a4d704fc
Ethan Neff <ethan.neff@eneff.com>
2022-04-28 20:16:23 -0700

    fix: make switch make tap noise

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -1,10 +1,21 @@
-import React, { RefAttributes } from 'react';
-import { SwitchProps } from 'react-native';
+import React, { memo, useCallback } from 'react';
 import { GestureSwitch } from '../../conversions';
+import { SoundManager } from '../../features';
 
 export type SwitchRef = GestureSwitch | null;
 
-export const Switch = (props: SwitchProps & RefAttributes<GestureSwitch>) => {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <GestureSwitch {...props} />;
+type Props = {
+  onValueChange: (value: boolean) => void;
+  value: boolean;
 };
+
+export const Switch = memo(function Switch({ value, onValueChange }: Props) {
+  const handleValueChange = useCallback(
+    (result: boolean) => {
+      SoundManager.play('tap');
+      onValueChange(result);
+    },
+    [onValueChange],
+  );
+  return <GestureSwitch onValueChange={handleValueChange} value={value} />;
+});
