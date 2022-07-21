29bfac7d0c0e1261be605ac1ab93034332287fa3
Ethan Neff <ethan.neff@eneff.com>
2022-07-20 20:50:08 -0700

    fix: make sound on slider change

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,2 +1,23 @@
-import Original from '@react-native-community/slider';
-export const Slider = Original;
+import Original, { SliderProps } from '@react-native-community/slider';
+import React, { useCallback } from 'react';
+import { SoundManager } from '../../features';
+
+export const Slider = ({ onSlidingComplete, ...rest }: SliderProps) => {
+  const handleSlidingComplete = useCallback(
+    (value: number) => {
+      if (!onSlidingComplete) return;
+      SoundManager.play('tap');
+      onSlidingComplete(value);
+    },
+    [onSlidingComplete],
+  );
+
+  return (
+    <Original
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...rest}
+      onSlidingComplete={handleSlidingComplete}
+      tapToSeek
+    />
+  );
+};
