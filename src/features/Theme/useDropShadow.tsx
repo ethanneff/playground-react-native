d5b3fd842e31760518b6d0e4d767531eda555ef5
Ethan Neff <ethan.neff@eneff.com>
2022-07-20 20:48:54 -0700

    fix: allow method to show drop shadow on top

--- src/features/Theme/useDropShadow.tsx
+++ src/features/Theme/useDropShadow.tsx
@@ -1,8 +1,11 @@
-type UseDropShadow = (elevation: number) => {
+type UseDropShadow = (
+  elevation: number,
+  height?: number,
+) => {
   elevation: number;
   shadowColor: string;
   shadowOffset: {
-    height: 2;
+    height: number;
     width: 0;
   };
   shadowOpacity: number;
@@ -11,14 +14,14 @@ type UseDropShadow = (elevation: number) => {
 };
 
 export const useDropShadow = (): UseDropShadow => {
-  return (elevation = 10) => {
+  return (elevation = 10, height = 2) => {
     const shadowOpacity = elevation * 0.036 + 0.12;
     const shadowRadius = elevation * 0.36 + 1.2;
     return {
       elevation,
       shadowColor: 'black',
       shadowOffset: {
-        height: 2,
+        height,
         width: 0,
       },
       shadowOpacity,
