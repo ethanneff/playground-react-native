92d5189078ef2e12825dc659ed347b0581ae0206
Ethan Neff <ethan.neff@eneff.com>
2022-07-22 21:42:03 -0700

    feat: add terms and privacy pages

---
+++ src/apps/Focus/screens/Privacy/index.tsx
@@ -0,0 +1,21 @@
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import React, { memo } from 'react';
+import { Screen, ScrollView, Text } from '../../../../components';
+import { UnAuthStackRoutes } from '../../types';
+
+export const Privacy = memo(function Privacy() {
+  const { goBack } =
+    useNavigation<StackNavigationProp<UnAuthStackRoutes, 'privacy'>>();
+
+  return (
+    <Screen
+      onLeftPress={goBack}
+      title="Privacy policy"
+    >
+      <ScrollView>
+        <Text title="ehlleo" />
+      </ScrollView>
+    </Screen>
+  );
+});
---
+++ src/apps/Focus/screens/Terms/index.tsx
@@ -0,0 +1,21 @@
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import React, { memo } from 'react';
+import { Screen, ScrollView, Text } from '../../../../components';
+import { UnAuthStackRoutes } from '../../types';
+
+export const Terms = memo(function Terms() {
+  const { goBack } =
+    useNavigation<StackNavigationProp<UnAuthStackRoutes, 'terms'>>();
+
+  return (
+    <Screen
+      onLeftPress={goBack}
+      title="Terms of service"
+    >
+      <ScrollView>
+        <Text title="ehlleo" />
+      </ScrollView>
+    </Screen>
+  );
+});
