164f6cf2476e9b190d7d8ee8112d86c334a4632c
Ethan Neff <ethan.neff@eneff.com>
2022-07-22 21:44:14 -0700

    feat: begin onboarding page

--- src/apps/Focus/screens/Onboarding/index.tsx
+++ src/apps/Focus/screens/Onboarding/index.tsx
@@ -1,3 +1,20 @@
-export const Onboarding = () => {
-  return null;
-};
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import React, { memo } from 'react';
+import { Screen, ScrollView, Text } from '../../../../components';
+import { UnAuthStackRoutes } from '../../types';
+
+export const Onboarding = memo(function Onboarding() {
+  const { goBack } = useNavigation<StackNavigationProp<UnAuthStackRoutes>>();
+
+  return (
+    <Screen
+      onLeftPress={goBack}
+      title="Begin your journey"
+    >
+      <ScrollView>
+        <Text title="ehlleo" />
+      </ScrollView>
+    </Screen>
+  );
+});
