d8d86ce6ce94229db4dd098c688c43649cad3fe3
Ethan Neff <ethan.neff@eneff.com>
2022-07-26 00:40:24 -0700

    feat: make download page wait until data

--- src/apps/Focus/screens/Download/index.tsx
+++ src/apps/Focus/screens/Download/index.tsx
@@ -1,19 +1,36 @@
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import Lottie from 'lottie-react-native';
-import React, { memo } from 'react';
-import { Screen, View } from '../../../../components';
+import React, { memo, useCallback, useEffect, useRef } from 'react';
+import { Screen, Text, View } from '../../../../components';
 import { getSmallestDimension, useRootSelector } from '../../../../redux';
+import { getLoaded } from '../../data';
 import { AuthStackRoutes } from '../../types';
 
+const minTime = 1500;
 export const Download = memo(function Download() {
-  const { goBack } =
+  const { navigate } =
     useNavigation<StackNavigationProp<AuthStackRoutes, 'download'>>();
   const width = useRootSelector(getSmallestDimension);
+  const loaded = useRootSelector(getLoaded);
   const imageSize = width * 0.65;
+  const init = useRef(Date.now());
+
+  const nav = useCallback(() => {
+    navigate('home');
+  }, [navigate]);
+
+  useEffect(() => {
+    if (!loaded) return;
+    const now = Date.now();
+    const diff = now - init.current;
+    const tooSoon = diff < minTime;
+    const next = minTime - diff;
+    setTimeout(nav, tooSoon ? next : 0);
+  }, [loaded, nav, navigate]);
 
   return (
-    <Screen onLeftPress={goBack}>
+    <Screen>
       <View
         center
         flex
@@ -28,6 +45,11 @@ export const Download = memo(function Download() {
             height: imageSize,
             alignSelf: 'center',
           }}
+        />
+        <Text
+          center
+          title="loading"
+          type="h5"
         />
       </View>
     </Screen>
