24d9c8b7e92558fd95c4e0ba46913fe1c09ccdcd
Ethan Neff <ethan.neff@eneff.com>
2022-07-26 00:35:52 -0700

    feat: add collapsible component

---
+++ src/components/Collapsible/index.tsx
@@ -0,0 +1,72 @@
+import React, { ReactNode, useCallback, useRef, useState } from 'react';
+import { Animated, Easing } from 'react-native';
+import { spacing, useColors, useDriver } from '../../features';
+import { Icon } from '../Icon';
+import { Text } from '../Text';
+import { TouchableOpacity } from '../TouchableOpacity';
+import { View } from '../View';
+
+type Props = {
+  children: ReactNode;
+  title: string;
+};
+
+export const Collapsible = ({ children, title }: Props) => {
+  const animation = useRef(new Animated.Value(0)).current;
+  const [open, setOpen] = useState(false);
+  const useNativeDriver = useDriver();
+  const colors = useColors();
+  const maxHeight = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 200000],
+  });
+  const icon = open ? 'minus' : 'plus';
+  const backgroundColor = open
+    ? colors.background.accent
+    : colors.background.secondary;
+  const color = open ? 'primaryB' : 'primaryA';
+
+  const handlePress = useCallback(() => {
+    const toValue = open ? 0 : 1;
+    Animated.timing(animation, {
+      toValue,
+      duration: 200,
+      easing: Easing.quad,
+      useNativeDriver,
+    }).start();
+    setOpen(Boolean(toValue));
+  }, [animation, open, useNativeDriver]);
+
+  return (
+    <View>
+      <TouchableOpacity onPress={handlePress}>
+        <View
+          row
+          style={{
+            paddingHorizontal: spacing(4),
+            paddingVertical: spacing(2),
+            justifyContent: 'space-between',
+            backgroundColor,
+          }}
+        >
+          <Text
+            color={color}
+            title={title}
+            type="h4"
+          />
+          <Icon
+            color={color}
+            name={icon}
+          />
+        </View>
+      </TouchableOpacity>
+      <Animated.View
+        style={{
+          maxHeight,
+        }}
+      >
+        <View style={{ padding: spacing(2) }}>{children}</View>
+      </Animated.View>
+    </View>
+  );
+};
