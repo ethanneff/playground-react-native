812e8a4e7cdb9fc6c035ec5b66a3c9d168af9340
Ethan Neff <ethan.neff@eneff.com>
2022-08-03 22:27:40 -0700

    fix: add logout on loading page

--- src/apps/Focus/screens/Download/index.tsx
+++ src/apps/Focus/screens/Download/index.tsx
@@ -1,33 +1,69 @@
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import Lottie from 'lottie-react-native';
-import React, { memo, useCallback, useEffect, useRef } from 'react';
-import { Screen, Text, View } from '../../../../components';
+import React, { memo, useCallback, useEffect, useRef, useState } from 'react';
+import { Animated } from 'react-native';
+import {
+  Button,
+  Screen,
+  Spacing,
+  Text,
+  TouchableOpacity,
+  View,
+} from '../../../../components';
+import { useColors } from '../../../../features';
 import { getSmallestDimension, useRootSelector } from '../../../../redux';
 import { getLoaded } from '../../data';
 import { AuthStackRoutes } from '../../types';
+import { useLogout } from '../../utils/useLogout';
+import { PulseAnimation } from './PulseAnimation';
+import { useHeartBeatAnimation } from './useHeartBeatAnimation';
 
 const minTime = 1500;
+const maxTime = 10000;
+
 export const Download = memo(function Download() {
   const { navigate } =
     useNavigation<StackNavigationProp<AuthStackRoutes, 'download'>>();
   const width = useRootSelector(getSmallestDimension);
   const loaded = useRootSelector(getLoaded);
+  const [showLogout, setShowLogout] = useState(false);
   const imageSize = width * 0.65;
+  const visible = useRef(true);
   const init = useRef(Date.now());
+  const heartBeatStyles = useHeartBeatAnimation();
+  const tapWidth = width / 1.5;
+  const colors = useColors();
+  const [pulses, setPulses] = useState([0]);
 
-  const nav = useCallback(() => {
+  const navNext = useCallback(() => {
+    visible.current = false;
     navigate('home');
   }, [navigate]);
 
+  const { handleLogout } = useLogout();
+
   useEffect(() => {
-    if (!loaded) return;
+    const timer = setTimeout(() => {
+      if (!visible.current) return;
+      setShowLogout(true);
+    }, maxTime);
+    return () => clearTimeout(timer);
+  }, [showLogout]);
+
+  useEffect(() => {
+    if (!loaded) return () => undefined;
     const now = Date.now();
     const diff = now - init.current;
     const tooSoon = diff < minTime;
     const next = minTime - diff;
-    setTimeout(nav, tooSoon ? next : 0);
-  }, [loaded, nav, navigate]);
+    const timer = setTimeout(navNext, tooSoon ? next : 0);
+    return () => clearTimeout(timer);
+  }, [loaded, navNext, navigate]);
+
+  const handleTapAnimation = useCallback(() => {
+    setPulses((p) => [...p, p.length]);
+  }, []);
 
   return (
     <Screen>
@@ -35,22 +71,62 @@ export const Download = memo(function Download() {
         center
         flex
       >
-        <Lottie
-          autoPlay
-          loop
-          source={require('./loader.json')}
-          speed={0.75}
-          style={{
-            width: imageSize,
-            height: imageSize,
-            alignSelf: 'center',
-          }}
-        />
-        <Text
-          center
-          title="loading"
-          type="h5"
-        />
+        <TouchableOpacity
+          activeOpacity={1}
+          onPress={handleTapAnimation}
+          style={{ justifyContent: 'center', alignItems: 'center' }}
+        >
+          <Lottie
+            autoPlay
+            loop
+            source={require('./loader.json')}
+            speed={0.75}
+            style={{
+              zIndex: 1,
+              width: imageSize,
+              height: imageSize,
+              alignSelf: 'center',
+            }}
+          />
+          {pulses.map((pulse, index) => (
+            <PulseAnimation
+              key={pulse}
+              repeat={index === 0}
+              style={{
+                position: 'absolute',
+                width: tapWidth,
+                height: tapWidth,
+                borderRadius: tapWidth,
+                borderColor: colors.border.secondary,
+                borderWidth: 4,
+                backgroundColor: colors.background.secondary,
+              }}
+            />
+          ))}
+        </TouchableOpacity>
+        <Animated.View style={heartBeatStyles}>
+          <Text
+            center
+            title="loading"
+            type="h5"
+          />
+        </Animated.View>
+        <View style={{ opacity: showLogout ? 1 : 0 }}>
+          <Spacing padding={5} />
+          <Text
+            center
+            title="Taking too long?"
+            type="body1"
+          />
+          <Spacing padding={0.5} />
+          <Button
+            center
+            color="secondary"
+            emphasis="low"
+            onPress={handleLogout}
+            title="try logging out"
+          />
+        </View>
       </View>
     </Screen>
   );
