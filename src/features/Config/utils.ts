3bacc994aa2e82abc154c07f3b53c1f7caf7f352
Ethan Neff <ethan.neff@eneff.com>
2022-11-25 09:11:05 +0800

    fix: update deps (ios, android, web)

--- src/features/Config/utils.ts
+++ src/features/Config/utils.ts
@@ -6,7 +6,7 @@ export const spacing = (value: number): number => value * 4;
 export const colorWithOpacity = (colorCode: string, opacity = 0.5): string => {
   const boundedOpacity = opacity < 0 ? 0 : opacity > 1 ? 1 : opacity;
   const leading = 4;
-  if (!colorCode || !colorCode.startsWith('hsl')) return colorCode;
+  if (!colorCode.startsWith('hsl')) return colorCode;
   const substr = colorCode.substring(leading, colorCode.length - 1);
   return `hsla(${substr}, ${boundedOpacity})`;
 };
