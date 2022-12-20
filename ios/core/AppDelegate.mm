5696c85562f5534a56e1a6cc3b1f6eafc84aa9cb
Ethan Neff <ethan.neff@eneff.com>
2022-12-20 00:03:36 -0800

    feat: add flipper performance

--- ios/core/AppDelegate.mm
+++ ios/core/AppDelegate.mm
@@ -28,10 +28,19 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 @end
 #endif
 
+#ifdef FB_SONARKIT_ENABLED
+#import <FlipperKit/FlipperClient.h>
+#import <FlipperPerformancePlugin.h>
+#endif
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+#ifdef FB_SONARKIT_ENABLED
+  FlipperClient *client = [FlipperClient sharedClient];
+  [client addPlugin:[FlipperPerformancePlugin new]];
+#endif  
   RCTAppSetupPrepareApp(application);
 
   [FIRApp configure];
--- src/features/Debugger/index.tsx
+++ src/features/Debugger/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { connectToDevTools } from 'react-devtools-core';
 import { Platform } from 'react-native';
 
 export const debugDev = (): void => {
   if (Platform.OS === 'web' || !__DEV__) return;
 
+  connectToDevTools({ host: 'localhost', port: 8097 });
   const DevMenu = require('react-native-dev-menu');
   DevMenu.addItem('Highlight Renders', () => {
     require('react-native-highlight-updates');
