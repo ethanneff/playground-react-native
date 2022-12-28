0616555713b8a1d693d3e708747b56689232cb22
Ethan Neff <ethan.neff@eneff.com>
2022-12-27 16:18:11 -0800

    fix: remove dev tools

--- src/features/Debugger/index.tsx
+++ src/features/Debugger/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { connectToDevTools } from 'react-devtools-core';
 import { Platform } from 'react-native';
 
 export const debugDev = (): void => {
   if (Platform.OS === 'web' || !__DEV__) return;
 
-  connectToDevTools({ host: 'localhost', port: 8097 });
   const DevMenu = require('react-native-dev-menu');
   DevMenu.addItem('Highlight Renders', () => {
     require('react-native-highlight-updates');
