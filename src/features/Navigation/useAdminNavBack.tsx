2724d534815bbf28026cbea5f068b219e8caaf1f
Ethan Neff <ethan.neff@eneff.com>
2023-03-22 10:53:54 -0700

    fix: always admin on dev

--- src/features/Navigation/useAdminNavBack.tsx
+++ src/features/Navigation/useAdminNavBack.tsx
@@ -3,7 +3,7 @@ import Config from 'react-native-config';
 
 export const useAdminNavBack = () => {
   const { goBack } = useNavigation();
-  const admin = Config.APP === 'admin';
+  const admin = Config.APP === 'admin' || __DEV__;
 
   const onLeftPress = admin ? goBack : undefined;
 
