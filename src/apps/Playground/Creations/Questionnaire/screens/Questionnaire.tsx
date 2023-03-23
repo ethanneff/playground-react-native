f86ba7186716d11133931fa49225d84a44ce0c63
Ethan Neff <ethan.neff@eneff.com>
2023-03-22 20:43:20 -0700

    refactor: remove modal from questionnaire

--- src/apps/Playground/Creations/Questionnaire/screens/Questionnaire.tsx
+++ src/apps/Playground/Creations/Questionnaire/screens/Questionnaire.tsx
@@ -1,9 +1,8 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback } from 'react';
 import {
   Card,
   FlatList,
   Icon,
-  Modal,
   Text,
   TouchableOpacity,
   View,
@@ -15,29 +14,12 @@ import {
 } from '../models';
 
 export const Questionnaire = (): JSX.Element => {
-  const [actionSheet, setActionSheet] = useState(false);
-  const [activeItem, setActiveItem] = useState('');
-
-  const handleLongPress = (id: string) => () => {
-    setActionSheet((state) => !state);
-    setActiveItem(id);
-  };
-  const handleItemPress = useCallback(
-    (id: string) => () => {
-      setActiveItem(id);
-    },
-    [],
-  );
-  const handleActionSheetClose = useCallback(() => {
-    setActionSheet(false);
-  }, []);
-
   const renderItem = useCallback<FlatListRenderItem<QuestionnaireType>>(
     ({ item }) => {
       const { length } = item.questions;
       const subtitle = `${length} question${length === 1 ? '' : 's'}`;
       return (
-        <Card onPress={handleItemPress(item.id)}>
+        <Card>
           <View
             style={{
               flexDirection: 'row',
@@ -54,32 +36,25 @@ export const Questionnaire = (): JSX.Element => {
                 type="caption"
               />
             </View>
-            <TouchableOpacity onPress={handleLongPress(item.id)}>
+            <TouchableOpacity>
               <Icon name="dots-horizontal" />
             </TouchableOpacity>
           </View>
         </Card>
       );
     },
-    [handleItemPress],
+    [],
   );
 
   const keyExtractor = useCallback((item: QuestionnaireType) => item.id, []);
 
   return (
-    <>
-      <FlatList
-        data={Object.values(questionnairesInitialState.items)}
-        estimatedItemSize={161}
-        keyExtractor={keyExtractor}
-        keyboardShouldPersistTaps="handled"
-        renderItem={renderItem}
-      />
-      {actionSheet ? (
-        <Modal onBackgroundPress={handleActionSheetClose}>
-          <Text title="hello" />
-        </Modal>
-      ) : null}
-    </>
+    <FlatList
+      data={Object.values(questionnairesInitialState.items)}
+      estimatedItemSize={161}
+      keyExtractor={keyExtractor}
+      keyboardShouldPersistTaps="handled"
+      renderItem={renderItem}
+    />
   );
 };
