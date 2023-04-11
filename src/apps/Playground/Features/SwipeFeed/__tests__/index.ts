83932e969d3df30e6edc5d5715b5426b5474c81f
Ethan Neff <ethan.neff@eneff.com>
2023-04-10 21:52:50 -0700

    fix: correct timers

--- src/apps/Playground/Features/SwipeFeed/__tests__/index.ts
+++ src/apps/Playground/Features/SwipeFeed/__tests__/index.ts
@@ -1,65 +1,67 @@
-import { sub, toDate } from 'date-fns';
+import { add, toDate } from 'date-fns';
 import { formatRelativeDate } from '../utils';
+
+jest.useFakeTimers({ now: 1673445238335 });
 
 describe('formatRelativeDate', () => {
   it('a few seconds ago', () => {
     expect.hasAssertions();
-    const date = toDate(sub(new Date(), { seconds: 10 }));
+    const date = toDate(add(new Date(), { seconds: 10 }));
     expect(formatRelativeDate(date)).toBe('');
   });
   it('30 seconds ago', () => {
     expect.hasAssertions();
-    const date = toDate(sub(new Date(), { seconds: 30 }));
+    const date = toDate(add(new Date(), { seconds: 30 }));
     expect(formatRelativeDate(date)).toBe('');
   });
   it('a minute ago', () => {
     expect.hasAssertions();
-    const date = toDate(sub(new Date(), { minutes: 1 }));
+    const date = toDate(add(new Date(), { minutes: 1 }));
     expect(formatRelativeDate(date)).toBe('1m');
   });
   it('20 minutes ago', () => {
     expect.hasAssertions();
-    const date = toDate(sub(new Date(), { minutes: 20 }));
+    const date = toDate(add(new Date(), { minutes: 20 }));
     expect(formatRelativeDate(date)).toBe('20m');
   });
   it('an hour ago', () => {
     expect.hasAssertions();
-    const date = toDate(sub(new Date(), { hours: 1 }));
+    const date = toDate(add(new Date(), { hours: 1 }));
     expect(formatRelativeDate(date)).toBe('1h');
   });
   it('3 hours ago', () => {
     expect.hasAssertions();
-    const date = toDate(sub(new Date(), { hours: 3 }));
+    const date = toDate(add(new Date(), { hours: 3 }));
     expect(formatRelativeDate(date)).toBe('3h');
   });
   it('a day ago', () => {
     expect.hasAssertions();
-    const date = toDate(sub(new Date(), { days: 1 }));
+    const date = toDate(add(new Date(), { days: 1 }));
     expect(formatRelativeDate(date)).toBe('1d');
   });
   it('2 days ago', () => {
     expect.hasAssertions();
-    const date = toDate(sub(new Date(), { days: 3 }));
+    const date = toDate(add(new Date(), { days: 3 }));
     expect(formatRelativeDate(date)).toBe('3d');
   });
   it('a month ago', () => {
     expect.hasAssertions();
-    const date = toDate(sub(new Date(), { months: 1 }));
+    const date = toDate(add(new Date(), { months: 1 }));
     expect(formatRelativeDate(date)).toBe('4w');
   });
   it('3 months ago', () => {
     expect.hasAssertions();
-    const date = toDate(sub(new Date(), { months: 3 }));
+    const date = toDate(add(new Date(), { months: 3 }));
     expect(formatRelativeDate(date)).toBe('12w');
   });
   it('a year ago', () => {
     expect.hasAssertions();
-    const date = toDate(sub(new Date(), { years: 1 }));
+    const date = toDate(add(new Date(), { years: 1 }));
     expect(formatRelativeDate(date)).toBe('1y');
   });
   it('3 years ago', () => {
     expect.hasAssertions();
-    const date = toDate(sub(new Date(), { years: 3 }));
+    const date = toDate(add(new Date(), { years: 3 }));
     expect(formatRelativeDate(date)).toBe('3y');
   });
 });
