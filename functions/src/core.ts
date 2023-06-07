43963b4dbc5f4ae1288e971dd16321ead6e177b9
Ethan Neff <ethan.neff@eneff.com>
2023-06-07 11:23:37 -0700

    fix: stuffs

--- functions/src/core.ts
+++ functions/src/core.ts
@@ -5,7 +5,7 @@ export const accountCreate = functions.auth.user().onCreate(async (user) => {
   const facebookLogin = user.providerData.find(
     (data) => data.providerId === 'facebook.com',
   );
-  if (facebookLogin) return;
+  if (!facebookLogin) return;
   try {
     await admin.auth().updateUser(user.uid, { emailVerified: true });
   } catch (err) {
